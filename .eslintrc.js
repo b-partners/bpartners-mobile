45b0e4da1b2f154c3d880d23696391e4e3a72229
lianafinaritra <hei.lianafinaritra@gmail.com>
2022-08-17 15:47:34 +0300

    chore: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,51 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest": true
     },
-  ],
-};
+    "extends": [
+        "@react-native-community",
+        "plugin:react/recommended",
+        "prettier",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "react-native",
+        "react-hooks"
+    ],
+    "overrides": [
+        {
+          files: ['*.ts', '*.tsx'],
+          rules: {
+            '@typescript-eslint/no-shadow': ['error'],
+            'no-shadow': 'off',
+            'no-undef': 'off',
+            'import/prefer-default-export': 'off',
+            'react/no-unescaped-entities': 'off',
+            'max-len': ['error', { code: 120 }],
+            "react-native/no-inline-styles": "error",
+            "react-hooks/rules-of-hooks": "off",
+            "react/prop-types": "off"
+          },
+        },
+     ],
+    "globals": {
+        "__DEV__": true
+    },
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+    }
+}
