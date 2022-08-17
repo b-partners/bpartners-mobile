95ff327a4187d1405727c470bbd1950c244d0e0d
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-17 18:07:03 +0300

    update: formatting currency properly

--- app/screens/transaction/Transaction.tsx
+++ app/screens/transaction/Transaction.tsx
@@ -2,9 +2,10 @@ import React, {PropsWithoutRef} from "react";
 import {TextStyle, View, ViewStyle} from "react-native";
 import {Text} from "../../components";
 import {spacing} from "../../theme";
-import { Transaction as ITransaction } from '../../models/transaction/transaction';
+import {Transaction as ITransaction} from '../../models/transaction/transaction';
+import {translate} from "../../i18n";
 
-const TRANSACTION_AMOUNT: TextStyle = {fontSize: 20, fontWeight: 'bold'};
+const TRANSACTION_AMOUNT: TextStyle = {fontSize: 19, fontWeight: 'bold'};
 
 const LIST_TEXT: TextStyle = {
     fontWeight: "bold", marginBottom: spacing[2]
@@ -19,19 +20,23 @@ const LIST_CONTAINER: ViewStyle = {
 export const Transaction = (props: PropsWithoutRef<{ item: ITransaction }>) => {
     const {item} = props;
     const date = new Date(item.updateDateTime);
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: translate('currency'),
+        maximumFractionDigits: 0
+    });
 
     return <View style={LIST_CONTAINER}>
-        <View>
-            <Text style={{...LIST_TEXT }}>
+        <View style={{flex: 2}}>
+            <Text style={{...LIST_TEXT}}>
                 {item.title}
             </Text>
             <Text style={{...LIST_TEXT, fontWeight: "normal"}}>
                 {date.toISOString().split("T")[0]}
             </Text>
         </View>
-        <View style={{marginLeft: "auto", flexDirection: "row"}}>
-            <Text style={TRANSACTION_AMOUNT}>{item.amount}</Text>
-            <Text style={{...TRANSACTION_AMOUNT, marginLeft: 5}} tx="transactionListScreen.currency"></Text>
+        <View style={{marginLeft: "auto", flexDirection: "row", flex: 1}}>
+            <Text style={TRANSACTION_AMOUNT}>{formatter.format(item.amount)}</Text>
         </View>
     </View>;
 }
