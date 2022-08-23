72d63e522bd2b7fee89433161faf7b7bb7ad25a3
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-08-23 17:38:48 +0300

    fix: forgot to export connexion-screen

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -2,4 +2,5 @@ export * from './welcome/welcome-screen';
 export * from './demo/demo-screen';
 export * from './transaction/transaction-list-screen';
 export * from './error/error-boundary';
+export * from "./connexion/connexion-screen"
 // export other screens here
