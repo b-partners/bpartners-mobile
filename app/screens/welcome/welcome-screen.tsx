6794cc62a2d4d037443504aaeace49693f6b2f8a
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-08-24 13:49:16 +0300

    style: format

--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -68,7 +68,7 @@ const FOOTER_CONTENT: ViewStyle = {
   justifyContent: 'space-between',
 };
 
-export const WelcomeScreen: FC<StackScreenProps<NavigatorParamList, 'welcome'>> = observer(({ navigation }) => {
+export const WelcomeScreen: FC<StackScreenProps<NavigatorParamList, 'connexion'>> = observer(({ navigation }) => {
   const login = () => navigation.navigate('demo');
   const { onboardingStore } = useStores();
 
