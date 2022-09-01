a1e186b9974fa7cb3290b1adf0eacc0979b8016f
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-01 23:06:38 +0300

    chore: fix useless file

--- app/services/api/api-request-transform.ts
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const authRequestTransform = whiteList => async request => {
-  if (whiteList) {
-    return;
-  }
-  const accessToken = await AsyncStorage.getItem('accessToken');
-  request.headers.Authorization = `Bearer ${accessToken}`;
-};
