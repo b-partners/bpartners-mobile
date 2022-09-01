e69192e074152bd22a536c083b5c9b58fbbb38e2
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-01 12:33:33 +0300

    chore: editing transaction model

--- app/models/transaction/transaction.ts
+++ app/models/transaction/transaction.ts
@@ -2,10 +2,12 @@ import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
 
 export const TransactionModel = types.model('Transaction').props({
   id: types.identifierNumber,
-  title: types.maybe(types.string),
+  label: types.maybe(types.string),
+  reference: types.maybe(types.string),
   amount: types.maybe(types.number),
-  paymentReqId: types.maybe(types.number),
-  updateDateTime: types.maybe(types.string),
+  category: types.maybe(types.string),
+  swanTransactionId: types.maybe(types.number),
+  paymentDateTime: types.maybe(types.string),
 });
 
 export interface Transaction extends Instance<typeof TransactionModel> {}
