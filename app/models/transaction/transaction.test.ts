eb1308cc156371367b5437fcb70d1c82a412fcab
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-01 12:42:28 +0300

    chore: editing transaction test

--- app/models/transaction/transaction.test.ts
+++ app/models/transaction/transaction.test.ts
@@ -3,9 +3,12 @@ import { TransactionModel } from './transaction';
 test('can be created', () => {
   const instance = TransactionModel.create({
     id: 1,
-    amount: 1000,
-    paymentReqId: 1,
-    updateDateTime: new Date(),
+    label: null,
+    reference: null,
+    amount: null,
+    category: null,
+    swanTransactionId: null,
+    paymentDateTime: null,
   });
 
   expect(instance).toBeTruthy();
