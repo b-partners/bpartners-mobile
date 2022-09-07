446d307382930b3709415e09164c99f3466331c6
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-07 13:01:35 +0300

    feat: profile screen

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -4,5 +4,5 @@ export * from './transaction/transaction-list-screen';
 export * from './error/error-boundary';
 export * from './sign-in/sign-in-screen';
 export * from './onboarding/onboarding-screen';
-// export other screens here
 export * from './payment-initiation/payment-initiation-screen';
+export * from './profile/profile-screen';
