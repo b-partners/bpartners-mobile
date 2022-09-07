db8e73c05eea5691583419a1b1748af57f36568b
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-07 15:53:23 +0300

    fix: adding balance

--- app/screens/transaction/transaction-list-screen.tsx
+++ app/screens/transaction/transaction-list-screen.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect } from 'react';
 import { FlatList, TextStyle, View, ViewStyle } from 'react-native';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { observer } from 'mobx-react-lite';
-import { GradientBackground, Header, Screen, Separator } from '../../components';
+import { GradientBackground, Header, Screen, Separator, Text } from '../../components';
 import { color, spacing } from '../../theme';
 import { useStores } from '../../models';
 import { NavigatorParamList } from '../../navigators';
@@ -20,12 +20,23 @@ const HEADER: TextStyle = {
   paddingHorizontal: spacing[4],
   paddingTop: spacing[3],
 };
+const SUB_HEADER: TextStyle = {
+  paddingHorizontal: spacing[4],
+  paddingTop: spacing[3],
+};
 const HEADER_TITLE: TextStyle = {
-  fontSize: 12,
+  fontSize: 15,
   fontWeight: 'bold',
   letterSpacing: 1.5,
   lineHeight: 15,
   textAlign: 'center',
+};
+const SUB_HEADER_TITLE: TextStyle = {
+  fontSize: 14,
+  fontWeight: 'bold',
+  letterSpacing: 1.5,
+  lineHeight: 15,
+  marginBottom: spacing[2],
 };
 const FLAT_LIST: ViewStyle = {
   paddingHorizontal: spacing[4],
@@ -52,6 +63,10 @@ export const TransactionListScreen: FC<DrawerScreenProps<NavigatorParamList, 'tr
       <GradientBackground colors={['#422443', '#281b34']} />
       <Screen style={CONTAINER} preset='fixed' backgroundColor={color.transparent}>
         <Header headerTx='transactionListScreen.title' style={HEADER} titleStyle={HEADER_TITLE} />
+        <View style={SUB_HEADER}>
+          <Text tx={'transactionListScreen.balance'} style={SUB_HEADER_TITLE} />
+          <Text style={SUB_HEADER_TITLE}>O €</Text>
+        </View>
         <FlatList
           contentContainerStyle={FLAT_LIST}
           data={[...transactions]}
