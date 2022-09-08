80a4dfff68eb41e0a1a55470871d5d2bf127754b
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-08 15:12:23 +0300

    chore: caching account

--- app/services/api/account-api.ts
+++ app/services/api/account-api.ts
@@ -2,6 +2,7 @@ import { ApiResponse } from 'apisauce';
 import { Api } from './api';
 import { GetUserAccount } from './api.types';
 import { getGeneralApiProblem } from './api-problem';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export class AccountApi {
   private api: Api;
@@ -11,16 +12,23 @@ export class AccountApi {
   }
 
   async getAccounts(userId: string): Promise<GetUserAccount> {
+    const account = JSON.parse(await AsyncStorage.getItem(`account-${userId}`));
+    if (account) {
+      console.tron.log(`Returning cached account ${account.id}`);
+      return account;
+    }
     try {
       // make the api call
+      console.tron.log(`Fetching ${userId}'s account`);
       const response: ApiResponse<any> = await this.api.apisauce.get(`users/${userId}/accounts`);
       // the typical ways to die when calling an api
       if (!response.ok) {
         const problem = getGeneralApiProblem(response);
         if (problem) return problem;
       }
-      const [account] = response.data;
-      return { kind: 'ok', account };
+      const [fetchedAccount] = response.data;
+      await AsyncStorage.setItem(`account-${userId}`, JSON.stringify(fetchedAccount));
+      return { kind: 'ok', account: fetchedAccount };
     } catch (e) {
       __DEV__ && console.tron.log(e.message);
       return { kind: 'bad-data' };
