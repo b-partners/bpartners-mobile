55b9ac1af4d8b2a0b0fb110fe61bc254da37c399
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-13 14:17:13 +0300

    chore: creating home screen

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -5,3 +5,4 @@ export * from './sign-in/sign-in-screen';
 export * from './onboarding/onboarding-screen';
 export * from './payment-initiation/payment-initiation-screen';
 export * from './profile/profile-screen';
+export * from "./home/home-screen"
