e5534190a42a7c155e937c5948ca5e20dc5b1ee2
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-13 14:12:21 +0300

    chore: adding sign in e2e test

---
+++ e2e/sign-in.spec.js
@@ -0,0 +1,21 @@
+const { reloadApp } = require('./reload');
+
+describe('Sign in', () => {
+  beforeEach(async () => {
+    await reloadApp();
+  });
+
+  it('should redirect to swan', async () => {
+    await element(by.id('sign-in-button')).tap();
+    await element(by.id('phone-number')).typeText('+261340465338');
+    await element(by.id('submit-button')).tap();
+    await expect(element(by.id('SignInWebViewScreen'))).toBeVisible();
+  });
+
+  it('should display form validation error', async () => {
+    await element(by.id('header-left-button')).tap();
+    await element(by.id('phone-number')).typeText('lorem');
+    await element(by.id('submit-button')).tap();
+    await expect(element(by.id('error-message'))).toBeVisible();
+  });
+});
