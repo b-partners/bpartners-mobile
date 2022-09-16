9a07586e75eb3c6014cce0af26d6cc3eddf40f66
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-16 15:33:58 +0300

    fix: rendering issue

--- app/screens/home/components/home-latest-transaction.tsx
+++ app/screens/home/components/home-latest-transaction.tsx
@@ -8,20 +8,16 @@ import React from 'react';
 export function HomeLatestTransaction(props: { transactions: ITransaction[]; onPress: () => void }) {
   return (
     <View>
-      {props.transactions.length && (
-        <View>
-          <View style={TRANSACTION_BUTTONS_STYLE}>
-            <Button tx='homeScreen.labels.allTransactions' style={BUTTON_STYLE} textStyle={BUTTON_TEXT_STYLE} onPress={props.onPress} />
-          </View>
-          <FlatList
-            data={[...props.transactions]}
-            renderItem={({ item }) => {
-              return <Transaction item={item} key={item.id} />;
-            }}
-            ItemSeparatorComponent={() => <Separator />}
-          />
-        </View>
-      )}
+      <View style={TRANSACTION_BUTTONS_STYLE}>
+        <Button tx='homeScreen.labels.allTransactions' style={BUTTON_STYLE} textStyle={BUTTON_TEXT_STYLE} onPress={props.onPress} />
+      </View>
+      <FlatList
+        data={[...props.transactions]}
+        renderItem={({ item }) => {
+          return <Transaction item={item} />;
+        }}
+        ItemSeparatorComponent={() => <Separator />}
+      />
     </View>
   );
 }
