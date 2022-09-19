b38265c67fd300a1de1e46861c50d73338fdf34b
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-19 17:21:25 +0300

    chore: replacing label and payer name component

--- app/screens/payment-initiation/payment-initiation-form.tsx
+++ app/screens/payment-initiation/payment-initiation-form.tsx
@@ -1,12 +1,13 @@
-import React, { FC, PropsWithoutRef } from 'react';
+import React, { FC, PropsWithoutRef, useState } from 'react';
 import { Formik } from 'formik';
 import FormField from '../sign-in/components/form-field';
-import { Button, TextStyle, View } from 'react-native';
+import { Button, TextStyle, TouchableOpacity, View } from 'react-native';
 import { translate } from '../../i18n';
 import * as yup from 'yup';
 import { color, spacing } from '../../theme';
 import { useStores } from '../../models';
 import uuid from 'react-native-uuid';
+import { AutocompletionFormField, Text } from '../../components';
 
 const FORM_FIELD_STYLE: TextStyle = { color: color.palette.black, paddingHorizontal: spacing[2], paddingBottom: 0 };
 const INVALID_FORM_FIELD = {
@@ -14,6 +15,9 @@ const INVALID_FORM_FIELD = {
   borderWidth: 2,
 };
 
+const AUTOCOMPLETION_CONTAINER_STYLE = { paddingVertical: spacing[2], backgroundColor: color.palette.white };
+
+const AUTOCOMPLETION_ITEM_TEXT_STYLE = { color: color.palette.black, paddingHorizontal: spacing[2] };
 export const PaymentInitiationForm: FC<PropsWithoutRef<any>> = () => {
   const initialValues = { label: '', reference: '', amount: null, payerName: '', payerEmail: '' };
 
@@ -23,6 +27,11 @@ export const PaymentInitiationForm: FC<PropsWithoutRef<any>> = () => {
     amount: yup.number().required().label(translate('paymentInitiationScreen.fields.amount')),
     payerEmail: yup.string().email().label(translate('paymentInitiationScreen.fields.payerEmail')),
   });
+
+  const [labelValue, setLabelValue] = useState<string>();
+  const [payerNameValue, setPayerNameValue] = useState<string>();
+  const [products] = useState(new Array(5).fill(null).map((_, i) => ({ description: `Test ${i}` })));
+  const [customers] = useState(new Array(5).fill(null).map((_, i) => ({ name: `Payer ${i}` })));
 
   return (
     <Formik
@@ -36,10 +45,29 @@ export const PaymentInitiationForm: FC<PropsWithoutRef<any>> = () => {
         }
       }}
     >
-      {({ handleSubmit, errors }) => {
+      {({ handleSubmit, errors, setFieldValue }) => {
         return (
           <>
-            <FormField name='label' inputStyle={[FORM_FIELD_STYLE]} placeholderTx='paymentInitiationScreen.fields.label' />
+            <AutocompletionFormField
+              data={[...products]}
+              hideResults={!products.length}
+              value={labelValue}
+              onChangeText={label => {
+                setLabelValue(label);
+              }}
+              keyExtractor={(item, i) => i}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    setLabelValue(item.description);
+                    setFieldValue('item', item.description);
+                  }}
+                  style={AUTOCOMPLETION_CONTAINER_STYLE}
+                >
+                  <Text text={item.description} style={{ ...FORM_FIELD_STYLE, ...AUTOCOMPLETION_ITEM_TEXT_STYLE }} />
+                </TouchableOpacity>
+              )}
+            />
             <FormField name='reference' inputStyle={[FORM_FIELD_STYLE]} placeholderTx='paymentInitiationScreen.fields.reference' />
             <FormField
               name='amount'
@@ -47,7 +75,26 @@ export const PaymentInitiationForm: FC<PropsWithoutRef<any>> = () => {
               placeholderTx='paymentInitiationScreen.fields.amount'
               keyboardType='phone-pad'
             />
-            <FormField name='payerName' inputStyle={[FORM_FIELD_STYLE]} placeholderTx='paymentInitiationScreen.fields.payerName' />
+            <AutocompletionFormField
+              data={[...customers]}
+              hideResults={!customers.length}
+              value={payerNameValue}
+              onChangeText={name => {
+                setPayerNameValue(name);
+              }}
+              keyExtractor={(item, i) => i}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    setPayerNameValue(item.name);
+                    setFieldValue('payerName', item.name);
+                  }}
+                  style={AUTOCOMPLETION_CONTAINER_STYLE}
+                >
+                  <Text text={item.name} style={{ ...FORM_FIELD_STYLE, ...AUTOCOMPLETION_ITEM_TEXT_STYLE }} />
+                </TouchableOpacity>
+              )}
+            />
             <FormField
               name='payerEmail'
               inputStyle={[FORM_FIELD_STYLE]}
