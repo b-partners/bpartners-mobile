c506cf03e949e246c07c9200c8faa10b99809e24
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-19 18:10:09 +0300

    fix: adding name query params

--- app/services/api/customer-api.ts
+++ app/services/api/customer-api.ts
@@ -10,11 +10,11 @@ export class CustomerApi {
     this.api = api;
   }
 
-  async getCustomers(account: string): Promise<GetCustomersResult> {
+  async getCustomers(account: string, name = ''): Promise<GetCustomersResult> {
     try {
       // make the api call
       console.tron.log(`Fetching account's customer`);
-      const response: ApiResponse<any> = await this.api.apisauce.get(`accounts/${account}/customers`);
+      const response: ApiResponse<any> = await this.api.apisauce.get(`accounts/${account}/customers`, { name });
       // the typical ways to die when calling an api
       if (!response.ok) {
         const problem = getGeneralApiProblem(response);
