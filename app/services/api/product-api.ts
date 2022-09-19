b914cdac9ee20375443a29124058a334453298f3
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-19 17:54:47 +0300

    feat: implementing get products

---
+++ app/services/api/product-api.ts
@@ -0,0 +1,30 @@
+import { ApiResponse } from 'apisauce';
+import { Api } from './api';
+import { GetProductsResult } from './api.types';
+import { getGeneralApiProblem } from './api-problem';
+
+export class ProductApi {
+  private api: Api;
+
+  constructor(api: Api) {
+    this.api = api;
+  }
+
+  async getProducts(account: string): Promise<GetProductsResult> {
+    try {
+      // make the api call
+      console.tron.log(`Fetching account's products`);
+      const response: ApiResponse<any> = await this.api.apisauce.get(`accounts/${account}/products`);
+      // the typical ways to die when calling an api
+      if (!response.ok) {
+        const problem = getGeneralApiProblem(response);
+        if (problem) return problem;
+      }
+      const [products] = response.data;
+      return { kind: 'ok', products };
+    } catch (e) {
+      __DEV__ && console.tron.log(e.message);
+      return { kind: 'bad-data' };
+    }
+  }
+}
