a1a8cff121b53dfb0d1ba2700435f42006a3b64d
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-09-19 17:37:40 +0300

    feat: creating product model

---
+++ app/models/product/product.test.ts
@@ -0,0 +1,16 @@
+import { ProductModel } from './product';
+import uuid from 'react-native-uuid';
+
+test('can be created', () => {
+  const instance = ProductModel.create({
+    id: uuid.v4() as string,
+    description: 'Lorem',
+    quantity: 0,
+    unitPrice: 0,
+    vatPercent: 0,
+    totalVat: 0,
+    totalPriceWithVat: 0,
+  });
+
+  expect(instance).toBeTruthy();
+});
---
+++ app/models/product/product.ts
@@ -0,0 +1,19 @@
+import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
+
+export const ProductModel = types.model('Product').props({
+  id: types.maybe(types.string),
+  description: types.maybe(types.string),
+  quantity: types.maybe(types.number),
+  unitPrice: types.maybe(types.number),
+  vatPercent: types.maybe(types.number),
+  totalVat: types.maybe(types.number),
+  totalPriceWithVat: types.maybe(types.number),
+});
+
+export interface Product extends Instance<typeof ProductModel> {}
+
+export interface ProductSnapshotOut extends SnapshotOut<typeof ProductModel> {}
+
+export interface ProductSnapshotIn extends SnapshotIn<typeof ProductModel> {}
+
+export const createProductDefaultModel = () => types.optional(ProductModel, {});
