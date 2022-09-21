ce4266e4546d86473b49fe1aac48066767f39fd2
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-21 21:46:34 +0300

    refactor: using one file instead of array of files when uploading

--- app/components/file-upload/file-upload.tsx
+++ app/components/file-upload/file-upload.tsx
@@ -4,11 +4,13 @@ import { TxKeyPath } from '../../i18n';
 import { Button } from '../button/button';
 import { View, ViewStyle } from 'react-native';
 import { color } from '../../theme';
+import { useStores } from '../../models';
 
 type FileUploadProps = {
   selectFileTx: TxKeyPath;
   uploadFileTx: TxKeyPath;
   onUploadFile: () => void;
+  fileId: string;
 };
 
 const BUTTON_STYLE = { backgroundColor: color.palette.lighterGrey, flex: 1 };
@@ -20,13 +22,16 @@ const CONTAINER_STYLE: ViewStyle = { display: 'flex', flexDirection: 'row' };
 const FLEX_ITEM_STYLE = { flex: 1 };
 
 export function FileUpload(props: FileUploadProps) {
-  const [fileToUpload, setFileToUpload] = useState([]);
+  const [fileToUpload, setFileToUpload] = useState<File>(null);
+  const { fileStore } = useStores();
 
   const uploadFile = () => {
     if (!fileToUpload) {
       console.tron.log('Please select a file', fileToUpload);
       return;
     }
+
+    fileStore.upload(fileToUpload, props.fileId);
   };
 
   const selectFile = async () => {
@@ -35,7 +40,13 @@ export function FileUpload(props: FileUploadProps) {
         type: [DocumentPicker.types.allFiles],
       });
       console.tron.log(`Picking file`, result);
-      setFileToUpload(result);
+      const documentPickerResponse = result[0];
+
+      // get the file from the file system
+      const blob = await (await fetch(documentPickerResponse.uri)).blob();
+      const file = new File([blob], documentPickerResponse.name);
+
+      setFileToUpload(file);
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
         console.tron.log(`Canceling upload`);
@@ -52,18 +63,18 @@ export function FileUpload(props: FileUploadProps) {
         <Button
           style={BUTTON_STYLE}
           textStyle={BUTTON_TEXT_STYLE}
-          onPress={fileToUpload.length > 0 ? uploadFile : selectFile}
-          tx={fileToUpload.length > 0 ? props.uploadFileTx : props.selectFileTx}
+          onPress={fileToUpload ? uploadFile : selectFile}
+          tx={fileToUpload ? props.uploadFileTx : props.selectFileTx}
         />
       </View>
       <View style={FLEX_ITEM_STYLE}>
-        {fileToUpload.length > 0 && (
+        {fileToUpload && (
           <Button
             style={BUTTON_STYLE}
             textStyle={BUTTON_TEXT_STYLE}
             onPress={() => {
               console.tron.log(`Deleting file`);
-              setFileToUpload([]);
+              setFileToUpload(null);
             }}
             tx={'profileScreen.fields.removeFileButton'}
           />
