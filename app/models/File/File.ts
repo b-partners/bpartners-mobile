41918994c5888ecd498e2ec5c671ae32f30b22de
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-21 21:40:11 +0300

    feat: adding fileInfos model

---
+++ app/models/File/File.ts
@@ -0,0 +1,17 @@
+import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
+
+export const FileModel = types.model('File').props({
+  id: types.maybe(types.identifier),
+  uploadedAt: types.maybe(types.string),
+  uploadedByUserId: types.maybe(types.string),
+  sizeInKB: types.maybe(types.integer),
+  sha256: types.maybe(types.string),
+});
+
+export interface File extends Instance<typeof FileModel> {}
+
+export interface FileSnapshotOut extends SnapshotOut<typeof FileModel> {}
+
+export interface FileSnapshotIn extends SnapshotIn<typeof FileModel> {}
+
+export const createFileDefaultModel = () => types.optional(FileModel, {});
