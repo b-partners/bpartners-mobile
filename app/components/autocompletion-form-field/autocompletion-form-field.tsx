44440f2d421967d4670d72814b419297c813a7b6
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-11 17:35:38 +0300

    feat: implementing container and input style container style override

--- app/components/autocompletion-form-field/autocompletion-form-field.tsx
+++ app/components/autocompletion-form-field/autocompletion-form-field.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AutocompleteDropdown, TAutocompleteDropdownItem } from 'react-native-autocomplete-dropdown';
+import { ViewStyle } from 'react-native';
 
 interface AutocompletionFormFieldProps<T> {
   data: any[];
@@ -7,15 +8,28 @@ interface AutocompletionFormFieldProps<T> {
   selectTitle: (item: T) => TAutocompleteDropdownItem;
   onChangeText: (text: string) => void;
   onSelectItem: (item: TAutocompleteDropdownItem) => void;
+  onClear: () => void;
+  containerStyle?: ViewStyle;
+  inputContainerStyle?: ViewStyle;
 }
 
 export function AutocompletionFormField<T>(props: AutocompletionFormFieldProps<T>) {
-  const { data, selectTitle, value, onChangeText, onSelectItem } = props;
+  const { data, selectTitle, value, onChangeText, onSelectItem, onClear, containerStyle, inputContainerStyle } = props;
   const [dataSet, setDataSet] = useState<TAutocompleteDropdownItem[]>([]);
 
   useEffect(() => {
     setDataSet(data.map(item => selectTitle(item)));
   }, [data, selectTitle]);
 
-  return <AutocompleteDropdown dataSet={dataSet} initialValue={value} onChangeText={onChangeText} onSelectItem={item => onSelectItem(item)} />;
+  return (
+    <AutocompleteDropdown
+      containerStyle={containerStyle}
+      inputContainerStyle={inputContainerStyle}
+      dataSet={dataSet}
+      initialValue={value}
+      onChangeText={onChangeText}
+      onSelectItem={item => onSelectItem(item)}
+      onClear={onClear}
+    />
+  );
 }
