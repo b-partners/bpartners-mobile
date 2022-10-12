7ce3e9ca77b0d3eadc8443fe1822756ea91abbfa
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-12 12:34:01 +0300

    fix: total computation, product deletion, quantity change

--- app/screens/create-invoice/components/invoice-form.tsx
+++ app/screens/create-invoice/components/invoice-form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, ViewStyle } from 'react-native';
-import { AutocompletionFormField, Separator, Text, TextField } from '../../../components';
+import { TouchableOpacity, View } from 'react-native';
+import { AutocompletionFormField, Icon, Separator, Text, TextField } from '../../../components';
 import { Customer } from '../../../models/entities/customer/customer';
 import { Product } from '../../../models/entities/product/product';
 import { currencyPipe } from '../../../utils/pipes';
@@ -8,27 +8,57 @@ import { translate } from '../../../i18n';
 import { spacing } from '../../../theme';
 import { AddItem } from './add-item';
 import {
+  DELETE_ICON_STYLE,
+  INPUT_LABEL_STYLE,
+  INPUT_TEXT_STYLE,
+  LABEL_CONTAINER_STYLE,
   PRODUCT_ITEM_CROSS_STYLE,
   PRODUCT_ITEM_FOOTER_STYLE,
+  PRODUCT_ITEM_HEADER_RIGHT_SECTION,
   PRODUCT_ITEM_HEADER_STYLE,
   PRODUCT_ITEM_QUANTITY_STYLE,
   SECTION_STYLE,
   TEXT_FIELD_STYLE,
+  TOTAL_SECTION_STYLE,
 } from '../styles';
 
 type InvoiceFormProps = { customers: Customer[]; products: Product[] };
-
-const TOTAL_SECTION_STYLE: ViewStyle = { display: 'flex', flexDirection: 'row', justifyContent: 'space-between' };
 
 export function InvoiceForm(props: InvoiceFormProps) {
   const { customers, products } = props;
   const [selectedCustomers, setSelectedCustomers] = useState<Customer[]>([]);
   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
 
+  const onClear = () => {
+    setSelectedProducts([]);
+  };
+
+  const paymentTotal = currencyPipe(translate('currency')).format(
+    selectedProducts.reduce((a, c) => {
+      return a + c.totalPriceWithVat * c.quantity;
+    }, 0)
+  );
+
   return (
     <View style={{ paddingHorizontal: spacing[3] }}>
-      <TextField testID='ref' nativeID='ref' style={TEXT_FIELD_STYLE} />
-      <TextField testID='title' nativeID='ref' style={TEXT_FIELD_STYLE} />
+      <TextField
+        testID='ref'
+        nativeID='ref'
+        style={TEXT_FIELD_STYLE}
+        labelTx='invoiceScreen.labels.ref'
+        labelContainerStyle={LABEL_CONTAINER_STYLE}
+        labelStyle={INPUT_LABEL_STYLE}
+        inputStyle={INPUT_TEXT_STYLE}
+      />
+      <TextField
+        testID='title'
+        nativeID='ref'
+        style={TEXT_FIELD_STYLE}
+        labelTx='invoiceScreen.labels.title'
+        labelContainerStyle={LABEL_CONTAINER_STYLE}
+        labelStyle={INPUT_LABEL_STYLE}
+        inputStyle={INPUT_TEXT_STYLE}
+      />
       <Text tx='invoiceScreen.labels.customerSection' style={SECTION_STYLE} />
       <Separator style={{ marginBottom: spacing[4] }} />
       <AutocompletionFormField<Customer>
@@ -58,25 +88,52 @@ export function InvoiceForm(props: InvoiceFormProps) {
         onChangeText={() => {}}
         selectTitle={item => ({ id: item.id, title: item.description })}
         data={products}
-        onClear={() => {
-          setSelectedProducts([]);
-        }}
+        onClear={onClear}
         renderItem={({ item }) => {
-          const total = item.quantity * item.totalPriceWithVat;
+          const productTotal = item.quantity * item.totalPriceWithVat;
+          const unitPrice = currencyPipe(translate('currency')).format(item.unitPrice);
+          const totalVat = `${currencyPipe(translate('currency')).format(item.totalVat)}`;
 
+          const deleteProduct = () => {
+            setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts.filter(p => p.id !== item.id)]);
+          };
+
+          let onChangeQuantity = quantity => {
+            setSelectedProducts(prevSelectedProducts => [
+              ...prevSelectedProducts.map(p =>
+                p.id === item.id
+                  ? {
+                      ...p,
+                      quantity: +quantity,
+                    }
+                  : p
+              ),
+            ]);
+          };
           return (
             <View>
               <View style={PRODUCT_ITEM_HEADER_STYLE}>
                 <Text text={item.description} />
-                <Text text={currencyPipe(translate('currency')).format(total)} />
+                <View style={PRODUCT_ITEM_HEADER_RIGHT_SECTION}>
+                  <Text text={currencyPipe(translate('currency')).format(productTotal)} />
+                  <TouchableOpacity style={DELETE_ICON_STYLE} onPress={deleteProduct}>
+                    <Icon icon='cross' />
+                  </TouchableOpacity>
+                </View>
               </View>
               <View>
-                <Text text={`${translate('invoiceScreen.labels.vat')}: ${currencyPipe(translate('currency')).format(item.totalVat)}`} />
+                <Text text={`${translate('invoiceScreen.labels.vat')}: ${totalVat}`} />
               </View>
               <View style={PRODUCT_ITEM_FOOTER_STYLE}>
-                <TextField keyboardType='numeric' style={PRODUCT_ITEM_QUANTITY_STYLE} value={item.quantity} />
+                <TextField
+                  keyboardType='numeric'
+                  style={PRODUCT_ITEM_QUANTITY_STYLE}
+                  inputStyle={INPUT_TEXT_STYLE}
+                  defaultValue={item.quantity.toString()}
+                  onChangeText={onChangeQuantity}
+                />
                 <Text text='x' style={PRODUCT_ITEM_CROSS_STYLE} />
-                <Text text={currencyPipe(translate('currency')).format(item.unitPrice)} />
+                <Text text={unitPrice} />
               </View>
             </View>
           );
@@ -84,13 +141,7 @@ export function InvoiceForm(props: InvoiceFormProps) {
       />
       <View style={TOTAL_SECTION_STYLE}>
         <Text text={`${translate('invoiceScreen.labels.totalSection')}: `} style={SECTION_STYLE} />
-        <Text
-          text={currencyPipe(translate('currency')).format(
-            selectedProducts.reduce((a, c) => {
-              return a + c.totalPriceWithVat * c.quantity;
-            }, 0)
-          )}
-        />
+        <Text text={paymentTotal} />
       </View>
       <Separator style={{ marginBottom: spacing[4] }} />
     </View>
