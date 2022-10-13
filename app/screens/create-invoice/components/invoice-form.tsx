459496972c69e82df4f2f15503a312a9ea80d9f6
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-13 17:41:51 +0300

    refactor: form's components

--- app/screens/create-invoice/components/invoice-form.tsx
+++ app/screens/create-invoice/components/invoice-form.tsx
@@ -1,29 +1,17 @@
 import React, { useState } from 'react';
-import { TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
-import { AutocompletionFormField, Button, Icon, Separator, Text, TextField } from '../../../components';
+import { TextStyle, View, ViewStyle } from 'react-native';
+import { AutocompletionFormField, Button, Separator, Text, TextField } from '../../../components';
 import { Customer } from '../../../models/entities/customer/customer';
 import { Product } from '../../../models/entities/product/product';
 import { currencyPipe } from '../../../utils/pipes';
 import { translate } from '../../../i18n';
 import { spacing } from '../../../theme';
 import { AddItem } from './add-item';
-import {
-  DELETE_ICON_STYLE,
-  INPUT_LABEL_STYLE,
-  INPUT_TEXT_STYLE,
-  LABEL_CONTAINER_STYLE,
-  PRODUCT_ITEM_CROSS_STYLE,
-  PRODUCT_ITEM_FOOTER_STYLE,
-  PRODUCT_ITEM_HEADER_RIGHT_SECTION,
-  PRODUCT_ITEM_HEADER_STYLE,
-  PRODUCT_ITEM_QUANTITY_STYLE,
-  SECTION_STYLE,
-  TEXT_FIELD_STYLE,
-  TOTAL_SECTION_STYLE,
-} from '../styles';
+import { INPUT_LABEL_STYLE, INPUT_TEXT_STYLE, LABEL_CONTAINER_STYLE, SECTION_STYLE, TEXT_FIELD_STYLE, TOTAL_SECTION_STYLE } from '../styles';
 import { Formik } from 'formik';
 import uuid from 'react-native-uuid';
 import { DatePicker } from '../../../components/date-picker';
+import { ProductFormField } from './product-form-field';
 
 type InvoiceFormProps = { customers: Customer[]; products: Product[] };
 
@@ -45,15 +33,35 @@ export function InvoiceForm(props: InvoiceFormProps) {
   const [selectedCustomers, setSelectedCustomers] = useState<Customer[]>([]);
   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
 
-  const onClear = () => {
-    setSelectedProducts([]);
-  };
-
   const paymentTotal = currencyPipe(translate('currency')).format(
     selectedProducts.reduce((a, c) => {
       return a + c.totalPriceWithVat * c.quantity;
     }, 0)
   );
+
+  const onClearProducts = () => {
+    setSelectedProducts([]);
+  };
+
+  const renderProductItem = ({ item }) => {
+    const deleteProduct = () => {
+      setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts.filter(p => p.id !== item.id)]);
+    };
+    const onChangeQuantity = quantity => {
+      setSelectedProducts(prevSelectedProducts => [
+        ...prevSelectedProducts.map(p =>
+          p.id === item.id
+            ? {
+                ...p,
+                quantity: +quantity,
+              }
+            : p
+        ),
+      ]);
+    };
+
+    return <ProductFormField item={item} onDeleteProduct={deleteProduct} onQuantityChange={onChangeQuantity} />;
+  };
 
   return (
     <View style={{ paddingHorizontal: spacing[3] }}>
@@ -88,6 +96,7 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 inputStyle={INPUT_TEXT_STYLE}
                 onChangeText={ref => setFieldValue('ref', ref)}
               />
+
               <TextField
                 testID='title'
                 nativeID='ref'
@@ -98,6 +107,7 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 inputStyle={INPUT_TEXT_STYLE}
                 onChangeText={title => setFieldValue('title', title)}
               />
+
               <View style={DATEPICKER_ROW_STYLE}>
                 <DatePicker
                   labelTx='invoiceScreen.labels.sendingDate'
@@ -106,6 +116,7 @@ export function InvoiceForm(props: InvoiceFormProps) {
                   onDateChange={date => setFieldValue('sendingDate', date)}
                 />
               </View>
+
               <View style={DATEPICKER_ROW_STYLE}>
                 <DatePicker
                   labelTx='invoiceScreen.labels.toPayAt'
@@ -114,8 +125,11 @@ export function InvoiceForm(props: InvoiceFormProps) {
                   onDateChange={date => setFieldValue('toPayAt', date)}
                 />
               </View>
+
               <Text tx='invoiceScreen.labels.customerSection' style={SECTION_STYLE} />
+
               <Separator style={{ marginBottom: spacing[4] }} />
+
               <AutocompletionFormField<Customer>
                 containerStyle={{ marginBottom: spacing[4] }}
                 onSelectItem={item => {
@@ -134,8 +148,11 @@ export function InvoiceForm(props: InvoiceFormProps) {
                   setSelectedCustomers([]);
                 }}
               />
+
               <Text tx='invoiceScreen.labels.productSection' style={SECTION_STYLE} />
+
               <Separator style={{ marginBottom: spacing[4] }} />
+
               <AddItem
                 containerStyle={{ marginBottom: spacing[4] }}
                 selectedItems={selectedProducts}
@@ -146,65 +163,17 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 onChangeText={() => {}}
                 selectTitle={item => ({ id: item.id, title: item.description })}
                 data={products}
-                onClear={onClear}
-                renderItem={({ item }) => {
-                  const productTotal = item.quantity * item.totalPriceWithVat;
-                  const unitPrice = currencyPipe(translate('currency')).format(item.unitPrice);
-                  const totalVat = `${currencyPipe(translate('currency')).format(item.totalVat)}`;
-
-                  const deleteProduct = () => {
-                    setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts.filter(p => p.id !== item.id)]);
-                  };
-
-                  const onChangeQuantity = quantity => {
-                    setSelectedProducts(prevSelectedProducts => [
-                      ...prevSelectedProducts.map(p =>
-                        p.id === item.id
-                          ? {
-                              ...p,
-                              quantity: +quantity,
-                            }
-                          : p
-                      ),
-                    ]);
-
-                    setFieldValue('products', selectedProducts);
-                  };
-
-                  return (
-                    <View>
-                      <View style={PRODUCT_ITEM_HEADER_STYLE}>
-                        <Text text={item.description} />
-                        <View style={PRODUCT_ITEM_HEADER_RIGHT_SECTION}>
-                          <Text text={currencyPipe(translate('currency')).format(productTotal)} />
-                          <TouchableOpacity style={DELETE_ICON_STYLE} onPress={deleteProduct}>
-                            <Icon icon='cross' />
-                          </TouchableOpacity>
-                        </View>
-                      </View>
-                      <View>
-                        <Text text={`${translate('invoiceScreen.labels.vat')}: ${totalVat}`} />
-                      </View>
-                      <View style={PRODUCT_ITEM_FOOTER_STYLE}>
-                        <TextField
-                          keyboardType='numeric'
-                          style={PRODUCT_ITEM_QUANTITY_STYLE}
-                          inputStyle={INPUT_TEXT_STYLE}
-                          defaultValue={item.quantity.toString()}
-                          onChangeText={onChangeQuantity}
-                        />
-                        <Text text='x' style={PRODUCT_ITEM_CROSS_STYLE} />
-                        <Text text={unitPrice} />
-                      </View>
-                    </View>
-                  );
-                }}
+                onClear={onClearProducts}
+                renderItem={renderProductItem}
               />
+
               <View style={TOTAL_SECTION_STYLE}>
                 <Text text={`${translate('invoiceScreen.labels.totalSection')}: `} style={SECTION_STYLE} />
                 <Text text={paymentTotal} />
               </View>
+
               <Separator style={{ marginBottom: spacing[4] }} />
+
               <View>
                 <Button tx='invoiceScreen.labels.createInvoice' textStyle={SUBMIT_BUTTON_TEXT_STYLE} onPress={() => handleSubmit()} />
               </View>
