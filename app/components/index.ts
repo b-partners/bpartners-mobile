2917f150d017a7a6b387723394f2915550045d97
lianafinaritra <hei.lianafinaritra@gmail.com>
2022-10-14 17:48:41 +0300

    feat: add dropdowwn storybook

--- app/components/index.ts
+++ app/components/index.ts
@@ -12,4 +12,5 @@ export * from './text-field/text-field';
 export * from './wallpaper/wallpaper';
 export * from './auto-image/auto-image';
 export * from './autocompletion-form-field/autocompletion-form-field';
+export * from './dropdown/dropdown';
 export { Separator } from './separator/Separator';
