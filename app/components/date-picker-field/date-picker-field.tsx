58db15e7d62056c7b29f9a501bc4196fe7b3a897
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-20 16:45:47 +0300

    style: reformat

--- app/components/date-picker-field/date-picker-field.tsx
+++ app/components/date-picker-field/date-picker-field.tsx
@@ -54,7 +54,7 @@ export function DatePickerField(props: DatePickerProps) {
       />
       {validationError && (
         <View>
-          <Text text={validationError}></Text>
+          <Text text={validationError} />
         </View>
       )}
     </View>
