9acc3e7c5b397594b5bf1fdd52c28b4be9e52ed1
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-10-28 17:11:53 +0300

    fix: adding 'from' and 'to' request params

--- app/services/api/transaction-api.ts
+++ app/services/api/transaction-api.ts
@@ -1,9 +1,8 @@
-import { ApiResponse } from 'apisauce';
-
-import { TransactionCategory } from '../../models/entities/transaction-category/transaction-category';
-import { Api } from './api';
-import { getGeneralApiProblem } from './api-problem';
-import { GetTransactionCategoriesResult, GetTransactionsResult } from './api.types';
+import {ApiResponse} from 'apisauce';
+import {TransactionCategory} from '../../models/entities/transaction-category/transaction-category';
+import {Api} from './api';
+import {getGeneralApiProblem} from './api-problem';
+import {GetTransactionCategoriesResult, GetTransactionsResult} from './api.types';
 
 export class TransactionApi {
   private api: Api;
@@ -25,19 +24,35 @@ export class TransactionApi {
         ...item,
         category: item.category && item.category.length ? item.category[0] : null,
       }));
-      return { kind: 'ok', transactions };
+      return {kind: 'ok', transactions};
     } catch (e) {
       __DEV__ && console.tron.log(e.message);
-      throw new Error('bad-data');
+      throw new Error( 'bad-data');
     }
   }
 
-  async getTransactionCategories(accountId: string, unique = true, userDefined = undefined): Promise<GetTransactionCategoriesResult> {
+  async getTransactionCategories(accountId: string,
+                                 unique = true,
+                                 userDefined = undefined,
+                                 from?: string,
+                                 to?: string
+  ): Promise<GetTransactionCategoriesResult> {
+    // by default the date interval will be from the past 6 month to the current date
+    const date = new Date();
+    const DEFAULT_ENDING_DATE = new Date().toISOString().split('T')[0];
+    const DEFAULT_STARTING_DATE = new Date(date.getFullYear(),date.getMonth()-6, date.getDate()).toISOString().split('T')[0];
+
+    to = to || DEFAULT_ENDING_DATE;
+    from = from || DEFAULT_STARTING_DATE;
+
+
     try {
       // make the api call
       const response: ApiResponse<any> = await this.api.apisauce.get(`accounts/${accountId}/transactionCategories`, {
         unique,
         userDefined,
+        from,
+        to,
       });
       // the typical ways to die when calling an api
       if (!response.ok) {
@@ -45,19 +60,19 @@ export class TransactionApi {
         if (problem) return problem;
       }
       const transactionCategories = response.data;
-      return { kind: 'ok', transactionCategories };
+      return {kind: 'ok', transactionCategories};
     } catch (e) {
       __DEV__ && console.tron.log(e.message);
-      throw new Error('bad-data');
+      throw new Error( 'bad-data');
     }
   }
 
   async updateTransactionCategories(
     accountId: string,
     transactionId: string,
-    transactionCategory: TransactionCategory
+    transactionCategory: TransactionCategory,
   ): Promise<GetTransactionCategoriesResult> {
-    const { type, vat } = transactionCategory;
+    const {type, vat} = transactionCategory;
     try {
       // make the api call
       const response: ApiResponse<any> = await this.api.apisauce.post(`accounts/${accountId}/transactions/${transactionId}/transactionCategories`, [
@@ -72,10 +87,10 @@ export class TransactionApi {
         if (problem) return problem;
       }
       const transactionCategories = response.data;
-      return { kind: 'ok', transactionCategories };
+      return {kind: 'ok', transactionCategories};
     } catch (e) {
       __DEV__ && console.tron.log(e.message);
-      return { kind: 'bad-data' };
+      return {kind: 'bad-data'};
     }
   }
 }
