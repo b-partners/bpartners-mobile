4285bcbb9939f02a9b3f9eea2a49e5aafa91c722
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-04 13:44:41 +0300

    refactor: auth flow

---
+++ app/components/bp-drawer/bp-drawer.tsx
@@ -0,0 +1,21 @@
+import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import React from 'react';
+
+import { translate } from '../../i18n';
+import { useStores } from '../../models';
+
+export const BpDrawer: React.FC<DrawerContentComponentProps> = props => {
+  const { authStore } = useStores();
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}></DrawerItemList>
+      <DrawerItem
+        label={translate('logoutScreen.title')}
+        onPress={() => {
+          authStore.logout();
+        }}
+      />
+    </DrawerContentScrollView>
+  );
+};
