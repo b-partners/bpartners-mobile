7476203d44130edbb3fa7c5f39bd6e59eb88be41
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-15 11:22:40 +0300

    fix: fetching transaction categories

--- app/screens/home/home-screen.tsx
+++ app/screens/home/home-screen.tsx
@@ -30,6 +30,7 @@ export const HomeScreen: FC<DrawerScreenProps<NavigatorParamList, 'home'>> = obs
 
   useEffect(() => {
     transactionStore.getTransactions();
+    transactionStore.getTransactionCategories();
   }, []);
 
   return (
