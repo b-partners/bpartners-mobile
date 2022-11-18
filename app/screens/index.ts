7c03f775f0dea576447f799f452ec171c7ef6052
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-11-18 16:33:36 +0300

    fix: circular dependency

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,7 +1,3 @@
-import { TextStyle, ViewStyle } from 'react-native';
-
-import { color, spacing } from '../theme';
-
 export * from './welcome/welcome-screen';
 export * from './transaction/transaction-list-screen';
 export * from './error/error-boundary';
@@ -11,7 +7,6 @@ export * from './payment-initiation/payment-initiation-screen';
 export * from './profile/profile-screen';
 export * from './home/home-screen';
 export { TRANSACTION_BUTTONS_STYLE } from './home/styles';
-export { FOOTER_STYLE } from './home/styles';
 export { BALANCE_TEXT_STYLE } from './home/styles';
 export { CHART_BUTTON_MARGIN_STYLE } from './home/styles';
 export { CHART_BUTTON_STYLE } from './home/styles';
@@ -32,22 +27,3 @@ export { HomeNavbar } from './home/components/home-navbar';
 export { TransactionSummary } from './home/components/transaction-summary';
 export { HomeLatestTransaction } from './home/components/home-latest-transaction';
 export { HomeFooter } from './home/components/home-footer';
-
-export const FULL: ViewStyle = {
-  flex: 1,
-};
-export const CONTAINER: ViewStyle = {
-  backgroundColor: color.transparent,
-};
-export const HEADER: TextStyle = {
-  paddingBottom: spacing[5] - 1,
-  paddingHorizontal: spacing[4],
-  paddingTop: spacing[3],
-};
-export const HEADER_TITLE: TextStyle = {
-  fontSize: 12,
-  fontWeight: 'bold',
-  letterSpacing: 1.5,
-  lineHeight: 15,
-  textAlign: 'center',
-};
---
+++ app/screens/payment-initiation/style.ts
@@ -0,0 +1,22 @@
+import { TextStyle, ViewStyle } from 'react-native';
+
+import { color, spacing } from '../../theme';
+
+export const CONTAINER: ViewStyle = {
+  backgroundColor: color.transparent,
+};
+export const FULL: ViewStyle = {
+  flex: 1,
+};
+export const HEADER: TextStyle = {
+  paddingBottom: spacing[5] - 1,
+  paddingHorizontal: spacing[4],
+  paddingTop: spacing[3],
+};
+export const HEADER_TITLE: TextStyle = {
+  fontSize: 12,
+  fontWeight: 'bold',
+  letterSpacing: 1.5,
+  lineHeight: 15,
+  textAlign: 'center',
+};
