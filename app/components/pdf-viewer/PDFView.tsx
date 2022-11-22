735dff80e12dc53614ca5cb35c8228295c48aef5
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-11-22 14:39:34 +0300

    feat: adding component for displaying pdf file

---
+++ app/components/pdf-viewer/PDFView.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Dimensions, ViewStyle } from "react-native";
+import Pdf from "react-native-pdf";
+
+
+type TSource = { uri: string, cache?: boolean } | number;
+
+interface IPDFView {
+  source: TSource;
+  onLoadProgress?: (percent: number) => void;
+  onLoadComplete?: (numberOfPages?: number, path?: string) => void;
+  onError?: (error) => any;
+  onPageChanged?: (page: number, numberOfPages: number) => void;
+  style?: ViewStyle;
+}
+
+export function PDFView(props: IPDFView) {
+  const { source, onPageChanged, onLoadComplete, onError, style, onLoadProgress } = props;
+
+  return (
+    <Pdf
+      enablePaging={true}
+      trustAllCerts={false}
+      source={source}
+      onLoadComplete={onLoadComplete}
+      onPageChanged={onPageChanged}
+      onError={onError}
+      style={[PDF_STYLE, style]}
+      onLoadProgress={onLoadProgress}
+    />
+  );
+}
+
+const PDF_STYLE: ViewStyle = {
+  flex: 1,
+  width: Dimensions.get("window").width,
+  height: Dimensions.get("window").height
+};
+
