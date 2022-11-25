594823246da14792cfe6bff72deb836dc611c36c
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-11-25 12:05:47 +0300

    test: update legal-file-store.test.ts

--- app/models/stores/legal-file-store/legal-file-store.test.ts
+++ app/models/stores/legal-file-store/legal-file-store.test.ts
@@ -1,7 +1,10 @@
-import {LegalFileStoreModel} from './legal-file-store';
+import { LegalFileStoreModel } from './legal-file-store';
 
 test('can be created', () => {
-  const instance = LegalFileStoreModel.create({name: "legalFile", id: "id1", approvalDatetime: "", fileUrl: ""});
-
+  const instance = LegalFileStoreModel.create([
+    { name: 'legalFile', id: 'id1', approvalDatetime: '', fileUrl: '' },
+    { name: null, id: null, approvalDatetime: null, fileUrl: null },
+    { name: undefined, id: undefined, approvalDatetime: undefined, fileUrl: undefined },
+  ]);
   expect(instance).toBeTruthy();
 });
