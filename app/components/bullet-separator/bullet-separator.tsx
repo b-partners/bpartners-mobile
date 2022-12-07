9fecafed2b1ff3420a1d3afcd75d730ad6bedc98
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-07 15:52:59 +0300

    chore: adding text translation for quotation creation button

--- app/components/bullet-separator/bullet-separator.tsx
+++ app/components/bullet-separator/bullet-separator.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { TextStyle, View, ViewStyle } from 'react-native';
 
 import { spacing } from '../../theme';
+import { palette } from '../../theme/palette';
 import { Text } from '../text/text';
 
 const CONTAINER_STYLE: ViewStyle = { marginHorizontal: spacing[1] };
-const BULLET_STYLE: TextStyle = { fontSize: 8 };
+const BULLET_STYLE: TextStyle = { fontSize: 8, color: palette.greyDarker };
 
 export function BulletSeparator() {
   return (
