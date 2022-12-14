5abf510b847bf9fe26ed064f1aa5ec322447bb7f
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-12-14 13:35:30 +0300

    style: reformat

--- app/models/entities/legal-file/legal-file.test.ts
+++ app/models/entities/legal-file/legal-file.test.ts
@@ -1,11 +1,11 @@
-import {LegalFileModel} from './legal-file';
+import { LegalFileModel } from './legal-file';
 
 test('can be created', () => {
   const instance = LegalFileModel.create({
-    id: "id1",
-    name: "legalFile1",
-    approvalDatetime: "2022-01-16",
-    fileUrl: "SOME_URL"
+    id: 'id1',
+    name: 'legalFile1',
+    approvalDatetime: '2022-01-16',
+    fileUrl: 'SOME_URL',
   });
 
   expect(instance).toBeTruthy();
