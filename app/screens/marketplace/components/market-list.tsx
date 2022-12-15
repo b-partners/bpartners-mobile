2b4faa9768f4af44b3f76a08335ab33c2da7c6bc
lianafinaritra <hei.lianafinaritra@gmail.com>
2022-12-15 14:55:12 +0300

    feat: add marketpalce screen

---
+++ app/screens/marketplace/components/market-list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { MARKET_LIST_STYLE } from '../styles';
+
+export function MarketList() {
+    return (
+        <View style={MARKET_LIST_STYLE}>
+        </View>
+    );
+}
