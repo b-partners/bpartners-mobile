fd4ded088c2f7410321ffbfda2c12c04cc84e417
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-01-12 17:18:11 +0300

    feat: implement marketplace UI interface

--- app/screens/marketplace/components/market-header.tsx
+++ app/screens/marketplace/components/market-header.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import Left from 'react-native-vector-icons/Entypo';
 
+import { AutoImage } from '../../../components';
 import { translate } from '../../../i18n';
-import { HEADER_STYLE, LEFT_STYLE, TEXT_HEADER_CONTAINER_STYLE, TEXT_HEADER_STYLE } from '../styles';
+import { HEADER_STYLE, LEFT_STYLE, TEXT_HEADER_CONTAINER_STYLE, TEXT_HEADER_STYLE, WAVE_STYLE } from '../styles';
 
 export function MarketHeader(props: any) {
   const { onPress } = props;
   return (
     <View style={HEADER_STYLE}>
+      <AutoImage source={require('./wave.webp')} style={WAVE_STYLE} resizeMethod='resize' />
       <TouchableOpacity style={LEFT_STYLE} onPress={onPress}>
-        <Left name='chevron-thin-left' size={25} color='#000' />
+        <Left name='chevron-thin-left' size={25} color='#ffffff' />
       </TouchableOpacity>
       <View style={TEXT_HEADER_CONTAINER_STYLE}>
         <Text style={TEXT_HEADER_STYLE}>{translate('marketPlaceScreen.header')}</Text>
