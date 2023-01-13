62755c802b1f5d4e1aab4c5eabba6ac6424924c0
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-01-13 10:47:09 +0300

    fix: quotation create button position

--- app/screens/invoice-quotation/invoices-screen.tsx
+++ app/screens/invoice-quotation/invoices-screen.tsx
@@ -1,28 +1,33 @@
-import { MaterialTopTabScreenProps } from '@react-navigation/material-top-tabs';
-import { observer } from 'mobx-react-lite';
-import React, { FC } from 'react';
-import { SectionList, View } from 'react-native';
+import { MaterialTopTabScreenProps } from "@react-navigation/material-top-tabs";
+import { observer } from "mobx-react-lite";
+import React, { FC } from "react";
+import { SectionList, View } from "react-native";
 
-import { BUTTON_TEXT_STYLE } from '..';
-import { Button, Screen, Separator, Text } from '../../components';
-import { MenuItem } from '../../components/menu/menu';
-import env from '../../config/env';
-import { translate } from '../../i18n';
-import { useStores } from '../../models';
-import { Invoice as IInvoice, InvoiceStatus } from '../../models/entities/invoice/invoice';
-import { NavigatorParamList } from '../../navigators';
-import { spacing } from '../../theme';
-import { palette } from '../../theme/palette';
-import { capitalizeFirstLetter } from '../../utils/capitalizeFirstLetter';
-import { fetchBinaryFile } from '../../utils/fetch-binary-file';
-import { showMessage } from '../../utils/snackbar';
-import { ErrorBoundary } from '../error/error-boundary';
-import { BUTTON_STYLE } from '../invoice-form/styles';
-import { Invoice } from './components/invoice';
-import { CONTAINER, FOOTER_COMPONENT_STYLE, FULL, SECTION_HEADER_TEXT_STYLE, SEPARATOR_STYLE } from './styles';
-import { sectionInvoicesByMonth } from './utils/section-quotation-by-month';
+import { Button, Screen, Separator, Text } from "../../components";
+import { MenuItem } from "../../components/menu/menu";
+import env from "../../config/env";
+import { translate } from "../../i18n";
+import { useStores } from "../../models";
+import { Invoice as IInvoice, InvoiceStatus } from "../../models/entities/invoice/invoice";
+import { NavigatorParamList } from "../../navigators";
+import { spacing } from "../../theme";
+import { palette } from "../../theme/palette";
+import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
+import { fetchBinaryFile } from "../../utils/fetch-binary-file";
+import { showMessage } from "../../utils/snackbar";
+import { ErrorBoundary } from "../error/error-boundary";
+import { Invoice } from "./components/invoice";
+import {
+  BUTTON_STYLE, BUTTON_TEXT_STYLE,
+  CONTAINER,
+  FOOTER_COMPONENT_STYLE,
+  FULL,
+  SECTION_HEADER_TEXT_STYLE,
+  SEPARATOR_STYLE
+} from "./styles";
+import { sectionInvoicesByMonth } from "./utils/section-quotation-by-month";
 
-export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'invoices'>> = observer(function InvoicesScreen({ navigation }) {
+export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, "invoices">> = observer(function InvoicesScreen({ navigation }) {
   const { invoiceStore, authStore } = useStores();
   const { invoices, loading } = invoiceStore;
   const { currentAccount, accessToken } = authStore;
@@ -32,18 +37,18 @@ export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'i
   };
 
   const sectionedQuotations = sectionInvoicesByMonth(invoices);
-  const items: MenuItem[] = [{ id: 'downloadInvoice', title: translate('invoiceScreen.menu.downloadInvoice') }];
+  const items: MenuItem[] = [{ id: "downloadInvoice", title: translate("invoiceScreen.menu.downloadInvoice") }];
 
   const downloadInvoice = async (url: string, fileName: string) => {
     try {
-      showMessage(translate('invoiceScreen.messages.downloadingInvoice'));
+      showMessage(translate("invoiceScreen.messages.downloadingInvoice"));
       await fetchBinaryFile({
         url,
-        fileName,
+        fileName
       });
-      showMessage(translate('invoiceScreen.messages.invoiceSuccessfullyDownload'));
+      showMessage(translate("invoiceScreen.messages.invoiceSuccessfullyDownload"));
     } catch (e) {
-      showMessage(translate('invoiceScreen.messages.downloadingInvoiceFailed'));
+      showMessage(translate("invoiceScreen.messages.downloadingInvoiceFailed"));
       __DEV__ && console.tron.log(e);
       throw e;
     }
@@ -53,30 +58,33 @@ export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'i
     <ErrorBoundary catchErrors='always'>
       <View testID='PaymentInitiationScreen' style={FULL}>
         <Screen style={CONTAINER} preset='fixed' backgroundColor={palette.white}>
-          <SectionList<IInvoice>
-            style={{ marginHorizontal: spacing[4] }}
-            sections={[...sectionedQuotations]}
-            renderItem={({ item }) => (
-              <Invoice
-                item={item}
-                menuItems={items}
-                menuAction={{
-                  downloadInvoice: () =>
-                    downloadInvoice(`${env.apiBaseUrl}/accounts/${currentAccount.id}/files/${item.fileId}/raw?accessToken=${accessToken}`, `${item.ref}.pdf`),
-                }}
-              />
-            )}
-            keyExtractor={item => item.id}
-            renderSectionHeader={({ section: { title } }) => <Text style={SECTION_HEADER_TEXT_STYLE}>{capitalizeFirstLetter(title)}</Text>}
-            refreshing={loading}
-            onRefresh={handleRefresh}
-            progressViewOffset={100}
-            stickySectionHeadersEnabled={true}
-            ItemSeparatorComponent={() => <Separator style={SEPARATOR_STYLE} />}
-            renderSectionFooter={() => <View style={FOOTER_COMPONENT_STYLE} />}
-          />
-
-          <Button tx='quotationScreen.createQuotation' style={BUTTON_STYLE} textStyle={BUTTON_TEXT_STYLE} onPress={() => navigation.navigate('invoiceForm')} />
+          <View>
+            <SectionList<IInvoice>
+              style={{ marginHorizontal: spacing[4] }}
+              sections={[...sectionedQuotations]}
+              renderItem={({ item }) => (
+                <Invoice
+                  item={item}
+                  menuItems={items}
+                  menuAction={{
+                    downloadInvoice: () =>
+                      downloadInvoice(`${env.apiBaseUrl}/accounts/${currentAccount.id}/files/${item.fileId}/raw?accessToken=${accessToken}`, `${item.ref}.pdf`)
+                  }}
+                />
+              )}
+              keyExtractor={item => item.id}
+              renderSectionHeader={({ section: { title } }) => <Text
+                style={SECTION_HEADER_TEXT_STYLE}>{capitalizeFirstLetter(title)}</Text>}
+              refreshing={loading}
+              onRefresh={handleRefresh}
+              progressViewOffset={100}
+              stickySectionHeadersEnabled={true}
+              ItemSeparatorComponent={() => <Separator style={SEPARATOR_STYLE} />}
+              renderSectionFooter={() => <View style={FOOTER_COMPONENT_STYLE} />}
+            />
+          </View>
+          <Button tx='quotationScreen.createQuotation' style={BUTTON_STYLE} textStyle={BUTTON_TEXT_STYLE}
+                  onPress={() => navigation.navigate("invoiceForm")} />
         </Screen>
       </View>
     </ErrorBoundary>
