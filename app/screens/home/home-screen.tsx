c3896983f1a74490f7efffd8ca026f68bb86d46a
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2023-01-26 23:33:27 +0300

    feat: adding header

--- app/screens/home/home-screen.tsx
+++ app/screens/home/home-screen.tsx
@@ -1,23 +1,52 @@
-import { DrawerScreenProps } from '@react-navigation/drawer';
+import { DrawerNavigationProp, DrawerScreenProps } from '@react-navigation/drawer';
+import { DrawerActions } from '@react-navigation/native';
 import { observer } from 'mobx-react-lite';
 import React, { FC, useEffect } from 'react';
-import { View } from 'react-native';
+import { ImageStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 
-import { GradientBackground, Screen } from '../../components';
+import { AutoImage, Icon, Screen } from '../../components';
+import { HeaderWithBalance } from '../../components/header-with-balance/header-with-balance';
+import env from '../../config/env';
 import { useStores } from '../../models';
 import { NavigatorParamList } from '../../navigators';
-import { color } from '../../theme';
+import { color, spacing } from '../../theme';
 import { ErrorBoundary } from '../error/error-boundary';
-import { Balance } from './components/balance';
-import { HomeHeader } from './components/home-header';
 import { HomeLatestTransaction } from './components/home-latest-transaction';
 import { TransactionSummary } from './components/transaction-summary';
 import { FULL } from './styles';
+
+const BULLET_STYLE: ViewStyle = { position: 'absolute', top: -5, left: -5, zIndex: 1 };
+
+export const Menu: FC<{ navigation: DrawerNavigationProp<any> }> = observer(({ navigation }) => {
+  return (
+    <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())} testID='menuContainer'>
+      <View>
+        <View style={BULLET_STYLE}>
+          <Icon icon='redBullet' />
+        </View>
+        <Icon icon='whiteMenu' />
+      </View>
+    </TouchableOpacity>
+  );
+});
+
+export const Logo: FC<{ uri: string }> = observer(({ uri }) => {
+  const LOGO_STYLE: ImageStyle = { width: '100%', height: '100%' };
+
+  return (
+    <View style={{ height: 50, width: 50 }}>
+      <AutoImage source={{ uri }} style={LOGO_STYLE} resizeMethod='resize' resizeMode='stretch' />
+    </View>
+  );
+});
 
 export const HomeScreen: FC<DrawerScreenProps<NavigatorParamList, 'home'>> = observer(({ navigation }) => {
   const { transactionStore, legalFilesStore, authStore } = useStores();
 
   const { availableBalance } = authStore.currentAccount;
+  const { currentAccount, currentUser, accessToken } = authStore;
+
+  const uri = `${env.apiBaseUrl}/accounts/${currentAccount.id}/files/${currentUser.logoFileId}/raw?accessToken=${accessToken}&fileType=LOGO`;
 
   const { transactions, loadingTransactions, currentMonthSummary } = transactionStore;
 
@@ -36,12 +65,12 @@ export const HomeScreen: FC<DrawerScreenProps<NavigatorParamList, 'home'>> = obs
   return (
     <ErrorBoundary catchErrors='always'>
       <View testID='homeScreen' style={FULL}>
-        <GradientBackground colors={['#422443', '#281b34']} />
         <Screen preset='auto' backgroundColor={color.transparent}>
-          <HomeHeader />
-          <Balance balance={availableBalance} />
-          <TransactionSummary summary={currentMonthSummary} />
-          <HomeLatestTransaction transactions={transactions} onPress={() => navigation.navigate('transactionList')} loading={loadingTransactions} />
+          <HeaderWithBalance balance={availableBalance} left={<Logo uri={uri} />} right={<Menu navigation={navigation} />} />
+          <View style={{ padding: spacing[3] }}>
+            <TransactionSummary summary={currentMonthSummary} />
+            <HomeLatestTransaction transactions={transactions} onPress={() => navigation.navigate('transactionList')} loading={loadingTransactions} />
+          </View>
         </Screen>
       </View>
     </ErrorBoundary>
