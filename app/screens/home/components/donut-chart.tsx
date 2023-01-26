092f93648fa639c0fe1224d7e50ab7f827c22b94
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2023-01-26 23:35:44 +0300

    Revert "chore: deleting unused file"
    This reverts commit 071c9ddfb7db8050b0e613db2245f9532c281ff7.

---
+++ app/screens/home/components/donut-chart.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { PieChart } from 'react-native-gifted-charts';
+
+import { Icon, Text } from '../../../components';
+import { TxKeyPath, translate } from '../../../i18n';
+import { TransactionSummary as ITransactionSummary } from '../../../models/entities/transaction-summary/transaction-summary';
+import { spacing } from '../../../theme';
+import { palette } from '../../../theme/palette';
+
+const CHART_CONTAINER: ViewStyle = { display: 'flex', flexDirection: 'row', alignItems: 'flex-end' };
+const LABELS_SECTION: ViewStyle = { flex: 1 };
+const CHART_SECTION: ViewStyle = { flex: 2, marginLeft: spacing[4] };
+const LABEL_CONTAINER_STYLE: ViewStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: spacing[2],
+};
+const LABEL_COLOR_STYLE = (color: string): ViewStyle => ({
+  backgroundColor: color,
+  height: 10,
+  width: 10,
+  marginRight: spacing[1],
+});
+
+const COLORS = [
+  palette.midnightGreen,
+  palette.purpleNavy,
+  palette.mulberry,
+  palette.pastelRed,
+  palette.cheese,
+  palette.saffron,
+  palette.japaneseLaurel,
+  palette.green,
+  palette.deepPurple,
+];
+
+const FILTERED_KEYS = ['id', 'month', 'updatedAt'];
+
+type DonutChartProps = {
+  summary: ITransactionSummary;
+};
+
+const TITLE_CONTAINER_STYLE: ViewStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  marginBottom: spacing[8],
+};
+
+const SUMMARY_TITLE_STYLE: TextStyle = { textTransform: 'uppercase' };
+
+export const DonutChart: React.FC<DonutChartProps> = props => {
+  const { summary } = props;
+
+  return (
+    <View>
+      <View style={TITLE_CONTAINER_STYLE}>
+        <Text tx='homeScreen.summary.title' style={SUMMARY_TITLE_STYLE} />
+        <TouchableOpacity>
+          <Icon icon='settings' />
+        </TouchableOpacity>
+      </View>
+      <View style={CHART_CONTAINER}>
+        <View style={LABELS_SECTION}>
+          {Object.keys(summary)
+            .filter(key => !FILTERED_KEYS.includes(key))
+            .map((item, i) => {
+              return (
+                <View style={LABEL_CONTAINER_STYLE} key={item}>
+                  <View style={LABEL_COLOR_STYLE(COLORS[i])} />
+                  <Text text={translate(`homeScreen.summary.${item}` as TxKeyPath)} />
+                </View>
+              );
+            })}
+        </View>
+        <View style={CHART_SECTION}>
+          <PieChart
+            donut
+            semiCircle
+            radius={110}
+            labelsPosition='outward'
+            data={[
+              ...Object.keys(summary)
+                .filter(key => !FILTERED_KEYS.includes(key))
+                .map((key, i) => {
+                  return {
+                    value: summary[key],
+                    text: translate(`homeScreen.summary.${key}` as TxKeyPath),
+                    color: COLORS[i],
+                  };
+                }),
+            ]}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
