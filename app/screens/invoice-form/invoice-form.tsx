d99903c4abd54f6a7aac474f5838a5502f461567
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-02-02 10:39:26 +0300

    feat: limitedValidation period

--- app/screens/invoice-form/invoice-form.tsx
+++ app/screens/invoice-form/invoice-form.tsx
@@ -1,22 +1,22 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Formik } from 'formik';
-import React, { useState } from 'react';
-import { FlatList, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
-import uuid from 'react-native-uuid';
-import * as yup from 'yup';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Formik } from "formik";
+import React, { useState } from "react";
+import { FlatList, TextStyle, TouchableOpacity, View, ViewStyle } from "react-native";
+import uuid from "react-native-uuid";
+import * as yup from "yup";
 
-import { Button, Icon, Separator, Switch, Text } from '../../components';
-import { DatePickerField } from '../../components/date-picker-field/date-picker-field';
-import { translate } from '../../i18n';
-import { Customer } from '../../models/entities/customer/customer';
-import { Invoice, InvoiceSnapshotIn, InvoiceStatus } from '../../models/entities/invoice/invoice';
-import { Product } from '../../models/entities/product/product';
-import { color, spacing } from '../../theme';
-import { palette } from '../../theme/palette';
-import CustomerListSelectionModal from './components/customer-selection-form/customer-list-selection-modal';
-import EditableTextField from './components/editable-text-field';
-import GridHeaderContent from './components/grid-header-content';
-import ProductCardItem from './components/product-card-item';
+import { Button, Icon, Separator, Switch, Text } from "../../components";
+import { DatePickerField } from "../../components/date-picker-field/date-picker-field";
+import { translate } from "../../i18n";
+import { Customer } from "../../models/entities/customer/customer";
+import { Invoice, InvoiceSnapshotIn, InvoiceStatus } from "../../models/entities/invoice/invoice";
+import { Product } from "../../models/entities/product/product";
+import { color, spacing } from "../../theme";
+import { palette } from "../../theme/palette";
+import CustomerListSelectionModal from "./components/customer-selection-form/customer-list-selection-modal";
+import EditableTextField from "./components/editable-text-field";
+import GridHeaderContent from "./components/grid-header-content";
+import ProductCardItem from "./components/product-card-item";
 
 type InvoiceFormProps = {
   invoice: Partial<InvoiceSnapshotIn>;
@@ -26,18 +26,18 @@ type InvoiceFormProps = {
 };
 
 const FULL: ViewStyle = { flex: 1 };
-const FLEX_ROW: ViewStyle = { ...FULL, flexDirection: 'row' };
+const FLEX_ROW: ViewStyle = { ...FULL, flexDirection: "row" };
 
 const SUBMIT_BUTTON_TEXT_STYLE: TextStyle = {
   fontSize: 14,
-  fontWeight: '700'
+  fontWeight: "700"
 };
 
 const EDITABLE_TF_CONTAINER: ViewStyle = { borderWidth: 0.5, borderColor: palette.lighterGrey, flex: 1 };
 const BUTTON_FILL_STYLE: ViewStyle = {
   flex: 1,
   backgroundColor: color.primary,
-  marginHorizontal: '5%',
+  marginHorizontal: "5%",
   borderRadius: 40,
   paddingVertical: spacing[3],
   paddingHorizontal: spacing[2],
@@ -45,13 +45,13 @@ const BUTTON_FILL_STYLE: ViewStyle = {
 };
 const HEADER_RIGHT_ROW: ViewStyle = { ...EDITABLE_TF_CONTAINER, flex: 1.5 };
 
-const LABEL_STYLE: TextStyle = { color: palette.greyDarker, fontSize: 14, fontWeight: '700' };
+const LABEL_STYLE: TextStyle = { color: palette.greyDarker, fontSize: 14, fontWeight: "700" };
 
 const CENTERED_FLEX: ViewStyle = {
   paddingVertical: spacing[2],
   paddingRight: spacing[2],
-  justifyContent: 'center',
-  alignItems: 'center',
+  justifyContent: "center",
+  alignItems: "center"
 };
 
 const SAVE_ICON_CONTAINER: ViewStyle = {
@@ -60,21 +60,21 @@ const SAVE_ICON_CONTAINER: ViewStyle = {
   width: 45,
   height: 45,
   borderRadius: 25,
-  justifyContent: 'center',
-  alignItems: 'center',
+  justifyContent: "center",
+  alignItems: "center"
 };
 
 const DATE_PICKER_FIELD_CONTAINER: ViewStyle = { ...EDITABLE_TF_CONTAINER, padding: spacing[4] };
 
 const SEPARATOR_STYLE: ViewStyle = { borderColor: palette.lighterGrey };
 
-const FLEX_WRAP: ViewStyle = { flex: 1, flexWrap: 'wrap' };
+const FLEX_WRAP: ViewStyle = { flex: 1, flexWrap: "wrap" };
 
-const USER_SELECT_ICON: ViewStyle = { justifyContent: 'center', marginRight: spacing[2] };
+const USER_SELECT_ICON: ViewStyle = { justifyContent: "center", marginRight: spacing[2] };
 
-const CLIENT_SELECTION_FORM_STYLE: ViewStyle = { justifyContent: 'space-between', flexDirection: 'row' };
+const CLIENT_SELECTION_FORM_STYLE: ViewStyle = { justifyContent: "space-between", flexDirection: "row" };
 
-const DATE_PICKER_STYLE: TextStyle = { color: palette.textClassicColor, fontSize: 18, fontWeight: '700' };
+const DATE_PICKER_STYLE: TextStyle = { color: palette.textClassicColor, fontSize: 18, fontWeight: "700" };
 
 const ADD_PRODUCT_BUTTON_STYLE: ViewStyle = {
   backgroundColor: palette.white,
@@ -84,7 +84,8 @@ const ADD_PRODUCT_BUTTON_STYLE: ViewStyle = {
   flexDirection: "row",
   marginHorizontal: spacing[4],
   marginVertical: spacing[4],
-  borderRadius: 40,
+  marginBottom: 20,
+  borderRadius: 40
 };
 
 const ADD_BUTTON_TEXT_STYLE: TextStyle = {
@@ -92,15 +93,24 @@ const ADD_BUTTON_TEXT_STYLE: TextStyle = {
   marginLeft: spacing[2]
 };
 
+const  VALIDITY_PERIOD_TEXT_STYLE: TextStyle = {
+  justifyContent: "space-between",
+  alignItems: "center",
+  paddingHorizontal: spacing[4],
+  paddingVertical: spacing[3],
+  borderTopWidth: 1
+};
+
 export function InvoiceForm(props: InvoiceFormProps) {
   const { onSaveInvoice, customers } = props;
   const [showUserListModal, setShowUserListModal] = useState(false);
+  const [limitedPeriodOfValidity, setLimitedPeriodOfValidity] = useState(false);
 
   const validate = values => {
     const errors: Partial<Record<keyof Invoice, string>> = {};
 
     if (values.sendingDate > values.toPayAt) {
-      errors.sendingDate = translate('invoiceScreen.errors.sendingDateLaterThanToPayAt');
+      errors.sendingDate = translate("invoiceScreen.errors.sendingDateLaterThanToPayAt");
     }
     return errors;
   };
@@ -109,25 +119,25 @@ export function InvoiceForm(props: InvoiceFormProps) {
   const [showMyMailAddress, setShowMyMailAddress] = useState<boolean>(false);
   const productInitialValue = {
     id: uuid.v4().toString(),
-    description: '',
+    description: "",
     totalPriceWithVat: 0,
     vatPercent: 0,
-    unitPrice: '0',
-    quantity: '1',
+    unitPrice: "0",
+    quantity: "1",
     totalVat: 0,
-    title: '',
-    tva: '0',
+    title: "",
+    tva: "0"
   };
   const [initialValues, setInitialValues] = useState({
     id: uuid.v4().toString(),
-    ref: '',
-    title: '',
+    ref: "",
+    title: "",
     comment: null,
     sendingDate: new Date(),
     toPayAt: new Date(),
     customer: customers[0],
     products: [productInitialValue],
-    status: InvoiceStatus.DRAFT,
+    status: InvoiceStatus.DRAFT
   });
 
   const validationSchema = yup.object().shape({
@@ -139,9 +149,9 @@ export function InvoiceForm(props: InvoiceFormProps) {
         description: yup.string().notRequired(),
         unitPrice: yup.number().min(0).required(),
         quantity: yup.number().min(1).required(),
-        tva: yup.number().min(0).max(100).required(),
+        tva: yup.number().min(0).max(100).required()
       })
-    ),
+    )
   });
   return (
     <View>
@@ -157,8 +167,8 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 description: item.description,
                 unitPrice: item.unitPrice,
                 vatPercent: item.tva,
-                quantity: item.quantity,
-              })),
+                quantity: item.quantity
+              }))
             });
           } catch (e) {
             __DEV__ && console.tron.log(e);
@@ -170,7 +180,7 @@ export function InvoiceForm(props: InvoiceFormProps) {
         {({ handleSubmit, values, setFieldValue }) => {
           const handleProductItemRemove = product => {
             setFieldValue(
-              'products',
+              "products",
               values.products.filter((p: Product) => p.id !== product.id)
             );
           };
@@ -185,17 +195,17 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 <View style={FLEX_ROW}>
                   <View style={FULL}>
                     <EditableTextField
-                      titleTx={'invoiceFormScreen.invoiceForm.quotationTitle'}
-                      formName={'title'}
-                      placeholderTx={'invoiceFormScreen.invoiceForm.quotationTitlePlaceholder'}
+                      titleTx={"invoiceFormScreen.invoiceForm.quotationTitle"}
+                      formName={"title"}
+                      placeholderTx={"invoiceFormScreen.invoiceForm.quotationTitlePlaceholder"}
                       containerStyle={{ ...HEADER_RIGHT_ROW }}
                     />
                   </View>
                   <View style={FULL}>
                     <EditableTextField
-                      titleTx={'invoiceFormScreen.invoiceForm.quotationNumber'}
-                      formName={'ref'}
-                      placeholderTx={'invoiceFormScreen.invoiceForm.quotationNumber'}
+                      titleTx={"invoiceFormScreen.invoiceForm.quotationNumber"}
+                      formName={"ref"}
+                      placeholderTx={"invoiceFormScreen.invoiceForm.quotationNumber"}
                       containerStyle={{ ...EDITABLE_TF_CONTAINER }}
                     />
                   </View>
@@ -209,11 +219,11 @@ export function InvoiceForm(props: InvoiceFormProps) {
                   >
                     <>
                       <GridHeaderContent
-                        headerTx={'invoiceFormScreen.customerSelectionForm.title'}
-                        bodyText={values.customer ? values.customer.name : 'le nom du client'}
+                        headerTx={"invoiceFormScreen.customerSelectionForm.title"}
+                        bodyText={values.customer ? values.customer.name : "le nom du client"}
                       />
                       <View style={USER_SELECT_ICON}>
-                        <MaterialCommunityIcons name={'chevron-down'} size={25} />
+                        <MaterialCommunityIcons name={"chevron-down"} size={25} />
                       </View>
                     </>
                   </TouchableOpacity>
@@ -223,18 +233,18 @@ export function InvoiceForm(props: InvoiceFormProps) {
                     onRequestClose={() => setShowUserListModal(false)}
                     onValidateChoice={customer => {
                       setShowUserListModal(false);
-                      setFieldValue('customer', customer);
+                      setFieldValue("customer", customer);
                     }}
                   />
                   <View style={{ ...DATE_PICKER_FIELD_CONTAINER, ...FULL }}>
                     <DatePickerField
                       value={initialValues.sendingDate}
                       onDateChange={date => setInitialValues({ ...initialValues, sendingDate: date })}
-                      labelTx={'invoiceFormScreen.invoiceForm.issueDate'}
+                      labelTx={"invoiceFormScreen.invoiceForm.issueDate"}
                       labelStyle={LABEL_STYLE}
                       isButtonPreset={false}
                       textStyle={DATE_PICKER_STYLE}
-                      dateSeparator={'/'}
+                      dateSeparator={"/"}
                     />
                   </View>
                 </View>
@@ -244,55 +254,68 @@ export function InvoiceForm(props: InvoiceFormProps) {
                 <FlatList<Product>
                   data={[...values.products]}
                   renderItem={({ item, index }) => (
-                    <ProductCardItem item={{ ...item }} onRemove={handleProductItemRemove} onChange={handleProductItemFieldChange(index)} />
+                    <ProductCardItem item={{ ...item }} onRemove={handleProductItemRemove}
+                                     onChange={handleProductItemFieldChange(index)} />
                   )}
                 />
                 <Button
-                  onPress={() => setFieldValue('products', [...values.products, { ...productInitialValue, id: uuid.v4().toString() }])}
+                  onPress={() => setFieldValue("products", [...values.products, {
+                    ...productInitialValue,
+                    id: uuid.v4().toString()
+                  }])}
                   style={ADD_PRODUCT_BUTTON_STYLE}
                 >
                   <>
                     <MaterialCommunityIcons name='plus' size={25} color={color.primary} />
-                    <Text tx={'invoiceFormScreen.invoiceForm.addItem'} style={ADD_BUTTON_TEXT_STYLE}/>
+                    <Text tx={"invoiceFormScreen.invoiceForm.addItem"} style={ADD_BUTTON_TEXT_STYLE} />
                   </>
                 </Button>
               </View>
-
               <View>
-                <View>
-                  <Text text={'Durée de validité limité'} />
+                <View style={[FLEX_ROW, CENTERED_FLEX, SEPARATOR_STYLE, VALIDITY_PERIOD_TEXT_STYLE]}>
+                    <Text tx={"invoiceFormScreen.invoiceForm.limitedValidityPeriod"}
+                          style={[FULL, { color: palette.textClassicColor, fontSize: 14, fontWeight: "400" }]} />
+                  <Switch style={FULL} value={limitedPeriodOfValidity}
+                          onToggle={()=>setLimitedPeriodOfValidity(!limitedPeriodOfValidity)} />
                 </View>
                 <View style={FLEX_ROW}>
                   <EditableTextField
-                    titleTx={'invoiceFormScreen.invoiceForm.paymentDelay'}
-                    formName={'title'}
-                    placeholderTx={'invoiceFormScreen.invoiceForm.paymentDelayPlaceholder'}
+                    titleTx={"invoiceFormScreen.invoiceForm.paymentDelay"}
+                    formName={"title"}
+                    placeholderTx={"invoiceFormScreen.invoiceForm.paymentDelayPlaceholder"}
                     containerStyle={EDITABLE_TF_CONTAINER}
                   />
                   <EditableTextField
-                    titleTx={'invoiceFormScreen.invoiceForm.downPayment'}
-                    formName={'acompte'}
-                    placeholder={'Taper '}
+                    titleTx={"invoiceFormScreen.invoiceForm.downPayment"}
+                    formName={"acompte"}
+                    placeholder={"Taper "}
                     containerStyle={EDITABLE_TF_CONTAINER}
-                    defaultValue={'10'}
-                    suffix={' %'}
+                    defaultValue={"10"}
+                    suffix={" %"}
                   />
                 </View>
                 <View style={[FLEX_ROW, CENTERED_FLEX]}>
-                  <GridHeaderContent headerTx={'invoiceFormScreen.invoiceForm.personalizedNotice'} bodyText={'Bon pour accord Signature du client'} style={FULL} />
+                  <GridHeaderContent headerTx={"invoiceFormScreen.invoiceForm.personalizedNotice"}
+                                     bodyText={"Bon pour accord Signature du client"} style={FULL}/>
                   <Switch style={FULL} value={legalNotice} onToggle={newValue => setLegalNotice(newValue)} />
                 </View>
                 <Separator style={SEPARATOR_STYLE} />
                 <View style={[FLEX_ROW, CENTERED_FLEX]}>
-                  <GridHeaderContent headerTx={'invoiceFormScreen.invoiceForm.viewMail'} bodyText={values['customer']['email'] || ''} style={FULL} />
-                  <Switch style={FULL} value={showMyMailAddress} onToggle={newValue => setShowMyMailAddress(newValue)} />
+                  <GridHeaderContent headerTx={"invoiceFormScreen.invoiceForm.viewMail"}
+                                     bodyText={values["customer"]["email"] || ""} style={FULL} />
+                  <Switch style={FULL} value={showMyMailAddress}
+                          onToggle={newValue => setShowMyMailAddress(newValue)} />
                 </View>
                 <Separator style={SEPARATOR_STYLE} />
               </View>
 
-              <View style={[FLEX_ROW, { marginVertical: spacing[6], marginHorizontal: spacing[4], justifyContent: "center", }]}>
+              <View style={[FLEX_ROW, {
+                marginVertical: spacing[6],
+                marginHorizontal: spacing[4],
+                justifyContent: "center"
+              }]}>
                 <View style={SAVE_ICON_CONTAINER}>
-                  <Icon icon={'save'} />
+                  <Icon icon={"save"} />
                 </View>
                 <Button
                   tx='invoiceScreen.labels.invoiceForm'
