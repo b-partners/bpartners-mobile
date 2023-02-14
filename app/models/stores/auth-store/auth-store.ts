9331e8772d442534800fd9ed88d9fdabf4a4e7c6
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-02-14 17:13:58 +0300

    run prettier

--- app/models/stores/auth-store/auth-store.ts
+++ app/models/stores/auth-store/auth-store.ts
@@ -148,7 +148,7 @@ export const AuthStoreModel = types
     getToken: flow(function* (code) {
       const signInApi = new AuthApi(self.environment.api);
       try {
-        const { accessToken, refreshToken } = env.isCi ? { accessToken: env.ciAccessToken , refreshToken: null } : yield signInApi.getToken(code);
+        const { accessToken, refreshToken } = env.isCi ? { accessToken: env.ciAccessToken, refreshToken: null } : yield signInApi.getToken(code);
         yield self.getTokenSuccess({ accessToken: accessToken, refreshToken: refreshToken });
       } catch (e) {
         self.getTokenFail(e);
--- app/screens/code-exchange/code-exchange-screen.tsx
+++ app/screens/code-exchange/code-exchange-screen.tsx
@@ -1,15 +1,15 @@
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { observer } from 'mobx-react-lite';
 import React, { FC, useEffect } from 'react';
-import {SafeAreaView, View} from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
-import {AutoImage, Loader, Screen} from '../../components';
+import { AutoImage, Loader, Screen } from '../../components';
 import env from '../../config/env';
 import { useStores } from '../../models';
 import { NavigatorParamList } from '../../navigators';
+import { spacing } from '../../theme';
+import { palette } from '../../theme/palette';
 import { ErrorBoundary } from '../error/error-boundary';
-import {spacing} from "../../theme";
-import {palette} from "../../theme/palette";
 
 export const CodeExchangeScreen: FC<DrawerScreenProps<NavigatorParamList, 'oauth'>> = observer(({ navigation, route }) => {
   const { authStore } = useStores();
@@ -38,15 +38,15 @@ export const CodeExchangeScreen: FC<DrawerScreenProps<NavigatorParamList, 'oauth
     <ErrorBoundary catchErrors='always'>
       <Screen backgroundColor='#fff'>
         <AutoImage
-            source={require('./code-exchange.background.png')}
-            resizeMode='stretch'
-            resizeMethod='auto'
-            style={{ position: 'absolute', height: '100%', width: '100%' }}
+          source={require('./code-exchange.background.png')}
+          resizeMode='stretch'
+          resizeMethod='auto'
+          style={{ position: 'absolute', height: '100%', width: '100%' }}
         />
         <View testID='SignInWebViewScreen' style={{ paddingHorizontal: spacing[7], height: '100%', justifyContent: 'center' }}>
           <AutoImage source={require('./bp-logo.png')} resizeMode='contain' resizeMethod='auto' style={{ width: '100%', height: '40%' }} />
           <SafeAreaView />
-          <Loader size="large" color={palette.white} containerStyle={{flex: 0}}/>
+          <Loader size='large' color={palette.white} containerStyle={{ flex: 0 }} />
         </View>
       </Screen>
     </ErrorBoundary>
