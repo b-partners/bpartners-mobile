198e8c20b233bc825daced630baf7c127dbaf120
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-02-15 14:33:23 +0300

    feat: add a back button

--- app/screens/profile/profile-screen.tsx
+++ app/screens/profile/profile-screen.tsx
@@ -53,7 +53,13 @@ export const ProfileScreen: FC<DrawerScreenProps<NavigatorParamList, 'profile'>>
       <View testID='TransactionListScreen' style={FULL}>
         <GradientBackground colors={['#422443', '#281b34']} />
         <Screen style={CONTAINER} preset='auto' backgroundColor={color.transparent}>
-          <Header headerTx='profileScreen.title' style={HEADER} titleStyle={HEADER_TITLE} />
+          <Header
+              headerTx='profileScreen.title'
+              style={HEADER}
+              titleStyle={HEADER_TITLE}
+              leftIcon={'back'}
+              onLeftPress={() => navigation.navigate('home')}
+          />
           <View style={LOGO_CONTAINER}>
             <Image
               source={{ uri: `${env.apiBaseUrl}/accounts/${currentAccount.id}/files/logo.jpeg/raw?accessToken=${accessToken}` }}
