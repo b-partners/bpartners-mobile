c50db40391700e2f570cc533469495c038e711e0
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-02-23 17:06:00 +0300

    fix: emulator test name

--- .detoxrc.js
+++ .detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_4a_API_31',
+        avdName: 'Pixel_5_API_31',
       },
     },
   },
