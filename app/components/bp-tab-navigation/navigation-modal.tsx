c163ddceeba303856a14c93dcbc89d6272715f3d
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-02-27 17:15:34 +0300

    fix: bottom modal on ios

--- app/components/bp-tab-navigation/navigation-modal.tsx
+++ app/components/bp-tab-navigation/navigation-modal.tsx
@@ -27,8 +27,6 @@ type ModalRouteProps = {
   help: () => void | null | string;
 };
 
-const modalHeight = Dimensions.get('window').height - 120;
-
 export const NavigationModal: React.FC<NavigationModalProps> = props => {
   const { modalVisible, setModalVisible } = props;
 
@@ -70,19 +68,19 @@ export const NavigationModal: React.FC<NavigationModalProps> = props => {
       visible={modalVisible}
       onDismiss={() => setModalVisible(false)}
       contentContainerStyle={{
-        zIndex: -100000000,
-        position: 'absolute',
-        top: 0,
+        position: 'relative',
         width: '100%',
         height: modalHeight,
         backgroundColor: 'rgba(0, 0, 0, 0.6)',
         padding: 20,
       }}
+      style={{
+        justifyContent: 'flex-start',
+      }}
     >
       <View
         testID='BottomTabModal'
         style={{
-          zIndex: -10000000,
           width: '18%',
           height: 200,
           backgroundColor: palette.purple,
