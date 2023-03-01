4dd4ff9861caa12f8d0784b4204228848014962d
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2023-03-01 09:26:33 +0300

    chore: wrapping form with formik

--- app/screens/invoice-form/invoice-form.tsx
+++ app/screens/invoice-form/invoice-form.tsx
@@ -1,5 +1,7 @@
+import { Formik } from 'formik';
 import React from 'react';
 import { TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+import uuid from 'react-native-uuid';
 import RNVIcon from 'react-native-vector-icons/AntDesign';
 
 import { Button, Icon, Text } from '../../components';
@@ -43,128 +45,173 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
   const { customers, products } = props;
 
   return (
-    <View style={{ paddingBottom: spacing[6] }}>
-      <View style={ROW_STYLE}>
-        <InvoiceFormField labelTx='invoiceFormScreen.invoiceForm.title' placeholderTx='invoiceFormScreen.invoiceForm.titlePlaceholder' style={{ flex: 1 }} />
-      </View>
-      <View style={ROW_STYLE}>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.reference'
-          placeholderTx='invoiceFormScreen.invoiceForm.referencePlaceholder'
-          style={{ flex: 1 }}
-        />
-      </View>
-      <View style={ROW_STYLE}>
-        <DatePickerField
-          labelTx='invoiceFormScreen.invoiceForm.sendingDate'
-          isButtonPreset={false}
-          labelStyle={DATE_PICKER_LABEL_STYLE}
-          containerStyle={DATE_PICKER_CONTAINER_STYLE}
-          textStyle={DATE_PICKER_TEXT_STYLE}
-          value={new Date()}
-          onDateChange={() => {}}
-          dateSeparator='/'
-        />
-        <DatePickerField
-          labelTx='invoiceFormScreen.invoiceForm.validityDate'
-          isButtonPreset={false}
-          labelStyle={DATE_PICKER_LABEL_STYLE}
-          containerStyle={DATE_PICKER_CONTAINER_STYLE}
-          textStyle={DATE_PICKER_TEXT_STYLE}
-          value={new Date()}
-          onDateChange={() => {}}
-          dateSeparator='/'
-        />
-      </View>
-      <View style={ROW_STYLE}>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowed'
-          placeholderTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowedPlaceholder'
-          style={{ flex: 1 }}
-        />
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.delayPenaltyPercent'
-          placeholderTx='invoiceFormScreen.invoiceForm.delayPenaltyPercentPlaceholder'
-          style={{ flex: 1 }}
-        />
-      </View>
-      <View style={ROW_STYLE}>
-        <InvoiceFormField labelTx='invoiceFormScreen.invoiceForm.comment' placeholderTx='invoiceFormScreen.invoiceForm.comment' style={{ flex: 1 }} />
-      </View>
-      <>
-        <View>
-          <ProductFormField products={products} />
-        </View>
-        <View style={{ ...ROW_STYLE, paddingHorizontal: spacing[3] }}>
-          <Button
-            style={{
-              backgroundColor: color.transparent,
-              borderColor: color.palette.secondaryColor,
-              borderWidth: 1,
-              borderRadius: 25,
-              flexDirection: 'row',
-              justifyContent: 'center',
-              paddingVertical: spacing[3],
-              paddingHorizontal: spacing[6],
-              width: '100%',
-            }}
-          >
-            <RNVIcon name='plus' size={16} color={color.palette.secondaryColor} />
-            <Text
-              tx='invoiceFormScreen.productForm.addProduct'
+    <Formik<Invoice>
+      initialValues={{
+        id: uuid.v4().toString(),
+        ref: null,
+        title: null,
+        sendingDate: new Date(),
+        validityDate: new Date(),
+        toPayAt: new Date(),
+        delayPenaltyPercent: null,
+        delayInPaymentAllowed: null,
+        comment: null,
+        customer: null,
+        products: [] as any,
+        status: null,
+        fileId: null,
+        totalVat: null,
+        totalPriceWithoutVat: null,
+        totalPriceWithVat: null,
+        paymentUrl: null,
+        createdAt: null,
+        updatedAt: null,
+      }}
+      onSubmit={() => {}}
+    >
+      {({}) => {
+        return (
+          <View style={{ paddingBottom: spacing[6] }}>
+            <View style={ROW_STYLE}>
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.title'
+                placeholderTx='invoiceFormScreen.invoiceForm.titlePlaceholder'
+                style={{ flex: 1 }}
+              />
+            </View>
+            <View style={ROW_STYLE}>
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.reference'
+                placeholderTx='invoiceFormScreen.invoiceForm.referencePlaceholder'
+                style={{ flex: 1 }}
+              />
+            </View>
+            <View style={ROW_STYLE}>
+              <DatePickerField
+                labelTx='invoiceFormScreen.invoiceForm.sendingDate'
+                isButtonPreset={false}
+                labelStyle={DATE_PICKER_LABEL_STYLE}
+                containerStyle={DATE_PICKER_CONTAINER_STYLE}
+                textStyle={DATE_PICKER_TEXT_STYLE}
+                value={new Date()}
+                onDateChange={() => {}}
+                dateSeparator='/'
+              />
+              <DatePickerField
+                labelTx='invoiceFormScreen.invoiceForm.validityDate'
+                isButtonPreset={false}
+                labelStyle={DATE_PICKER_LABEL_STYLE}
+                containerStyle={DATE_PICKER_CONTAINER_STYLE}
+                textStyle={DATE_PICKER_TEXT_STYLE}
+                value={new Date()}
+                onDateChange={() => {}}
+                dateSeparator='/'
+              />
+            </View>
+            <View style={ROW_STYLE}>
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowed'
+                placeholderTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowedPlaceholder'
+                style={{ flex: 1 }}
+              />
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.delayPenaltyPercent'
+                placeholderTx='invoiceFormScreen.invoiceForm.delayPenaltyPercentPlaceholder'
+                style={{ flex: 1 }}
+              />
+            </View>
+            <View style={ROW_STYLE}>
+              <InvoiceFormField labelTx='invoiceFormScreen.invoiceForm.comment' placeholderTx='invoiceFormScreen.invoiceForm.comment' style={{ flex: 1 }} />
+            </View>
+            <>
+              <View>
+                <ProductFormField products={products} />
+              </View>
+              <View style={{ ...ROW_STYLE, paddingHorizontal: spacing[3] }}>
+                <Button
+                  style={{
+                    backgroundColor: color.transparent,
+                    borderColor: color.palette.secondaryColor,
+                    borderWidth: 1,
+                    borderRadius: 25,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    paddingVertical: spacing[3],
+                    paddingHorizontal: spacing[6],
+                    width: '100%',
+                  }}
+                >
+                  <RNVIcon name='plus' size={16} color={color.palette.secondaryColor} />
+                  <Text
+                    tx='invoiceFormScreen.productForm.addProduct'
+                    style={{
+                      color: color.palette.secondaryColor,
+                      fontFamily: 'Geometria',
+                      marginLeft: spacing[3],
+                    }}
+                  />
+                </Button>
+              </View>
+            </>
+            <View style={ROW_STYLE}>
+              <SelectFormField
+                value={null}
+                onValueChange={() => {}}
+                labelTx='invoiceScreen.labels.customerSection'
+                modalTx='invoiceFormScreen.customerForm.title'
+                placeholderTx='invoiceScreen.labels.customerSectionPlaceholder'
+                items={customers}
+                itemLabel='name'
+                itemValue='id'
+                itemSuffix={<Icon icon='edit' />}
+                itemSuffixAction={customer => {
+                  __DEV__ && console.tron.log({ customer });
+                }}
+                footer={<CustomerFormFieldFooter />}
+                selectContainerStyle={{
+                  padding: spacing[3],
+                  marginVertical: spacing[6],
+                  width: '100%',
+                  borderWidth: 1,
+                  borderColor: '#E1E5EF',
+                }}
+                style={INVOICE_LABEL_STYLE}
+              />
+            </View>
+            <View
               style={{
-                color: color.palette.secondaryColor,
-                fontFamily: 'Geometria',
-                marginLeft: spacing[3],
+                ...ROW_STYLE,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                paddingHorizontal: spacing[5],
               }}
-            />
-          </Button>
-        </View>
-      </>
-      <View style={ROW_STYLE}>
-        <SelectFormField
-          value={null}
-          onValueChange={() => {}}
-          labelTx='invoiceScreen.labels.customerSection'
-          modalTx='invoiceFormScreen.customerForm.title'
-          placeholderTx='invoiceScreen.labels.customerSectionPlaceholder'
-          items={customers}
-          itemLabel='name'
-          itemValue='id'
-          itemSuffix={<Icon icon='edit' />}
-          itemSuffixAction={customer => {
-            __DEV__ && console.tron.log({ customer });
-          }}
-          footer={<CustomerFormFieldFooter />}
-          selectContainerStyle={{
-            padding: spacing[3],
-            marginVertical: spacing[6],
-            width: '100%',
-            borderWidth: 1,
-            borderColor: '#E1E5EF',
-          }}
-          style={INVOICE_LABEL_STYLE}
-        />
-      </View>
-      <View style={{ ...ROW_STYLE, flexDirection: 'row', justifyContent: 'center', paddingHorizontal: spacing[5] }}>
-        <TouchableOpacity>
-          <View
-            style={{
-              borderColor: color.palette.secondaryColor,
-              borderWidth: 2,
-              borderRadius: 100,
-              padding: spacing[3],
-            }}
-          >
-            <RNVIcon name='save' size={25} color={color.palette.secondaryColor} />
+            >
+              <TouchableOpacity>
+                <View
+                  style={{
+                    borderColor: color.palette.secondaryColor,
+                    borderWidth: 2,
+                    borderRadius: 100,
+                    padding: spacing[3],
+                  }}
+                >
+                  <RNVIcon name='save' size={25} color={color.palette.secondaryColor} />
+                </View>
+              </TouchableOpacity>
+              <Button
+                tx='invoiceFormScreen.invoicePreview'
+                style={{
+                  backgroundColor: color.palette.secondaryColor,
+                  borderRadius: 25,
+                  marginLeft: 15,
+                  flex: 1,
+                }}
+                textStyle={{ color: color.palette.white, fontSize: 14, fontFamily: 'Geometria-Bold' }}
+              />
+            </View>
           </View>
-        </TouchableOpacity>
-        <Button
-          tx='invoiceFormScreen.invoicePreview'
-          style={{ backgroundColor: color.palette.secondaryColor, borderRadius: 25, marginLeft: 15, flex: 1 }}
-          textStyle={{ color: color.palette.white, fontSize: 14, fontFamily: 'Geometria-Bold' }}
-        />
-      </View>
-    </View>
+        );
+      }}
+    </Formik>
   );
 };
