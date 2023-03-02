4e3947451091c42bd2c8de4f30c215a5fed1dfd4
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2023-03-02 09:59:25 +0300

    chore: removing formik

--- app/screens/invoice-form/components/product-form-field.tsx
+++ app/screens/invoice-form/components/product-form-field.tsx
@@ -1,3 +1,4 @@
+import { Observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
@@ -24,83 +25,87 @@ export const ProductFormField: React.FC<ProductFormFieldProps> = props => {
   }, [currentProduct]);
 
   return (
-    <View
-      style={{
-        paddingHorizontal: spacing[6],
-        paddingVertical: spacing[4],
-        shadowColor: color.palette.secondaryColor,
-        shadowOffset: {
-          width: 0,
-          height: 4,
-        },
-        shadowOpacity: 0.32,
-        shadowRadius: 5.46,
-        elevation: 9,
-        backgroundColor: color.palette.white,
-        zIndex: 10,
-        borderRadius: 10,
-        marginBottom: spacing[5],
-      }}
-    >
-      <Button
-        style={{
-          flexDirection: 'row',
-          backgroundColor: color.transparent,
-          position: 'absolute',
-          top: -10,
-          right: -15,
-        }}
-        onPress={() => onDeleteItem(currentProduct, index)}
-      >
-        <Text
-          tx='invoiceFormScreen.productForm.delete'
+    <Observer>
+      {() => (
+        <View
           style={{
-            color: color.palette.secondaryColor,
-            fontFamily: 'Geometria',
-            fontSize: 13,
-            marginRight: spacing[1],
+            paddingHorizontal: spacing[6],
+            paddingVertical: spacing[4],
+            shadowColor: color.palette.secondaryColor,
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.32,
+            shadowRadius: 5.46,
+            elevation: 9,
+            backgroundColor: color.palette.white,
+            zIndex: 10,
+            borderRadius: 10,
+            marginBottom: spacing[5],
           }}
-        />
-        <Icon icon='trash' />
-      </Button>
-      <View>
-        <SelectFormField
-          itemLabel='description'
-          itemValue='id'
-          items={props.items}
-          labelTx='invoiceFormScreen.productForm.title'
-          placeholderTx='invoiceFormScreen.productForm.placeholder'
-          modalTx='invoiceFormScreen.productForm.title'
-          value={currentProduct?.id}
-          onValueChange={product => {
-            setCurrentProduct(product);
-          }}
-        />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.productForm.quantity'
-          style={{ flex: 1 }}
-          keyboardType='numeric'
-          onChangeText={quantity => {
-            setCurrentProduct(product => ({ ...product, quantity: +quantity }));
-          }}
-        />
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.productForm.unitPrice'
-          style={{ flex: 2 }}
-          editable={false}
-          value={printCurrency(currentProduct?.unitPrice)?.toString()}
-        />
-        <InvoiceFormField labelTx='invoiceFormScreen.productForm.vat' style={{ flex: 2 }} editable={false} value={printVat(currentProduct?.vatPercent)} />
-      </View>
-      <View>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.productForm.totalWithVat'
-          editable={false}
-          value={printCurrency(currentProduct?.unitPriceWithVat * currentProduct?.quantity)}
-        />
-      </View>
-    </View>
+        >
+          <Button
+            style={{
+              flexDirection: 'row',
+              backgroundColor: color.transparent,
+              position: 'absolute',
+              top: -10,
+              right: -15,
+            }}
+            onPress={() => onDeleteItem(currentProduct, index)}
+          >
+            <Text
+              tx='invoiceFormScreen.productForm.delete'
+              style={{
+                color: color.palette.secondaryColor,
+                fontFamily: 'Geometria',
+                fontSize: 13,
+                marginRight: spacing[1],
+              }}
+            />
+            <Icon icon='trash' />
+          </Button>
+          <View>
+            <SelectFormField
+              itemLabel='description'
+              itemValue='id'
+              items={props.items}
+              labelTx='invoiceFormScreen.productForm.title'
+              placeholderTx='invoiceFormScreen.productForm.placeholder'
+              modalTx='invoiceFormScreen.productForm.title'
+              value={currentProduct?.id}
+              onValueChange={product => {
+                setCurrentProduct(product);
+              }}
+            />
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <InvoiceFormField
+              labelTx='invoiceFormScreen.productForm.quantity'
+              style={{ flex: 1 }}
+              keyboardType='numeric'
+              onChangeText={quantity => {
+                setCurrentProduct(product => ({ ...product, quantity: +quantity }));
+              }}
+            />
+            <InvoiceFormField
+              labelTx='invoiceFormScreen.productForm.unitPrice'
+              style={{ flex: 2 }}
+              editable={false}
+              value={printCurrency(currentProduct?.unitPrice)?.toString()}
+            />
+            <InvoiceFormField labelTx='invoiceFormScreen.productForm.vat' style={{ flex: 2 }} editable={false} value={printVat(currentProduct?.vatPercent)} />
+          </View>
+          <View>
+            <InvoiceFormField
+              labelTx='invoiceFormScreen.productForm.totalWithVat'
+              editable={false}
+              value={printCurrency(currentProduct?.unitPriceWithVat * currentProduct?.quantity)}
+            />
+          </View>
+        </View>
+      )}
+    </Observer>
   );
 };
