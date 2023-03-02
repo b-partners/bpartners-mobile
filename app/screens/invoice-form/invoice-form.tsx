9851313df11340f7971a44b25d058140a2ebeac9
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2023-03-02 11:40:33 +0300

    chore: binding invoice form to react hook form

--- app/screens/invoice-form/invoice-form.tsx
+++ app/screens/invoice-form/invoice-form.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import { TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 import RNVIcon from 'react-native-vector-icons/AntDesign';
 
 import { Button, Icon, Text } from '../../components';
 import { DatePickerField } from '../../components/date-picker-field/date-picker-field';
 import { Customer } from '../../models/entities/customer/customer';
-import { Invoice } from '../../models/entities/invoice/invoice';
-import { Product } from '../../models/entities/product/product';
+import { Invoice, createInvoiceDefaultModel } from '../../models/entities/invoice/invoice';
+import { Product, createProductDefaultModel } from '../../models/entities/product/product';
 import { color, spacing } from '../../theme';
 import { CustomerFormFieldFooter } from './components/customer-form-field-footer';
 import { InvoiceFormField } from './components/invoice-form-field';
-import { ProductsFormField } from './components/products-form-field';
+import { ProductFormField } from './components/product-form-field';
 import { SelectFormField } from './select-form-field/select-form-field';
 
 type InvoiceFormProps = { invoice: Invoice; customers: Customer[]; products: Product[]; onSaveInvoice: (invoice: Invoice) => Promise<void> };
@@ -42,58 +43,160 @@ const INVOICE_LABEL_STYLE: TextStyle = {
 export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
   const { customers, products } = props;
 
+  const { control, handleSubmit } = useForm({
+    defaultValues: createInvoiceDefaultModel().create(),
+  });
+
+  const { fields, append, remove, update } = useFieldArray({ control, name: 'products' });
+
+  const onSubmit = invoice => {
+    __DEV__ && console.tron.log({ invoice });
+  };
+
   return (
     <View style={{ paddingBottom: spacing[6] }}>
       <View style={ROW_STYLE}>
-        <InvoiceFormField labelTx='invoiceFormScreen.invoiceForm.title' placeholderTx='invoiceFormScreen.invoiceForm.titlePlaceholder' style={{ flex: 1 }} />
-      </View>
-      <View style={ROW_STYLE}>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.reference'
-          placeholderTx='invoiceFormScreen.invoiceForm.referencePlaceholder'
-          style={{ flex: 1 }}
+        <Controller
+          name='title'
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => {
+            return (
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.title'
+                placeholderTx='invoiceFormScreen.invoiceForm.titlePlaceholder'
+                style={{ flex: 1 }}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            );
+          }}
         />
       </View>
       <View style={ROW_STYLE}>
-        <DatePickerField
-          labelTx='invoiceFormScreen.invoiceForm.sendingDate'
-          isButtonPreset={false}
-          labelStyle={DATE_PICKER_LABEL_STYLE}
-          containerStyle={DATE_PICKER_CONTAINER_STYLE}
-          textStyle={DATE_PICKER_TEXT_STYLE}
-          dateSeparator='/'
-          value={new Date()}
-          onDateChange={() => {}}
-        />
-        <DatePickerField
-          labelTx='invoiceFormScreen.invoiceForm.validityDate'
-          isButtonPreset={false}
-          labelStyle={DATE_PICKER_LABEL_STYLE}
-          containerStyle={DATE_PICKER_CONTAINER_STYLE}
-          textStyle={DATE_PICKER_TEXT_STYLE}
-          dateSeparator='/'
-          value={new Date()}
-          onDateChange={() => {}}
+        <Controller
+          name='ref'
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => {
+            return (
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.reference'
+                placeholderTx='invoiceFormScreen.invoiceForm.referencePlaceholder'
+                style={{ flex: 1 }}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            );
+          }}
         />
       </View>
       <View style={ROW_STYLE}>
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowed'
-          placeholderTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowedPlaceholder'
-          style={{ flex: 1 }}
+        <Controller
+          name='sendingDate'
+          control={control}
+          render={({ field: { value, onChange } }) => {
+            return (
+              <DatePickerField
+                labelTx='invoiceFormScreen.invoiceForm.sendingDate'
+                isButtonPreset={false}
+                labelStyle={DATE_PICKER_LABEL_STYLE}
+                containerStyle={DATE_PICKER_CONTAINER_STYLE}
+                textStyle={DATE_PICKER_TEXT_STYLE}
+                dateSeparator='/'
+                value={value}
+                onDateChange={onChange}
+              />
+            );
+          }}
         />
-        <InvoiceFormField
-          labelTx='invoiceFormScreen.invoiceForm.delayPenaltyPercent'
-          placeholderTx='invoiceFormScreen.invoiceForm.delayPenaltyPercentPlaceholder'
-          style={{ flex: 1 }}
+        <Controller
+          name='validityDate'
+          control={control}
+          render={({ field: { value, onChange } }) => {
+            return (
+              <DatePickerField
+                labelTx='invoiceFormScreen.invoiceForm.validityDate'
+                isButtonPreset={false}
+                labelStyle={DATE_PICKER_LABEL_STYLE}
+                containerStyle={DATE_PICKER_CONTAINER_STYLE}
+                textStyle={DATE_PICKER_TEXT_STYLE}
+                dateSeparator='/'
+                value={value}
+                onDateChange={onChange}
+              />
+            );
+          }}
         />
       </View>
       <View style={ROW_STYLE}>
-        <InvoiceFormField labelTx='invoiceFormScreen.invoiceForm.comment' placeholderTx='invoiceFormScreen.invoiceForm.comment' style={{ flex: 1 }} />
+        <Controller
+          name='delayInPaymentAllowed'
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => {
+            return (
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowed'
+                placeholderTx='invoiceFormScreen.invoiceForm.delayInPaymentAllowedPlaceholder'
+                style={{ flex: 1 }}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value?.toString()}
+              />
+            );
+          }}
+        />
+        <Controller
+          name='delayPenaltyPercent'
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => {
+            return (
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.delayPenaltyPercent'
+                placeholderTx='invoiceFormScreen.invoiceForm.delayPenaltyPercentPlaceholder'
+                style={{ flex: 1 }}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value?.toString()}
+              />
+            );
+          }}
+        />
+      </View>
+      <View style={{ ...ROW_STYLE, marginBottom: spacing[5] }}>
+        <Controller
+          name='comment'
+          control={control}
+          render={({ field: { value, onBlur, onChange } }) => {
+            return (
+              <InvoiceFormField
+                labelTx='invoiceFormScreen.invoiceForm.comment'
+                placeholderTx='invoiceFormScreen.invoiceForm.comment'
+                style={{ flex: 1 }}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value?.toString()}
+              />
+            );
+          }}
+        />
       </View>
       <>
-        <View>
-          <ProductsFormField items={products} onValueChange={products => {}} onDeleteItem={(product, i) => {}} value={[]} />
+        <View style={{ paddingHorizontal: spacing[4] }}>
+          {fields.map((_, i) => {
+            return (
+              <ProductFormField
+                index={i}
+                items={products}
+                onDeleteItem={async (_, index) => {
+                  await remove(index);
+                }}
+                onValueChange={product => {
+                  update(i, product);
+                }}
+              />
+            );
+          })}
         </View>
         <View style={{ ...ROW_STYLE, paddingHorizontal: spacing[3] }}>
           <Button
@@ -104,11 +207,14 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
               borderRadius: 25,
               flexDirection: 'row',
               justifyContent: 'center',
-              paddingVertical: spacing[3],
               paddingHorizontal: spacing[6],
               width: '100%',
+              marginBottom: 0,
             }}
-            onPress={() => {}}
+            onPress={async () => {
+              const product = await createProductDefaultModel().create();
+              await append(product);
+            }}
           >
             <RNVIcon name='plus' size={16} color={color.palette.secondaryColor} />
             <Text
@@ -123,26 +229,34 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
         </View>
       </>
       <View style={ROW_STYLE}>
-        <SelectFormField
-          value={''}
-          onValueChange={customer => {}}
-          labelTx='invoiceScreen.labels.customerSection'
-          modalTx='invoiceFormScreen.customerForm.title'
-          placeholderTx='invoiceScreen.labels.customerSectionPlaceholder'
-          items={customers}
-          itemLabel='name'
-          itemValue='id'
-          itemSuffix={<Icon icon='edit' />}
-          itemSuffixAction={() => {}}
-          footer={<CustomerFormFieldFooter />}
-          selectContainerStyle={{
-            padding: spacing[3],
-            marginVertical: spacing[6],
-            width: '100%',
-            borderWidth: 1,
-            borderColor: '#E1E5EF',
+        <Controller
+          name='customer'
+          control={control}
+          render={({ field: { value, onChange } }) => {
+            return (
+              <SelectFormField
+                value={value?.id}
+                onValueChange={onChange}
+                labelTx='invoiceScreen.labels.customerSection'
+                modalTx='invoiceFormScreen.customerForm.title'
+                placeholderTx='invoiceScreen.labels.customerSectionPlaceholder'
+                items={customers}
+                itemLabel='name'
+                itemValue='id'
+                itemSuffix={<Icon icon='edit' />}
+                itemSuffixAction={() => {}}
+                footer={<CustomerFormFieldFooter />}
+                selectContainerStyle={{
+                  padding: spacing[3],
+                  marginVertical: spacing[6],
+                  width: '100%',
+                  borderWidth: 1,
+                  borderColor: '#E1E5EF',
+                }}
+                style={INVOICE_LABEL_STYLE}
+              />
+            );
           }}
-          style={INVOICE_LABEL_STYLE}
         />
       </View>
       <View
@@ -153,7 +267,7 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
           paddingHorizontal: spacing[5],
         }}
       >
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={handleSubmit(onSubmit)}>
           <View
             style={{
               borderColor: color.palette.secondaryColor,
