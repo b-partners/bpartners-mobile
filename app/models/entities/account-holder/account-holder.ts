c0d308155192ec8726011f453f5bce1105836f68
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-09 15:04:09 +0300

    feat: update account screen

--- app/models/entities/account-holder/account-holder.ts
+++ app/models/entities/account-holder/account-holder.ts
@@ -1,25 +1,25 @@
-import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
+import {Instance, SnapshotIn, SnapshotOut, types} from 'mobx-state-tree';
+import {ContactAddressModel, createContactAddressDefaultModel} from "../contact-address/contact-address";
 
 export const AccountHolderModel = types.model('AccountHolder').props({
-  id: types.maybe(types.maybeNull(types.string)),
-  name: types.maybe(types.maybeNull(types.string)),
-  address: types.maybe(types.maybeNull(types.string)),
-  city: types.maybe(types.maybeNull(types.string)),
-  country: types.maybe(types.maybeNull(types.string)),
-  postalCode: types.maybe(types.maybeNull(types.string)),
+    id: types.maybe(types.maybeNull(types.string)),
+    name: types.maybe(types.maybeNull(types.string)),
+    siren: types.maybe(types.maybeNull(types.string)),
+    contactAddress: types.maybe(types.maybeNull(ContactAddressModel))
 });
 
-export interface AccountHolder extends Instance<typeof AccountHolderModel> {}
+export interface AccountHolder extends Instance<typeof AccountHolderModel> {
+}
 
-export interface AccountSnapshotOut extends SnapshotOut<typeof AccountHolderModel> {}
+export interface AccountSnapshotOut extends SnapshotOut<typeof AccountHolderModel> {
+}
 
-export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {}
+export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {
+}
 
 export const createAccountDefaultModel = () =>
-  types.optional(AccountHolderModel, {
-    name: null,
-    address: null,
-    city: null,
-    country: null,
-    postalCode: null,
-  });
+    types.optional(AccountHolderModel, {
+        name: null,
+        siren: null,
+        contactAddress: createContactAddressDefaultModel()
+    });
