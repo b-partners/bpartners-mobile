f3968882b75e566fc8fd61911b3549ef70fb0724
Liana Finaritra <100028310+lianafinaritra@users.noreply.github.com>
2023-03-10 09:16:41 +0300

    feat: show all marketplaces (#127)
    * feat: add missing parameters to get marketplace
    
    * run prettier

--- app/models/entities/criteria/criteria.ts
+++ app/models/entities/criteria/criteria.ts
@@ -2,13 +2,19 @@ import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
 
 import { InvoiceStatus } from '../invoice/invoice';
 
-export const CriteriaModel = types.model('Criteria').props({
+export const PageCriteriaModel = types.model('PageCriteria').props({
   page: types.maybe(types.maybeNull(types.number)),
   pageSize: types.maybe(types.maybeNull(types.number)),
+});
+
+export const CriteriaModel = types.model('Criteria').props({
+  page: types.maybe(PageCriteriaModel),
   status: types.maybe(types.maybeNull(types.enumeration(Object.values(InvoiceStatus)))),
 });
 
 export interface Criteria extends Instance<typeof CriteriaModel> {}
+
+export interface PageCriteria extends Instance<typeof PageCriteriaModel> {}
 
 export interface CriteriaSnapshotOut extends SnapshotOut<typeof CriteriaModel> {}
 
