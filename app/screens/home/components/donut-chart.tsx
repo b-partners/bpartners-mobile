2e50657a87c18438320d3e7ac66d23946a3d14fb
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-13 14:14:01 +0300

    feat: add label to chart

--- app/screens/home/components/donut-chart.tsx
+++ app/screens/home/components/donut-chart.tsx
@@ -3,25 +3,16 @@ import { TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 import { PieChart } from 'react-native-gifted-charts';
 import Snackbar from 'react-native-snackbar';
 
-import { Text } from '../../../components';
-// import { Icon, Text } from '../../../components';
+import { Icon, Text } from '../../../components';
 import { TxKeyPath, translate } from '../../../i18n';
-import { TransactionSummary as ITransactionSummary } from '../../../models/entities/transaction-summary/transaction-summary';
+import { TransactionSummary as ITransactionSummary, TransactionSummary } from '../../../models/entities/transaction-summary/transaction-summary';
 import { color, spacing } from '../../../theme';
 import { palette } from '../../../theme/palette';
 import { printCurrency } from '../../../utils/money';
 
 const CHART_CONTAINER: ViewStyle = { display: 'flex', flexDirection: 'row', alignItems: 'flex-start' };
 const LABELS_SECTION: ViewStyle = { flex: 1 };
-const CHART_SECTION: ViewStyle = {
-  flex: 2,
-  position: 'absolute',
-  bottom: -121,
-  right: 0,
-  alignItems: 'center',
-  alignContent: 'center',
-  justifyContent: 'center',
-};
+const CHART_SECTION: ViewStyle = { flex: 2, position: 'absolute', bottom: -121, right: 0, alignItems: 'center', alignContent: 'center', justifyContent: 'center' };
 const LABEL_CONTAINER_STYLE: ViewStyle = {
   display: 'flex',
   flexDirection: 'row',
@@ -69,6 +60,15 @@ const SUMMARY_TITLE_STYLE: TextStyle = {
   fontFamily: 'Geometria-Heavy',
 };
 
+const mock_summary: TransactionSummary = {
+  id: 'one',
+  month: 2,
+  income: 9000,
+  outcome: 9000,
+  cashFlow: 202301,
+  updatedAt: new Date(),
+};
+
 export const DonutChart: React.FC<DonutChartProps> = props => {
   const { summary } = props;
 
@@ -91,7 +91,9 @@ export const DonutChart: React.FC<DonutChartProps> = props => {
     <View>
       <View style={TITLE_CONTAINER_STYLE}>
         <Text tx='homeScreen.summary.title' style={SUMMARY_TITLE_STYLE} />
-        <TouchableOpacity onPress={() => showSnackbar()}>{/*<Icon icon='settings' />*/}</TouchableOpacity>
+        <TouchableOpacity onPress={() => showSnackbar()}>
+          <Icon icon='settings' />
+        </TouchableOpacity>
       </View>
       <View style={CHART_CONTAINER}>
         <View style={LABELS_SECTION}>
@@ -101,9 +103,7 @@ export const DonutChart: React.FC<DonutChartProps> = props => {
               return (
                 <View style={LABEL_CONTAINER_STYLE} key={item}>
                   <View style={LABEL_COLOR_STYLE(COLORS[i])} />
-                  <Text style={{ color: '#989FB3', fontFamily: 'Geometria' }}>
-                    {translate(`homeScreen.summary.${item}` as TxKeyPath)}: {printCurrency(summary[item])}
-                  </Text>
+                  <Text text={translate(`homeScreen.summary.${item}` as TxKeyPath)} style={{ color: '#989FB3', fontFamily: 'Geometria' }} />
                 </View>
               );
             })}
@@ -117,13 +117,14 @@ export const DonutChart: React.FC<DonutChartProps> = props => {
             showText
             textSize={15}
             labelsPosition='outward'
+            textColor={'black'}
             data={[
-              ...Object.keys(summary)
+              ...Object.keys(mock_summary)
                 .filter(key => !FILTERED_KEYS.includes(key))
                 .map((key, i) => {
                   return {
-                    value: summary[key],
-                    text: printCurrency(summary[key]),
+                    value: mock_summary[key],
+                    text: printCurrency(mock_summary[key]),
                     color: COLORS[i],
                   };
                 }),
