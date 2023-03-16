0bcc3501f5b11245dd0e1c5cf15b2ab78357a1dd
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-16 16:43:13 +0300

    fix: add validation to customer creation form

--- app/screens/invoice-form/components/customer-selection-form/customer-creation-form.tsx
+++ app/screens/invoice-form/components/customer-selection-form/customer-creation-form.tsx
@@ -1,7 +1,7 @@
 import { Formik } from 'formik';
 import { observer } from 'mobx-react-lite';
 import React, { FC, PropsWithoutRef, useState } from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import * as yup from 'yup';
 import {Button, Loader, Text} from '../../../../components';
 import {color, spacing} from "../../../../theme";
@@ -9,113 +9,127 @@ import FormField from "../../../../components/forms/form-field";
 import {translate} from "../../../../i18n";
 
 const INVALID_FORM_FIELD = {
-  borderBottomColor: '#FF5983',
-  borderBottomWidth: 2,
+    borderBottomColor: '#FF5983',
+    borderBottomWidth: 2,
 };
 
 export const CustomerCreationForm: FC<
-  PropsWithoutRef<{
-    loading?: boolean;
-  }>
+    PropsWithoutRef<{
+        loading?: boolean;
+    }>
 > = observer(props => {
-  const initialValues = { firstName: '', name: '', phoneNumber: '', address: '', postalCode: '', city: '', country: '' };
+    const initialValues = { firstName: '', lastName: '', phoneNumber: '', address: '', postalCode: '', city: '', country: '' };
 
-  const validationSchema = yup.object().shape({
-    amount: yup.number().required().label(translate('paymentInitiationScreen.fields.amount')),
-    payerEmail: yup.string().email().label(translate('paymentInitiationScreen.fields.payerEmail')),
-  });
+    const validationSchema = yup.object().shape({
+        customerFirstName: yup.string().required().label(translate('paymentInitiationScreen.fields.amount')),
+        customerLastName: yup.string().required().label(translate('paymentInitiationScreen.fields.payerEmail')),
+    });
 
-  const { init, paymentUrl, loading } = props;
+    const { loading } = props;
 
-  const [firstName, setFirstName] = useState('');
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [address, setAddress] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [city, setCity] = useState('');
-    const [country, setCountry] = useState('');
-  return (
-    <View testID='paymentInitiationScreen'>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={() =>
-            __DEV__ && console.tron.log(firstName)
-        }
-      >
-          return (
-            <View style={{ paddingVertical: spacing[6], paddingHorizontal: spacing[3] }}>
-              <FormField
-                testID='reference'
-                name='reference'
-                labelTx='paymentInitiationScreen.fields.reference'
-                value={firstName}
-                inputStyle={{ textTransform: 'none' }}
-              />
-              <FormField
-                testID='label'
-                name='label'
-                labelTx='paymentInitiationScreen.fields.label'
-                value={name}
-                inputStyle={{ textTransform: 'none' }}
-              />
-                <FormField
-                    testID='label'
-                    name='label'
-                    labelTx='paymentInitiationScreen.fields.label'
-                    value={phoneNumber}
-                    inputStyle={{ textTransform: 'none' }}
-                />
-              <FormField
-                testID='clientEmail'
-                name='payerEmail'
-                labelTx='paymentInitiationScreen.fields.payerEmail'
-                keyboardType='email-address'
-                value={address}
-                inputStyle={{ textTransform: 'lowercase' }}
-              />
-                <FormField
-                    testID='clientEmail'
-                    name='payerEmail'
-                    labelTx='paymentInitiationScreen.fields.payerEmail'
-                    keyboardType='email-address'
-                    value={postalCode}
-                    inputStyle={{ textTransform: 'lowercase' }}
-                />
-                <FormField
-                    testID='clientEmail'
-                    name='payerEmail'
-                    labelTx='paymentInitiationScreen.fields.payerEmail'
-                    keyboardType='email-address'
-                    value={city}
-                    inputStyle={{ textTransform: 'lowercase' }}
-                />
-                <FormField
-                    testID='clientEmail'
-                    name='payerEmail'
-                    labelTx='paymentInitiationScreen.fields.payerEmail'
-                    keyboardType='email-address'
-                    value={country}
-                    inputStyle={{ textTransform: 'lowercase' }}
-                />
-              <View style={{ marginTop: spacing[4] }}>
-                <Button
-                  testID='submit'
-                  tx='paymentInitiationScreen.fields.submit'
-                  onPress={() => {}}
-                  style={{
-                    backgroundColor: color.palette.secondaryColor,
-                    height: 45,
-                    borderRadius: 25,
-                  }}
-                  textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
-                >
-                  {loading ? <Loader /> : <Text tx='paymentInitiationScreen.fields.submit' />}
-                </Button>
-              </View>
-            </View>
-          );
-      </Formik>
-    </View>
-  );
+    const [formValues, setFormValues] = useState(initialValues);
+    const [customerfirstName, setCustomerFirstName] = useState('');
+    const [customerLastname, setCustomerLastName] = useState('');
+    const [customerPhoneNumber, setPhoneNumber] = useState('');
+    const [customerAddress, setCustomerAddress] = useState('');
+    const [customerPostalCode, setCustomerPostalCode] = useState('');
+    const [customerCity, setCustomerCity] = useState('');
+    const [customerCountry, setCustomerCountry] = useState('');
+    return (
+        <View testID='paymentInitiationScreen'>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={() => {}}
+            >
+                {({ values, handleSubmit, errors }) => {
+                    return (
+                        <View style={{ paddingVertical: spacing[6], paddingHorizontal: spacing[3] }}>
+                            <FormField
+                                testID='customerFirstName'
+                                name='customerFirstName'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.firstName'
+                                value={formValues.firstName}
+                                onChangeText={(value) => setFormValues({...formValues, firstName: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerLastName'
+                                name='customerLastName'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.lastName'
+                                value={formValues.lastName}
+                                onChangeText={(value) => setFormValues({...formValues, lastName: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerPhoneNumber'
+                                name='customerPhoneNumber'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.phoneNumber'
+                                value={formValues.phoneNumber}
+                                onChangeText={(value) => setFormValues({...formValues, phoneNumber: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerAddress'
+                                name='customerAddress'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.address'
+                                value={formValues.address}
+                                onChangeText={(value) => setFormValues({...formValues, address: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerPostalCode'
+                                name='customerPostalCode'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.postalCode'
+                                value={formValues.postalCode}
+                                onChangeText={(value) => setFormValues({...formValues, postalCode: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerCity'
+                                name='customerCity'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.city'
+                                value={formValues.city}
+                                onChangeText={(value) => setFormValues({...formValues, city: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <FormField
+                                testID='customerCountry'
+                                name='customerCountry'
+                                labelTx='invoiceFormScreen.customerSelectionForm.customerCreationForm.country'
+                                value={formValues.country}
+                                onChangeText={(value) => setFormValues({...formValues, country: value})}
+                                inputStyle={{ textTransform: 'none' }}
+                            />
+                            <View style={{ marginTop: spacing[4] }}>
+                                <Button
+                                    testID='submit'
+                                    tx='paymentInitiationScreen.fields.submit'
+                                    onPress={() => {
+                                        setCustomerFirstName(formValues.firstName);
+                                        setCustomerLastName(formValues.lastName);
+                                        setPhoneNumber(formValues.phoneNumber);
+                                        setCustomerAddress(formValues.address);
+                                        setCustomerPostalCode(formValues.postalCode);
+                                        setCustomerCity(formValues.city);
+                                        setCustomerCountry(formValues.country);
+                                        setFormValues(initialValues);
+                                        handleSubmit();
+                                    }}
+                                    style={{
+                                        backgroundColor: color.palette.secondaryColor,
+                                        height: 45,
+                                        borderRadius: 25,
+                                    }}
+                                    textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
+                                >
+                                    <Text tx='paymentInitiationScreen.fields.submit' />
+                                </Button>
+                            </View>
+                        </View>
+                    );
+                }}
+            </Formik>
+        </View>
+    );
 });
--- app/screens/invoice-form/components/customer-selection-form/customer-creation-modal.tsx
+++ app/screens/invoice-form/components/customer-selection-form/customer-creation-modal.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Modal, View } from 'react-native';
+import {Modal, ScrollView, View} from 'react-native';
 import {palette} from "../../../../theme/palette";
 import {Header} from "../../../../components";
 import {CustomerCreationForm} from "./customer-creation-form";
+import {useStores} from "../../../../models";
 
 type ShareModalProps = {
     creationModal: boolean;
@@ -16,16 +17,19 @@ export const CustomerCreationModal: React.FC<ShareModalProps> = props => {
         setCreationModal(false);
     };
 
+    const { paymentInitiationStore } = useStores();
+    const { paymentUrl, initiatingPayment: loading } = paymentInitiationStore;
+
     return (
         <Modal animationType='slide' transparent={true} visible={creationModal} onRequestClose={closeShareModal}>
             <View style={{ height: '100%', width: '100%', backgroundColor: 'rgba(16,16,19,0.9)', justifyContent: 'flex-end', alignItems: 'center' }}>
-                <View style={{ backgroundColor: palette.white, height: '90%', width: '100%', borderRadius: 20 }}>
+                <View style={{ backgroundColor: palette.white, height: '90%', width: '100%' }}>
                     <View>
                         <Header rightIcon='cross' onRightPress={closeShareModal} style={{ borderTopLeftRadius: 50 }} headerTx='invoiceFormScreen.customerSelectionForm.addClient' />
                     </View>
-                    <View>
-                        <CustomerCreationForm/>
-                    </View>
+                    <ScrollView>
+                        <CustomerCreationForm init={paymentInitiationStore.init} paymentUrl={'paymentUrl'} loading={loading} />
+                    </ScrollView>
                 </View>
             </View>
         </Modal>
