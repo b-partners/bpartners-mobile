5c21c455f41f5a4ffa29a627b7f89755a7ae27c1
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-17 15:33:26 +0300

    chore: adding button to copy the link

--- app/screens/payment-initiation/share-modal.tsx
+++ app/screens/payment-initiation/share-modal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Linking, Modal, View } from 'react-native';
+import { Clipboard, Linking, Modal, View } from 'react-native';
 import RNAntDesign from 'react-native-vector-icons/AntDesign';
 import RNFontAwesome from 'react-native-vector-icons/FontAwesome';
+import CopyContentIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { Button, Header, Text } from '../../components';
 import { palette } from '../../theme/palette';
@@ -19,9 +20,21 @@ export const ShareModal: React.FC<ShareModalProps> = props => {
     setShareModal(false);
   };
 
+  const copyToClipboard = (text: string) => {
+    Clipboard.setString(text);
+  };
+
   return (
     <Modal animationType='slide' transparent={true} visible={shareModal} onRequestClose={closeShareModal}>
-      <View style={{ height: '100%', width: '100%', backgroundColor: 'rgba(16,16,19,0.9)', justifyContent: 'center', alignItems: 'center' }}>
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'rgba(16,16,19,0.9)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <View style={{ backgroundColor: palette.white, height: '50%', width: '90%', borderRadius: 20 }}>
           <View>
             <Header rightIcon='cross' onRightPress={closeShareModal} style={{ borderTopLeftRadius: 50 }} headerTx='paymentInitiationScreen.share' />
@@ -46,11 +59,34 @@ export const ShareModal: React.FC<ShareModalProps> = props => {
                 textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
               >
                 <RNFontAwesome name='whatsapp' size={30} color={palette.green} />
-                <Text tx='paymentInitiationScreen.whatsapp' style={{ marginLeft: 10, color: palette.green, fontFamily: 'Geometria', fontSize: 20 }} />
+                <Text
+                  tx='paymentInitiationScreen.whatsapp'
+                  style={{
+                    marginLeft: 10,
+                    color: palette.green,
+                    fontFamily: 'Geometria',
+                    fontSize: 20,
+                  }}
+                />
               </Button>
             </View>
             <View style={{ width: '100%', alignItems: 'center' }}>
-              <Text tx='paymentInitiationScreen.or' style={{ marginLeft: 10, color: palette.black, fontFamily: 'Geometria', fontSize: 20 }} />
+              <Button
+                onPress={() => {
+                  copyToClipboard(paymentUrl);
+                }}
+                style={{
+                  flexDirection: 'row',
+                  backgroundColor: palette.white,
+                  borderColor: palette.oceanBoatBlue,
+                  height: 50,
+                  width: '50%',
+                  borderRadius: 25,
+                }}
+                textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
+              >
+                <CopyContentIcon name='content-copy' size={30} color={palette.oceanBoatBlue} />
+              </Button>
             </View>
             <View style={{ width: '100%', alignItems: 'center' }}>
               <Button
@@ -71,7 +107,15 @@ export const ShareModal: React.FC<ShareModalProps> = props => {
                 textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
               >
                 <RNAntDesign name='message1' size={30} color={palette.lightYellow} />
-                <Text tx='paymentInitiationScreen.sms' style={{ marginLeft: 10, color: palette.yellow, fontFamily: 'Geometria', fontSize: 20 }} />
+                <Text
+                  tx='paymentInitiationScreen.sms'
+                  style={{
+                    marginLeft: 10,
+                    color: palette.yellow,
+                    fontFamily: 'Geometria',
+                    fontSize: 20,
+                  }}
+                />
               </Button>
             </View>
           </View>
