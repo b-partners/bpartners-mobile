bba6262f3ba3071f118870bf2d1548cbb8b07fca
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-24 09:21:44 +0300

    feat: add new attributes to the profile (#156)
    * feat: creating contact-address model
    
    * feat: update i18n according to the new specification
    
    * feat: update account screen
    
    * feat: creating business-activity model
    
    * feat: creating company-info model
    
    * feat: creating revenue-target model

---
+++ app/models/entities/contact-address/contact-address.test.ts
@@ -0,0 +1,12 @@
+import { ContactAddressModel } from './contact-address';
+
+test('can be created', () => {
+  const instance = ContactAddressModel.create({
+    address: '6 RUE PAUL LANGEVIN',
+    city: 'FONTENAY-SOUS-BOIS',
+    country: 'FRANCE',
+    postalCode: '94120',
+  });
+
+  expect(instance).toBeTruthy();
+});
