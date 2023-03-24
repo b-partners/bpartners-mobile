5aa02f00421f704680ac54c24e616b76861bc275
Judicael <judi.randriamanjato@gmail.com>
2023-03-24 10:02:47 +0300

    feat: implement goal-progress-bar

--- app/screens/home/components/transaction-summary.tsx
+++ app/screens/home/components/transaction-summary.tsx
@@ -1,15 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { View } from 'react-native';
+import { TextStyle, View, ViewStyle } from 'react-native';
+import Snackbar from 'react-native-snackbar';
 
-// import { TextStyle, View, ViewStyle } from 'react-native';
-// import Snackbar from 'react-native-snackbar';
-// import { Button } from '../../../components';
+import { Button } from '../../../components';
 import { AccountHolder as IAccountHolder } from '../../../models/entities/account-holder/account-holder';
 import { TransactionSummary as ITransactionSummary } from '../../../models/entities/transaction-summary/transaction-summary';
-
-/*import { color, spacing } from '../../../theme';
-import { palette } from '../../../theme/palette';*/
+import { color, spacing } from '../../../theme';
+import { palette } from '../../../theme/palette';
 import { DonutChart } from './donut-chart';
 import { GoalProgressBar } from './goal-progress-bar';
 
@@ -18,21 +16,21 @@ interface TransactionSummaryProps {
   accountHolder: IAccountHolder;
 }
 
-/*const BOOST_MY_RESULT_BUTTON_STYLE: ViewStyle = {
+const BOOST_MY_RESULT_BUTTON_STYLE: ViewStyle = {
   borderRadius: 25,
   height: 50,
   marginBottom: spacing[4],
   backgroundColor: '#9C255A',
   borderColor: color.transparent,
-};*/
+};
 
-/*const BOOST_MY_RESULT_BUTTON_TEXT_STYLE: TextStyle = {
+const BOOST_MY_RESULT_BUTTON_TEXT_STYLE: TextStyle = {
   fontSize: 16,
   fontFamily: 'Geometria-Bold',
-};*/
+};
 
 export const TransactionSummary: React.FC<TransactionSummaryProps> = observer(({ summary: summary, accountHolder: accountHolder }) => {
-  /* const showSnackbar = () => {
+  const showSnackbar = () => {
     Snackbar.show({
       text: 'Cette fonctionnalité est encore en construction',
       duration: Snackbar.LENGTH_LONG,
@@ -45,19 +43,19 @@ export const TransactionSummary: React.FC<TransactionSummaryProps> = observer(({
         onPress: () => Snackbar.dismiss(),
       },
     });
-  };*/
+  };
 
   return (
     <>
       <View>
         {summary && <DonutChart summary={summary} />}
         <GoalProgressBar accountHolder={accountHolder} />
-        {/*<Button
+        <Button
           onPress={() => showSnackbar()}
           tx='homeScreen.summary.boostMyResult'
           style={BOOST_MY_RESULT_BUTTON_STYLE}
           textStyle={BOOST_MY_RESULT_BUTTON_TEXT_STYLE}
-        />*/}
+        />
       </View>
     </>
   );
