0aaf05d0e15de4b633088c3231f0f85d3a2d4c29
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-24 09:21:44 +0300

    feat: add new attributes to the profile (#156)
    * feat: creating contact-address model
    
    * feat: update i18n according to the new specification
    
    * feat: update account screen
    
    * feat: creating business-activity model
    
    * feat: creating company-info model
    
    * feat: creating revenue-target model

--- app/models/entities/account-holder/account-holder.ts
+++ app/models/entities/account-holder/account-holder.ts
@@ -1,25 +1,34 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
 
+import { BusinessActivityModel, createBusinessActivityDefaultModel } from '../business-activity/business-activity';
+import { CompanyInfoModel, createCompanyInfoDefaultModel } from '../company-info/company-info';
+import { ContactAddressModel, createContactAddressDefaultModel } from '../contact-address/contact-address';
+import { RevenueTargetModel } from '../revenue-target/revenue-target';
+
 export const AccountHolderModel = types.model('AccountHolder').props({
   id: types.maybe(types.maybeNull(types.string)),
   name: types.maybe(types.maybeNull(types.string)),
-  address: types.maybe(types.maybeNull(types.string)),
-  city: types.maybe(types.maybeNull(types.string)),
-  country: types.maybe(types.maybeNull(types.string)),
-  postalCode: types.maybe(types.maybeNull(types.string)),
+  siren: types.maybe(types.maybeNull(types.string)),
+  officialActivityName: types.maybe(types.maybeNull(types.string)),
+  contactAddress: types.maybe(types.maybeNull(ContactAddressModel)),
+  businessActivities: types.maybe(types.maybeNull(BusinessActivityModel)),
+  companyInfo: types.maybe(types.maybeNull(CompanyInfoModel)),
+  revenueTargets: types.maybe(types.array(RevenueTargetModel)),
 });
 
 export interface AccountHolder extends Instance<typeof AccountHolderModel> {}
 
 export interface AccountSnapshotOut extends SnapshotOut<typeof AccountHolderModel> {}
 
-export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {}
-
 export const createAccountDefaultModel = () =>
   types.optional(AccountHolderModel, {
     name: null,
-    address: null,
-    city: null,
-    country: null,
-    postalCode: null,
+    siren: null,
+    officialActivityName: null,
+    contactAddress: createContactAddressDefaultModel(),
+    businessActivities: createBusinessActivityDefaultModel(),
+    companyInfo: createCompanyInfoDefaultModel(),
+    revenueTargets: [],
   });
+
+export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {}
