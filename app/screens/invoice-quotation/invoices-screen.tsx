28fa74b932bdaf1a57c1b2d9dc8cff65b10a5add
Tahiry Randriamiarintsoa <tahirycontact@gmail.com>
2023-03-24 15:28:02 +0300

    Revert "chore: improve organization (#155)" (#162)
    This reverts commit f6c72a3ef7f18fd2c472744e95afe86e4432f552.

--- app/screens/invoice-quotation/invoices-screen.tsx
+++ app/screens/invoice-quotation/invoices-screen.tsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
 import { SectionList, View } from 'react-native';
 
-import { Screen, Separator, Text } from '../../components';
+import { Button, Screen, Separator, Text } from '../../components';
 import { MenuItem } from '../../components/menu/menu';
 import env from '../../config/env';
 import { translate } from '../../i18n';
@@ -17,10 +17,19 @@ import { fetchBinaryFile } from '../../utils/fetch-binary-file';
 import { showMessage } from '../../utils/snackbar';
 import { ErrorBoundary } from '../error/error-boundary';
 import { Invoice } from './components/invoice';
-import { CONTAINER, FOOTER_COMPONENT_STYLE, FULL, SECTION_HEADER_TEXT_STYLE, SECTION_LIST_CONTAINER_STYLE, SEPARATOR_STYLE } from './styles';
+import {
+  BUTTON_STYLE,
+  BUTTON_TEXT_STYLE,
+  CONTAINER,
+  FOOTER_COMPONENT_STYLE,
+  FULL,
+  SECTION_HEADER_TEXT_STYLE,
+  SECTION_LIST_CONTAINER_STYLE,
+  SEPARATOR_STYLE,
+} from './styles';
 import { sectionInvoicesByMonth } from './utils/section-quotation-by-month';
 
-export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'invoices'>> = observer(function InvoicesScreen() {
+export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'invoices'>> = observer(function InvoicesScreen({ navigation }) {
   const { invoiceStore, authStore } = useStores();
   const { invoices, loading } = invoiceStore;
   const { currentAccount, accessToken } = authStore;
