284c1d7daeac5d552507ef9f606653c4ba9abf2f
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-24 15:21:54 +0300

    chore: improve organization (#155)
    * chore: change the order to DESC in invoiceGroupedByMounth
    
    * chore: edit the message when a quote is created
    
    * chore: edit draft as option instead of edit invoice
    
    * chore: make the create quote button always visible

--- app/screens/invoice-quotation/invoices-screen.tsx
+++ app/screens/invoice-quotation/invoices-screen.tsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
 import { SectionList, View } from 'react-native';
 
-import { Button, Screen, Separator, Text } from '../../components';
+import { Screen, Separator, Text } from '../../components';
 import { MenuItem } from '../../components/menu/menu';
 import env from '../../config/env';
 import { translate } from '../../i18n';
@@ -17,19 +17,10 @@ import { fetchBinaryFile } from '../../utils/fetch-binary-file';
 import { showMessage } from '../../utils/snackbar';
 import { ErrorBoundary } from '../error/error-boundary';
 import { Invoice } from './components/invoice';
-import {
-  BUTTON_STYLE,
-  BUTTON_TEXT_STYLE,
-  CONTAINER,
-  FOOTER_COMPONENT_STYLE,
-  FULL,
-  SECTION_HEADER_TEXT_STYLE,
-  SECTION_LIST_CONTAINER_STYLE,
-  SEPARATOR_STYLE,
-} from './styles';
+import { CONTAINER, FOOTER_COMPONENT_STYLE, FULL, SECTION_HEADER_TEXT_STYLE, SECTION_LIST_CONTAINER_STYLE, SEPARATOR_STYLE } from './styles';
 import { sectionInvoicesByMonth } from './utils/section-quotation-by-month';
 
-export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'invoices'>> = observer(function InvoicesScreen({ navigation }) {
+export const InvoicesScreen: FC<MaterialTopTabScreenProps<NavigatorParamList, 'invoices'>> = observer(function InvoicesScreen() {
   const { invoiceStore, authStore } = useStores();
   const { invoices, loading } = invoiceStore;
   const { currentAccount, accessToken } = authStore;
