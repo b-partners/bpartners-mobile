5231cbaae6fdb3c5218f8759d954e7ed2fb7cc64
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-27 13:57:52 +0300

    feat: implement prospect-screen (#164)
    * feat: add prospect-api
    
    * feat: add prospect screen
    
    * feat: add prospect filter
    
    * fix: show the prospect menu in the drawer
    
    * feat: adding location in the prospect-screen
    
    * chore: improve the UI on prospect-screen
    
    * chore: create component when no data is provided and using it on the prospect-screen
    
    * fix: fix bugs after resolving conflicts
    
    * style: run prettier
    
    ---------
    
    Co-authored-by: lianafinaritra <hei.lianafinaritra@gmail.com>

---
+++ app/models/stores/prospect-store/prospect-store.ts
@@ -0,0 +1,47 @@
+import { Instance, SnapshotIn, SnapshotOut, flow, types } from 'mobx-state-tree';
+
+import { ProspectApi } from '../../../services/api/prospect-api';
+import { ProspectModel, ProspectSnapshotOut } from '../../entities/prospect/prospect';
+import { withCredentials } from '../../extensions/with-credentials';
+import { withEnvironment } from '../../extensions/with-environment';
+import { withRootStore } from '../../extensions/with-root-store';
+
+export const ProspectStoreModel = types
+  .model('Prospect')
+  .props({
+    prospects: types.optional(types.array(ProspectModel), []),
+  })
+  .extend(withRootStore)
+  .extend(withEnvironment)
+  .extend(withCredentials)
+  .actions(self => ({
+    catchOrThrow: (error: Error) => self.rootStore.authStore.catchOrThrow(error),
+  }))
+  .actions(self => ({
+    getProspectsSuccess: (prospectSnapshotOuts: ProspectSnapshotOut[]) => {
+      self.prospects.replace(prospectSnapshotOuts);
+    },
+  }))
+  .actions(() => ({
+    getProspectsFail: error => {
+      __DEV__ && console.tron.log(error);
+    },
+  }))
+  .actions(self => ({
+    getProspects: flow(function* () {
+      const prospectApi = new ProspectApi(self.environment.api);
+      try {
+        const getProspectsResult = yield prospectApi.getProspects(self.currentAccountHolder.id);
+        self.getProspectsSuccess(getProspectsResult.prospects);
+      } catch (e) {
+        self.getProspectsFail(e.message);
+        self.catchOrThrow(e);
+      }
+    }),
+  }));
+
+export interface ProspectStore extends Instance<typeof ProspectStoreModel> {}
+
+export interface ProspectStoreSnapshotOut extends SnapshotOut<typeof ProspectStoreModel> {}
+
+export interface ProspectStoreSnapshotIn extends SnapshotIn<typeof ProspectStoreModel> {}
