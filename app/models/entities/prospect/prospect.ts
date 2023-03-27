fd584170d199751e67f4092d5c713856b14b3ba5
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-03-27 13:57:52 +0300

    feat: implement prospect-screen (#164)
    * feat: add prospect-api
    
    * feat: add prospect screen
    
    * feat: add prospect filter
    
    * fix: show the prospect menu in the drawer
    
    * feat: adding location in the prospect-screen
    
    * chore: improve the UI on prospect-screen
    
    * chore: create component when no data is provided and using it on the prospect-screen
    
    * fix: fix bugs after resolving conflicts
    
    * style: run prettier
    
    ---------
    
    Co-authored-by: lianafinaritra <hei.lianafinaritra@gmail.com>

---
+++ app/models/entities/prospect/prospect.ts
@@ -0,0 +1,29 @@
+import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
+
+export enum ProspectStatus {
+  TO_CONTACT = 'TO_CONTACT',
+  CONTACTED = 'CONTACTED',
+  CONVERTED = 'CONVERTED',
+}
+
+export const LocationModel = types.model('Location').props({
+  type: types.maybeNull(types.string),
+  longitude: types.maybeNull(types.number),
+  latitude: types.maybeNull(types.number),
+});
+
+export const ProspectModel = types.model('Account').props({
+  id: types.maybeNull(types.string),
+  name: types.maybeNull(types.string),
+  email: types.maybeNull(types.string),
+  phone: types.maybeNull(types.string),
+  address: types.maybeNull(types.string),
+  status: types.maybeNull(types.enumeration(Object.values(ProspectStatus))),
+  location: types.maybeNull(LocationModel),
+});
+
+export interface Prospect extends Instance<typeof ProspectModel> {}
+
+export interface ProspectSnapshotOut extends SnapshotOut<typeof ProspectModel> {}
+
+export interface ProspectSnapshotIn extends SnapshotIn<typeof ProspectModel> {}
