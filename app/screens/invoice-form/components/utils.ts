eec6474b00f6b72aa995f8678b255996ed8b4073
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-27 13:46:44 +0300

    refactor: invoice style

--- app/screens/invoice-form/components/utils.ts
+++ app/screens/invoice-form/components/utils.ts
@@ -1,12 +1,6 @@
 import React from "react";
-import {ViewStyle} from "react-native";
 
 export type ModalProps = {
     visibleModal: boolean;
     setVisibleModal: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-export const INVALID_FORM_FIELD: ViewStyle = {
-    borderBottomColor: '#FF5983',
-    borderBottomWidth: 2,
 };
