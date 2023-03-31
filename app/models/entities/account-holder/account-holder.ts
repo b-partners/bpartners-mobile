ed261d2449b89ac94e049dba747a6ce66338c35e
Tahiry Randriamiarintsoa <tahirycontact@gmail.com>
2023-03-31 19:50:50 +0300

    Revert "feat: add new attributes to the profile (#156)"
    This reverts commit 89dae07428599a3dc85d988e42c0669289128544.

--- app/models/entities/account-holder/account-holder.ts
+++ app/models/entities/account-holder/account-holder.ts
@@ -1,34 +1,25 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from 'mobx-state-tree';
 
-import { BusinessActivityModel, createBusinessActivityDefaultModel } from '../business-activity/business-activity';
-import { CompanyInfoModel, createCompanyInfoDefaultModel } from '../company-info/company-info';
-import { ContactAddressModel, createContactAddressDefaultModel } from '../contact-address/contact-address';
-import { RevenueTargetModel } from '../revenue-target/revenue-target';
-
 export const AccountHolderModel = types.model('AccountHolder').props({
   id: types.maybe(types.maybeNull(types.string)),
   name: types.maybe(types.maybeNull(types.string)),
-  siren: types.maybe(types.maybeNull(types.string)),
-  officialActivityName: types.maybe(types.maybeNull(types.string)),
-  contactAddress: types.maybe(types.maybeNull(ContactAddressModel)),
-  businessActivities: types.maybe(types.maybeNull(BusinessActivityModel)),
-  companyInfo: types.maybe(types.maybeNull(CompanyInfoModel)),
-  revenueTargets: types.maybe(types.array(RevenueTargetModel)),
+  address: types.maybe(types.maybeNull(types.string)),
+  city: types.maybe(types.maybeNull(types.string)),
+  country: types.maybe(types.maybeNull(types.string)),
+  postalCode: types.maybe(types.maybeNull(types.string)),
 });
 
 export interface AccountHolder extends Instance<typeof AccountHolderModel> {}
 
 export interface AccountSnapshotOut extends SnapshotOut<typeof AccountHolderModel> {}
 
+export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {}
+
 export const createAccountDefaultModel = () =>
   types.optional(AccountHolderModel, {
     name: null,
-    siren: null,
-    officialActivityName: null,
-    contactAddress: createContactAddressDefaultModel(),
-    businessActivities: createBusinessActivityDefaultModel(),
-    companyInfo: createCompanyInfoDefaultModel(),
-    revenueTargets: [],
+    address: null,
+    city: null,
+    country: null,
+    postalCode: null,
   });
-
-export interface AccountSnapshotIn extends SnapshotIn<typeof AccountHolderModel> {}
