9859eb63393e84159481d911d07c2b93125920a9
Tahiry Randriamiarintsoa <tahirycontact@gmail.com>
2023-03-31 19:50:50 +0300

    Revert "Revert "feat: profile (#130)""
    This reverts commit e6674f3f13cf6ec2fae50b5741f0bbc3d1bfe345.

---
+++ app/models/entities/business-activity/business-activity.test.ts
@@ -0,0 +1,10 @@
+import { BusinessActivityModel } from './business-activity';
+
+test('can be created', () => {
+  const instance = BusinessActivityModel.create({
+    primary: 'agenceur',
+    secondary: 'architecte',
+  });
+
+  expect(instance).toBeTruthy();
+});
