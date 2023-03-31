ceb0daedb4722cbf9927e4359bdda6cf1bfbd3e9
Tahiry Randriamiarintsoa <tahirycontact@gmail.com>
2023-03-31 19:50:50 +0300

    Revert "feat: profile (#130)"
    This reverts commit 7131dafd9b63ba5f3714b222c4c5fcf2863a14cc.

--- app/models/entities/company-info/company-info.test.ts
+++
@@ -1,9 +0,0 @@
-import { CompanyInfoModel } from './company-info';
-
-test('can be created', () => {
-  const instance = CompanyInfoModel.create({
-    socialCapital: 0,
-  });
-
-  expect(instance).toBeTruthy();
-});
