0399fdd5e28a35b29fe21d64012cfea5f236d421
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-04-05 10:47:05 +0300

    chore: change fileIcon border to white

--- app/screens/support-contact/support-contact-screen.tsx
+++ app/screens/support-contact/support-contact-screen.tsx
@@ -2,6 +2,7 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
 import { View, ViewStyle } from 'react-native';
+import ZocialIcon from 'react-native-vector-icons/Zocial';
 
 import { Header, Screen, Text } from '../../components';
 import { translate } from '../../i18n';
@@ -9,8 +10,7 @@ import { TabNavigatorParamList } from '../../navigators';
 import { color, spacing } from '../../theme';
 import { palette } from '../../theme/palette';
 import { ErrorBoundary } from '../error/error-boundary';
-import {SCREEN_STYLE} from "../marketplace/styles";
-import ZocialIcon from 'react-native-vector-icons/Zocial';
+import { SCREEN_STYLE } from '../marketplace/styles';
 
 const CONTAINER: ViewStyle = {
   backgroundColor: color.transparent,
@@ -31,43 +31,49 @@ export const SupportContactScreen: FC<StackScreenProps<TabNavigatorParamList, 's
           }}
         />
         <Screen style={CONTAINER} preset='auto' backgroundColor={palette.white}>
+          <View
+            style={{
+              width: '100%',
+              height: 150,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
             <View
-                style={{
-                    width: '100%',
-                    height: 150,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderWidth: 1,
+                borderColor: palette.white,
+                borderRadius: 70,
+                backgroundColor: '#EEF0F4',
+                height: 100,
+                width: 100,
+              }}
             >
-                <View
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        borderWidth: 1,
-                        borderColor: palette.white,
-                        borderRadius: 70,
-                        backgroundColor: '#EEF0F4',
-                        height: 100,
-                        width: 100
-                    }}
-                >
-                    <ZocialIcon name='email' size={70} color='#D3D9DD' />
-                </View>
-                <Text
-                    tx={'supportContactScreen.mail'}
-                    style={{
-                        color: palette.lightGrey,
-                        marginTop: 2,
-                        fontSize: 13,
-                        fontFamily: 'Geometria'
-                    }}
-                />
+              <ZocialIcon name='email' size={70} color='#D3D9DD' />
             </View>
-            <Text text={translate('supportContactScreen.messagePartOne')} style={{ color: palette.lightGrey, textAlign: 'center', lineHeight: 22, fontFamily: 'Geometria' }} />
-            <Text text={`${translate('supportContactScreen.messagePartTwo')} ${translate('supportContactScreen.mail')}`} style={{ color: palette.lightGrey, textAlign: 'center', lineHeight: 22, fontFamily: 'Geometria' }} />
+            <Text
+              tx={'supportContactScreen.mail'}
+              style={{
+                color: palette.lightGrey,
+                marginTop: 2,
+                fontSize: 13,
+                fontFamily: 'Geometria',
+              }}
+            />
+          </View>
+          <Text
+            text={translate('supportContactScreen.messagePartOne')}
+            style={{ color: palette.lightGrey, textAlign: 'center', lineHeight: 22, fontFamily: 'Geometria' }}
+          />
+          <Text
+            text={`${translate('supportContactScreen.messagePartTwo')} ${translate('supportContactScreen.mail')}`}
+            style={{ color: palette.lightGrey, textAlign: 'center', lineHeight: 22, fontFamily: 'Geometria' }}
+          />
         </Screen>
       </View>
     </ErrorBoundary>
