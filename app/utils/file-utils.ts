f819485812185af7357594989fcbe2f0ea86fe86
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-04-06 14:59:13 +0300

    feat: creating utility for creating file url

--- app/utils/file-utils.ts
+++ app/utils/file-utils.ts
@@ -1,5 +1,7 @@
 import RNFS from 'react-native-fs';
 
+import env from '../config/env';
+
 type DownloadOptions = { url: string; accessToken?: string; fileName: string };
 
 export const fetchBinaryFiles = async (options: DownloadOptions) => {
@@ -9,4 +11,8 @@ export const fetchBinaryFiles = async (options: DownloadOptions) => {
     fromUrl: url,
     toFile: `${RNFS.DownloadDirectoryPath}/${fileName}`,
   }).promise;
+};
+
+export const createFileUrl = (fileId: string, accountId: string, accessToken: string, fileType: string, baseUrl = env.apiBaseUrl) => {
+  return `${baseUrl}/accounts/${accountId}/files/${fileId}/raw?accessToken=${accessToken}&fileType=${fileType}`;
 };
