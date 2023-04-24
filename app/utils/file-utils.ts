1130c3fea9aa1beb8bfcdcb444cb3878b671cddc
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-04-24 21:08:45 +0300

    feat: adding utility for downloading file

--- app/utils/file-utils.ts
+++ app/utils/file-utils.ts
@@ -1,3 +1,4 @@
+import ReactNativeBlobUtil, { FetchBlobResponse } from 'react-native-blob-util';
 import RNFS from 'react-native-fs';
 
 import env from '../config/env';
@@ -11,6 +12,39 @@ export const fetchBinaryFiles = async (options: DownloadOptions) => {
     fromUrl: url,
     toFile: `${RNFS.DownloadDirectoryPath}/${fileName}`,
   }).promise;
+};
+
+type DownloadOptionsV2 = { mimeType?: string; temp?: boolean } & DownloadOptions;
+export const fetchBinaryFileV2 = async (options: DownloadOptionsV2) => {
+  const { url, fileName, mimeType, temp = false } = options;
+  const dirs = ReactNativeBlobUtil.fs.dirs;
+  let downloadedFilePath = null;
+
+  ReactNativeBlobUtil.config({
+    fileCache: true,
+    path: dirs.DownloadDir + fileName,
+    overwrite: true,
+  })
+    .fetch('GET', url, {})
+    .then(async (res: FetchBlobResponse) => {
+      if (temp){
+        downloadedFilePath=res.path();
+        return
+      }
+
+      const result = await ReactNativeBlobUtil.MediaCollection.copyToMediaStore(
+        {
+          name: fileName,
+          parentFolder: '',
+          mimeType,
+        },
+        'Download', // Media Collection to store the file in ("Audio" | "Image" | "Video" | "Download")
+        res.path()
+      );
+      downloadedFilePath = result;
+    });
+
+  return downloadedFilePath;
 };
 
 export const createFileUrl = (fileId: string, accountId: string, accessToken: string, fileType: string, baseUrl = env.apiBaseUrl) => {
