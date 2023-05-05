d994117ba508385e79068de0400adb02cd8b6e3a
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-05-05 14:04:03 +0300

    feat: update fr.json and en.json

--- app/screens/welcome/welcome-screen.tsx
+++ app/screens/welcome/welcome-screen.tsx
@@ -7,10 +7,11 @@ import { observer } from 'mobx-react-lite';
 import React, { FC, useEffect, useState } from 'react';
 import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import * as Keychain from 'react-native-keychain';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
 import * as yup from 'yup';
 
 import awsExports from '../../../src/aws-exports';
-import {AutoImage, Button, Icon, Loader, Screen, Text} from '../../components';
+import { AutoImage, Button, Icon, Loader, Screen, Text } from '../../components';
 import env from '../../config/env';
 import { translate } from '../../i18n/translate';
 import { useStores } from '../../models';
@@ -20,7 +21,6 @@ import { palette } from '../../theme/palette';
 import { showMessage } from '../../utils/snackbar';
 import { ErrorBoundary } from '../error/error-boundary';
 import KeyboardAvoidingWrapper from './keyboardAvoidingWrapper';
-import IoniconIcon from 'react-native-vector-icons/Ionicons';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -147,53 +147,56 @@ export const WelcomeScreen: FC<DrawerScreenProps<NavigatorParamList, 'oauth'>> =
                   </View>
                   <View style={styles.field}>
                     <Text tx='welcomeScreen.password' style={styles.label} />
-                    <View style={{ width: '100%', flexDirection: 'row', borderWidth: 1,
-                      borderColor: '#ccc',
-                      borderRadius: 5 }}>
-                    <TextInput
-                      style={{
+                    <View style={{ width: '100%', flexDirection: 'row', borderWidth: 1, borderColor: '#ccc', borderRadius: 5 }}>
+                      <TextInput
+                        style={{
                           backgroundColor: '#fff',
                           padding: 10,
-                        width: '75%'
+                          width: '75%',
                         }}
-                      onChangeText={handleChange('password')}
-                      onBlur={handleBlur('password')}
-                      defaultValue={userDetails.username !== null ? userDetails.password : values.password}
-                      secureTextEntry={showPassword}
-                    />
-                      <View style={{ width: '25%', backgroundColor: '#fff', justifyContent: 'center',   alignItems: 'center' }}>
-                        { showPassword ? <IoniconIcon name='eye-off-outline' size={28} color={color.palette.secondaryColor} onPress={() => toggleShowPassword()}/> :
-                            <IoniconIcon name='eye-sharp' size={28} color={color.palette.secondaryColor} onPress={() => toggleShowPassword()}/>
-                        }
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        defaultValue={userDetails.username !== null ? userDetails.password : values.password}
+                        secureTextEntry={showPassword}
+                      />
+                      <View style={{ width: '25%', backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
+                        {showPassword ? (
+                          <IoniconIcon name='eye-off-outline' size={28} color={color.palette.secondaryColor} onPress={() => toggleShowPassword()} />
+                        ) : (
+                          <IoniconIcon name='eye-sharp' size={28} color={color.palette.secondaryColor} onPress={() => toggleShowPassword()} />
+                        )}
                       </View>
                     </View>
                     {errors.password && touched.password && <Text style={styles.error}>{errors.password}</Text>}
                   </View>
                   <Button
-                      onPress={async () => {
-                        await signIn(values.email, values.password);
-                      }}
-                      style={{
-                        borderRadius: 50,
-                        paddingVertical: spacing[3],
-                        backgroundColor: '#fff',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        marginTop: spacing[4],
-                      }}
+                    onPress={async () => {
+                      await signIn(values.email, values.password);
+                    }}
+                    style={{
+                      borderRadius: 50,
+                      paddingVertical: spacing[3],
+                      backgroundColor: '#fff',
+                      display: 'flex',
+                      flexDirection: 'row',
+                      marginTop: spacing[4],
+                    }}
                   >
-                    {loading ? <Loader size={25}/> :
-                        <>
-                      <Text
-                        tx='welcomeScreen.login'
-                        style={{
-                          color: color.palette.secondaryColor,
-                          fontFamily: 'Geometria-Bold',
-                          marginRight: spacing[2],
-                        }}
-                    />
-                      <Icon icon='user'/>
-                        </>}
+                    {loading ? (
+                      <Loader size={25} />
+                    ) : (
+                      <>
+                        <Text
+                          tx='welcomeScreen.login'
+                          style={{
+                            color: color.palette.secondaryColor,
+                            fontFamily: 'Geometria-Bold',
+                            marginRight: spacing[2],
+                          }}
+                        />
+                        <Icon icon='user' />
+                      </>
+                    )}
                   </Button>
                 </View>
               )}
