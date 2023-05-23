a1e90269e37977ee1283d30996afa233b3ca38f8
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-05-23 11:36:11 +0300

    feat: wrapping mail sender lib by a utils function

--- app/utils/email.ts
+++ app/utils/email.ts
@@ -1,67 +1,27 @@
-// TODO: mae this more extensible
-/*
-import ReactNativeBlobUtil from 'react-native-blob-util';
 import Mailer from 'react-native-mail';
 
-import { translate } from '../i18n';
-import { Invoice } from '../models/entities/invoice/invoice';
-import { createFileUrl } from './file-utils';
-
-type TEmail = {
-  recipients: string[];
-  subject: string;
-  body: string;
-};
 type TAttachment = {
-  fileName: string;
+  path?: string; // Specify either 'path' or 'uri'
+  uri?: string;
+  type?: string; // Specify either 'type' or 'mimeType'
+  mimeType?: string;
+  name?: string;
 };
-type TSendEmail = {
-  invoice: Invoice;
-  email: TEmail;
-  attachment?: TAttachment;
+
+type SendEmailParams = {
+  subject?: string;
+  recipients?: string[];
+  ccRecipients?: string[];
+  bccRecipients?: string[];
+  body?: string;
+  customChooserTitle?: string;
+  isHTML?: boolean;
+  attachments?: TAttachment[];
 };
 
-*/
-/*export function sendEmail(params: TSendEmail) {
-  const { email, invoice, attachment } = params;
-  const { subject, recipients, body } = params.email;
-
-  const dirs = ReactNativeBlobUtil.fs.dirs;
-  let downloadedFilePath = null;
-
-  ReactNativeBlobUtil.config({
-    fileCache: true,
-    path: dirs.DownloadDir + `/${fileName}`,
-    overwrite: true,
-  })
-    .fetch('GET', invoiceUrl, {})
-    .then(res => {
-      __DEV__ && console.tron.log('The file saved to ', res.path());
-      downloadedFilePath = res.path();
-      const email = {
-        subject: `${translate('invoicePreviewScreen.invoice')} ${sub}`,
-        recipients: [customer.email],
-        // TODO add current account holder email
-        ccRecipients: [],
-        body: `<p>${translate('invoicePreviewScreen.email.body')}</p>`,
-        isHTML: true,
-        attachments: [
-          {
-            path: downloadedFilePath,
-            type: 'pdf',
-            name: fileName,
-          },
-        ],
-      };
-
-      // Open mail client and preload with some default values
-      // and pass as attachment the pdf
-      Mailer.mail(email, error => {
-        if (error) {
-          __DEV__ && console.tron.error('Could not send email: ' + error, error);
-        } else {
-          __DEV__ && console.tron.log('Email sent successfully');
-        }
-      });
-    });
-}*/
+export const sendEmail = (email: SendEmailParams) => {
+  Mailer.mail({ ...email }, error => {
+    __DEV__ && console.tron.error(error, []);
+    throw new Error('An error occurred while sending mail ' + error);
+  });
+};
