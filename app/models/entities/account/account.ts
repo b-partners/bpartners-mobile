72cbb466b744c0264d579207a57a557352ab7942
tahiry-dev <tahirycontact@gmail.com>
2023-05-31 11:38:41 +0300

    fix: update account model to fetch value from api

--- app/models/entities/account/account.ts
+++ app/models/entities/account/account.ts
@@ -4,8 +4,8 @@ export const AccountModel = types.model('Account').props({
   id: types.maybe(types.maybeNull(types.string)),
   availableBalance: types.maybe(types.maybeNull(types.number)),
   name: types.maybe(types.maybeNull(types.string)),
-  IBAN: types.maybe(types.maybeNull(types.string)),
-  BIC: types.maybe(types.maybeNull(types.string)),
+  iban: types.maybe(types.maybeNull(types.string)),
+  bic: types.maybe(types.maybeNull(types.string)),
 });
 
 export interface Account extends Instance<typeof AccountModel> {}
