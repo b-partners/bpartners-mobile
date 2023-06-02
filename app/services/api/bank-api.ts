dee98128dceea38a143a47fff022ee70b64d392e
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-06-02 14:19:11 +0300

    feat: implement update bank information in api

--- app/services/api/bank-api.ts
+++ app/services/api/bank-api.ts
@@ -3,6 +3,7 @@ import { ApiResponse } from 'apisauce';
 import { Api } from './api';
 import { getGeneralApiProblem } from './api-problem';
 import { BankConnection } from './api.types';
+import {Account} from "../../models/entities/account/account";
 
 export class BankApi {
   private api: Api;
@@ -13,6 +14,17 @@ export class BankApi {
 
   async connectToBank(userId: string, accountId: string): Promise<BankConnection> {
     const response: ApiResponse<BankConnection> = await this.api.apisauce.post(`users/${userId}/accounts/${accountId}/initiateBankConnection`);
+
+    if (!response.ok) {
+      const problem = getGeneralApiProblem(response);
+      if (problem) throw new Error(problem.kind);
+    }
+
+    return response.data;
+  }
+
+  async updateBankInfos(userId: string, accountId: string): Promise<Account> {
+    const response: ApiResponse<Account> = await this.api.apisauce.put(`users/${userId}/accounts/${accountId}/identity`);
 
     if (!response.ok) {
       const problem = getGeneralApiProblem(response);
