0e84618d3fd94d9d728c16ef63bd6710f61d4da2
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-06-09 11:33:30 +0300

    feat: implement validation in registraion form

--- app/screens/registration/components/input-field.tsx
+++ app/screens/registration/components/input-field.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
+import ErrorMessage from '../../../components/forms/error-message';
 import { TxKeyPath, translate } from '../../../i18n';
+import { color } from '../../../theme';
 import { palette } from '../../../theme/palette';
 
 interface InputFieldProps {
@@ -9,27 +12,31 @@ interface InputFieldProps {
   error: boolean;
   value: string;
   onChange: ((text: string) => void) & Function;
+  errorMessage: string;
 }
 
-export const InputField = ({ labelTx, error, value, onChange }: InputFieldProps) => {
+export const InputField = ({ labelTx, error, value, onChange, errorMessage }: InputFieldProps) => {
   return (
-    <TextInput
-      label={translate(labelTx)}
-      error={error}
-      textColor={palette.secondaryColor}
-      selectionColor={palette.secondaryColor}
-      value={value}
-      onChangeText={onChange}
-      style={{
-        backgroundColor: 'white',
-        borderRadius: 5,
-      }}
-      theme={{
-        colors: {
-          primary: palette.greyDarker,
-        },
-      }}
-    />
+    <View>
+      <TextInput
+        label={translate(labelTx)}
+        error={error}
+        textColor={palette.secondaryColor}
+        selectionColor={palette.secondaryColor}
+        value={value}
+        onChangeText={onChange}
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 5,
+        }}
+        theme={{
+          colors: {
+            primary: palette.greyDarker,
+          },
+        }}
+      />
+      <ErrorMessage name={'error'} error={errorMessage} visible={error} style={{ color: color.error }} />
+    </View>
   );
 };
 
