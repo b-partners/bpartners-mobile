d8c7e6d9393b7219305b3fdced38697adc5864f5
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-06-29 11:02:41 +0300

    refactor: drafts pagination

--- app/screens/invoice-quotation/drafts-screen.tsx
+++ app/screens/invoice-quotation/drafts-screen.tsx
@@ -1,9 +1,11 @@
 import { MaterialTopTabScreenProps } from '@react-navigation/material-top-tabs';
 import { observer } from 'mobx-react-lite';
 import React, { FC, useState } from 'react';
-import { SectionList, View } from 'react-native';
+import { SectionList, TouchableOpacity, View } from 'react-native';
+import { Snackbar } from 'react-native-paper';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 
-import { Loader, Screen, Separator, Text } from '../../components';
+import { Button, Loader, Screen, Separator, Text } from '../../components';
 import { MenuItem } from '../../components/menu/menu';
 import { translate } from '../../i18n';
 import { useStores } from '../../models';
@@ -14,11 +16,17 @@ import { palette } from '../../theme/palette';
 import { capitalizeFirstLetter } from '../../utils/capitalizeFirstLetter';
 import { showMessage } from '../../utils/snackbar';
 import { ErrorBoundary } from '../error/error-boundary';
-import { invoicePageSize, itemsPerPage } from '../invoice-form/components/utils';
 import { Invoice } from './components/invoice';
-import { InvoiceCreationButton } from './components/invoice-creation-button';
-import { InvoicePagination } from './components/invoice-pagination';
-import { FOOTER_COMPONENT_STYLE, FULL, LOADER_STYLE, SECTION_HEADER_TEXT_STYLE, SECTION_LIST_CONTAINER_STYLE, SEPARATOR_STYLE } from './styles';
+import {
+  BUTTON_INVOICE_STYLE,
+  BUTTON_TEXT_STYLE,
+  FOOTER_COMPONENT_STYLE,
+  FULL,
+  LOADER_STYLE,
+  SECTION_HEADER_TEXT_STYLE,
+  SECTION_LIST_CONTAINER_STYLE,
+  SEPARATOR_STYLE,
+} from './styles';
 import { navigateToTab } from './utils/reset-tab-navigation';
 import { sectionInvoicesByMonth } from './utils/section-quotation-by-month';
 
@@ -26,24 +34,29 @@ export const DraftsScreen: FC<MaterialTopTabScreenProps<TabNavigatorParamList, '
   const { invoiceStore, draftStore, quotationStore } = useStores();
   const { drafts, loadingDraft } = draftStore;
   const [navigationState, setNavigationState] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [maxPage, setMaxPage] = useState(Math.ceil(drafts.length / itemsPerPage));
+  /*const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(Math.ceil(allDrafts.length / 10));*/
   const messageOption = { backgroundColor: palette.green };
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
   const startItemIndex = (currentPage - 1) * itemsPerPage;
   const endItemIndex = currentPage * itemsPerPage;
   const displayedItems = drafts.slice(startItemIndex, endItemIndex);
 
   const handleRefresh = async () => {
-    await draftStore.getDrafts({ page: 1, pageSize: invoicePageSize, status: InvoiceStatus.DRAFT });
-    setMaxPage(Math.ceil(drafts.length / itemsPerPage));
+    await draftStore.getDrafts({ page: 1, pageSize: 500, status: InvoiceStatus.DRAFT });
   };
 
-  const handleScroll = event => {
-    const offsetY = event.nativeEvent.contentOffset.y;
-    if (offsetY <= -5) {
-      handleRefresh();
-    }
-  };
+  /*useEffect(() => {
+    setPage(1);
+    setMaxPage(Math.ceil(allDrafts.length / 10));
+  }, [allDrafts]);
+
+  useEffect(() => {
+    draftStore.getDrafts({ page: page, pageSize: 10, status: InvoiceStatus.DRAFT });
+  }, [page]);*/
 
   const editInvoice = async (item: IInvoice) => {
     setNavigationState(true);
@@ -51,8 +64,7 @@ export const DraftsScreen: FC<MaterialTopTabScreenProps<TabNavigatorParamList, '
       return;
     }
     try {
-      const currentInvoice = await invoiceStore.getInvoice(item.id);
-      __DEV__ && console.tron.log(currentInvoice);
+      await invoiceStore.getInvoice(item.id);
       invoiceStore.saveInvoiceInit();
       navigation.navigate('invoiceForm', { invoiceID: item.id, initialStatus: InvoiceStatus.DRAFT });
     } catch (e) {
@@ -87,12 +99,15 @@ export const DraftsScreen: FC<MaterialTopTabScreenProps<TabNavigatorParamList, '
       };
       navigateToTab(navigation, 'quotations');
       await invoiceStore.saveInvoice(editedItem);
-      await quotationStore.getQuotations({ page: 1, pageSize: invoicePageSize, status: InvoiceStatus.PROPOSAL });
+      await quotationStore.getQuotations({ page: 1, pageSize: 10, status: InvoiceStatus.PROPOSAL });
       setNavigationState(false);
-      await draftStore.getDrafts({ page: 1, pageSize: invoicePageSize, status: InvoiceStatus.DRAFT });
+      await draftStore.getDrafts({ page: 1, pageSize: 10, status: InvoiceStatus.DRAFT });
       showMessage(translate('invoiceScreen.messages.successfullyMarkAsProposal'), messageOption);
     } catch (e) {
       __DEV__ && console.tron.log(`Failed to convert invoice, ${e}`);
+    } finally {
+      await quotationStore.getAllQuotations({ status: InvoiceStatus.PROPOSAL, page: 1, pageSize: 500 });
+      await draftStore.getAllDrafts({ status: InvoiceStatus.DRAFT, page: 1, pageSize: 500 });
     }
   };
   const items: MenuItem[] = [
@@ -131,7 +146,6 @@ export const DraftsScreen: FC<MaterialTopTabScreenProps<TabNavigatorParamList, '
                 stickySectionHeadersEnabled={true}
                 ItemSeparatorComponent={() => <Separator style={SEPARATOR_STYLE} />}
                 renderSectionFooter={() => <View style={FOOTER_COMPONENT_STYLE} />}
-                onScrollEndDrag={handleScroll}
               />
             </View>
           </Screen>
@@ -139,8 +153,67 @@ export const DraftsScreen: FC<MaterialTopTabScreenProps<TabNavigatorParamList, '
           <Loader size='large' containerStyle={LOADER_STYLE} />
         )}
         <View style={{ flexDirection: 'row', marginTop: spacing[2], height: 80 }}>
-          <InvoicePagination maxPage={maxPage} page={currentPage} setPage={setCurrentPage} />
-          <InvoiceCreationButton navigation={navigation} navigationState={navigationState} setNavigationState={setNavigationState} />
+          <View style={{ width: '25%', alignItems: 'center', flexDirection: 'row', height: '100%', justifyContent: 'space-evenly' }}>
+            {currentPage === 1 ? (
+              <View style={{ width: '35%', height: '80%', justifyContent: 'center', alignItems: 'center' }}>
+                <EntypoIcon name='chevron-thin-left' size={27} color={palette.lighterGrey} />
+              </View>
+            ) : (
+              <TouchableOpacity
+                style={{ width: '35%', height: '80%', justifyContent: 'center', alignItems: 'center' }}
+                onPress={() => {
+                  setCurrentPage(currentPage - 1);
+                }}
+              >
+                <EntypoIcon name='chevron-thin-left' size={25} color='#000' />
+              </TouchableOpacity>
+            )}
+            <View style={{ width: '30%', height: '80%', justifyContent: 'center', alignItems: 'center' }}>
+              <Text text={currentPage.toString()} style={{ fontSize: 20, fontWeight: '600', color: palette.textClassicColor }} />
+            </View>
+            {currentPage === drafts.length ? (
+              <View style={{ width: '35%', height: '80%', justifyContent: 'center', alignItems: 'center' }}>
+                <EntypoIcon name='chevron-thin-right' size={27} color={palette.lighterGrey} />
+              </View>
+            ) : (
+              <TouchableOpacity
+                style={{ width: '35%', height: '80%', justifyContent: 'center', alignItems: 'center' }}
+                onPress={() => {
+                  setCurrentPage(currentPage + 1);
+                }}
+              >
+                <EntypoIcon name='chevron-thin-right' size={25} color='#000' />
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={{ width: '75%', justifyContent: 'center' }}>
+            {navigationState ? (
+              <Snackbar
+                visible={navigationState}
+                elevation={5}
+                style={{
+                  backgroundColor: palette.yellow,
+                  marginVertical: spacing[5],
+                  marginHorizontal: spacing[4],
+                  borderRadius: 40,
+                  paddingHorizontal: spacing[2],
+                }}
+                onDismiss={() => setNavigationState(false)}
+              >
+                {translate('common.loading')}
+              </Snackbar>
+            ) : (
+              <Button
+                tx='quotationScreen.createQuotation'
+                style={BUTTON_INVOICE_STYLE}
+                textStyle={BUTTON_TEXT_STYLE}
+                onPress={() => {
+                  invoiceStore.saveInvoiceInit();
+                  navigation.navigate('invoiceForm');
+                }}
+              />
+            )}
+          </View>
         </View>
       </View>
     </ErrorBoundary>
