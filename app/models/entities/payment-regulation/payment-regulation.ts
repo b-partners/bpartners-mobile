f9456e5915b1a8dbcb480fb88ada010649058f3c
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-05 16:44:19 +0300

    feat: implement payment regulation creation

--- app/models/entities/payment-regulation/payment-regulation.ts
+++ app/models/entities/payment-regulation/payment-regulation.ts
@@ -16,6 +16,9 @@ export const PaymentRequestModel = types.model('PaymentRequest').props({
 export const PaymentRegulationModel = types.model('PaymentRegulation').props({
   paymentRequest: types.maybe(PaymentRequestModel),
   maturityDate: types.maybeNull(types.string),
+  comment: types.maybeNull(types.string),
+  percent: types.maybeNull(types.number),
+  amount: types.maybeNull(types.number),
 });
 
 export interface PaymentRegulation extends Instance<typeof PaymentRegulationModel> {}
@@ -25,4 +28,20 @@ export interface PaymentRegulationSnapshotOut extends SnapshotOut<typeof Payment
 
 export interface PaymentRegulationSnapshotIn extends SnapshotIn<typeof PaymentRegulationModel> {}
 
-export const createProductDefaultModel = () => types.optional(PaymentRegulationModel, {});
+/*const createPaymentRequestDefaultModel = () =>
+  types.optional(PaymentRequestModel, {
+    amount: null,
+});*/
+
+const today = new Date();
+const year = today.getFullYear();
+const month = ('0' + (today.getMonth() + 1)).slice(-2);
+const day = ('0' + today.getDate()).slice(-2);
+const formattedDate = year + '-' + month + '-' + day;
+export const createPaymentRegulationDefaultModel = () =>
+  types.optional(PaymentRegulationModel, {
+    maturityDate: formattedDate,
+    comment: null,
+    percent: 3000,
+    amount: null,
+  });
