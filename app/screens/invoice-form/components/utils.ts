c0bf4e2d0168d5fe1d7c8bd4a5b4883986474bf6
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-06 10:47:53 +0300

    feat: implement date input and validation in payment regulation form

--- app/screens/invoice-form/components/utils.ts
+++ app/screens/invoice-form/components/utils.ts
@@ -1,4 +1,7 @@
 import React from 'react';
+import { TextStyle, ViewStyle } from 'react-native';
+
+import { color, spacing } from '../../../theme';
 
 export type ModalProps = {
   visibleModal: boolean;
@@ -7,3 +10,18 @@ export type ModalProps = {
 
 export const invoicePageSize = 500;
 export const itemsPerPage = 10;
+
+export const DATE_PICKER_LABEL_STYLE: TextStyle = { color: color.palette.greyDarker, fontFamily: 'Geometria-Bold' };
+
+export const DATE_PICKER_CONTAINER_STYLE: ViewStyle = {
+  padding: spacing[4],
+  backgroundColor: color.transparent,
+  borderColor: '#E1E5EF',
+  borderWidth: 1,
+};
+
+export const DATE_PICKER_TEXT_STYLE: TextStyle = {
+  color: color.palette.textClassicColor,
+  marginTop: spacing[2],
+  fontFamily: 'Geometria-Bold',
+};
