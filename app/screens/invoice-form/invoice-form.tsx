7faf72112147d55e92496cb54f0622341d95cdaa
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-07 16:42:21 +0300

    feat: implement modification of payment regulation

--- app/screens/invoice-form/invoice-form.tsx
+++ app/screens/invoice-form/invoice-form.tsx
@@ -13,6 +13,7 @@ import { translate } from '../../i18n';
 import { useStores } from '../../models';
 import { Customer } from '../../models/entities/customer/customer';
 import { Invoice, InvoiceStatus, createInvoiceDefaultModel } from '../../models/entities/invoice/invoice';
+import { PaymentRegulation } from '../../models/entities/payment-regulation/payment-regulation';
 import { Product, createProductDefaultModel } from '../../models/entities/product/product';
 import { TabNavigatorParamList, navigate } from '../../navigators';
 import { color, spacing } from '../../theme';
@@ -99,6 +100,7 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
   const [removePaymentRegulation, setRemovePaymentRegulation] = useState(false);
   const [totalPercent, setTotalPercent] = useState(0);
   const [savedInvoice, setSavedInvoice] = useState<Invoice>();
+  const [currentPayment, setCurrentPayment] = useState<PaymentRegulation>(null);
 
   const navigateToTab = (tab: string) => {
     navigation.reset({
@@ -661,6 +663,7 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
                 marginBottom: spacing[5],
               }}
               onPress={() => {
+                setCurrentPayment(null);
                 setPaymentCreation(true);
               }}
             >
@@ -744,6 +747,7 @@ export const InvoiceForm: React.FC<InvoiceFormProps> = props => {
         append={paymentAppend}
         totalPercent={totalPercent}
         setTotalPercent={setTotalPercent}
+        item={currentPayment}
       />
       <InvoiceCreationModal
         invoiceType={invoiceType}
