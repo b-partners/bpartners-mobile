42b1cd2838b24268018ddcb52e1334ec3fa2eba2
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-21 09:50:13 +0300

    fix: keyboard action in payment regulation creation

--- app/screens/invoice-form/components/payment-regulation-form-field/payment-creation-modal.tsx
+++ app/screens/invoice-form/components/payment-regulation-form-field/payment-creation-modal.tsx
@@ -1,7 +1,7 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Modal, View } from 'react-native';
+import { KeyboardAvoidingView, Modal, Platform, ScrollView, View } from 'react-native';
 import CloseIcon from 'react-native-vector-icons/AntDesign';
 
 import { Button, Text } from '../../../../components';
@@ -84,169 +84,173 @@ export const PaymentCreationModal: React.FC<PaymentCreationModalProps> = props =
 
   return (
     <Modal animationType='slide' transparent={true} visible={open} onRequestClose={onClose}>
-      <View style={{ height: '100%', width: '100%', backgroundColor: 'rgba(16,16,19,0.9)', justifyContent: 'center', alignItems: 'center' }}>
-        <View style={{ backgroundColor: palette.white, height: '45%', width: '90%', borderRadius: 15 }}>
-          <View
-            style={{
-              width: '100%',
-              borderBottomWidth: 1,
-              borderBottomColor: palette.secondaryColor,
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: spacing[2],
-              position: 'relative',
-              height: 50,
-            }}
-          >
-            <Text
-              tx='invoiceFormScreen.paymentRegulationForm.add'
+      <KeyboardAvoidingView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <ScrollView
+          style={{ width: '100%', height: '100%', backgroundColor: 'rgba(16,16,19,0.9)' }}
+          contentContainerStyle={{ justifyContent: 'center', alignItems: 'center', flexGrow: 1 }}
+        >
+          <View style={{ backgroundColor: palette.white, height: 330, width: '90%', borderRadius: 15 }}>
+            <View
               style={{
-                color: palette.secondaryColor,
-                fontFamily: 'Geometria',
-                fontSize: 18,
+                width: '100%',
+                borderBottomWidth: 1,
+                borderBottomColor: palette.secondaryColor,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+                height: 50,
               }}
-            />
-            <Button
-              onPress={onClose}
-              style={{
-                backgroundColor: palette.white,
-                position: 'absolute',
-                right: 26,
-              }}
-              textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
             >
-              <CloseIcon name='close' size={25} color={palette.secondaryColor} />
-            </Button>
-          </View>
-          <View style={{ width: '100%', height: '75%', flexDirection: 'column' }}>
-            <View style={{ width: '100%', height: '85%', justifyContent: 'center', marginVertical: '5%' }}>
-              <View style={{ marginBottom: 10, width: '70%', marginHorizontal: '15%' }}>
-                <Controller
-                  control={control}
-                  name='percent'
-                  rules={{
-                    required: translate('errors.required'),
-                    validate: {
-                      isNumber: value => isNumber(value) || translate('errors.invalidPercent'),
-                      isValidPercent: value => isValidPercent(value) || translate('invoiceFormScreen.paymentRegulationForm.invalidPercent'),
-                    },
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                    <InputField
-                      labelTx={'invoiceFormScreen.paymentRegulationForm.percent'}
-                      error={!!errors.percent}
-                      value={value}
-                      onChange={onChange}
-                      errorMessage={errors.percent?.message}
-                      backgroundColor={palette.lighterGrey}
-                      rightRender={true}
-                      rightText={'%'}
-                    />
-                  )}
-                />
-              </View>
-              <View style={{ marginBottom: 10, width: '70%', marginHorizontal: '15%', height: 65 }}>
-                <Controller
-                  control={control}
-                  name='maturityDate'
-                  rules={{
-                    required: translate('errors.required'),
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                    <DatePickerField
-                      labelTx='invoiceFormScreen.paymentRegulationForm.maturityDate'
-                      isButtonPreset={false}
-                      labelStyle={DATE_PICKER_LABEL_STYLE}
-                      containerStyle={{
-                        padding: spacing[4],
-                        backgroundColor: palette.lighterGrey,
-                        borderColor: '#E1E5EF',
-                        borderWidth: 1,
-                        borderRadius: 10,
-                      }}
-                      textStyle={DATE_PICKER_TEXT_STYLE}
-                      dateSeparator='/'
-                      value={value}
-                      onDateChange={onChange}
-                    />
-                  )}
-                />
-              </View>
-              <View style={{ marginBottom: 10, width: '70%', marginHorizontal: '15%' }}>
-                <Controller
-                  control={control}
-                  name='comment'
-                  defaultValue=''
-                  render={({ field: { onChange, value } }) => (
-                    <InputField
-                      labelTx={'invoiceFormScreen.paymentRegulationForm.comment'}
-                      error={!!errors.comment}
-                      value={value}
-                      onChange={onChange}
-                      errorMessage={errors.comment?.message}
-                      backgroundColor={palette.lighterGrey}
-                    />
-                  )}
-                />
-              </View>
+              <Text
+                tx='invoiceFormScreen.paymentRegulationForm.add'
+                style={{
+                  color: palette.secondaryColor,
+                  fontFamily: 'Geometria',
+                  fontSize: 18,
+                }}
+              />
+              <Button
+                onPress={onClose}
+                style={{
+                  backgroundColor: palette.white,
+                  position: 'absolute',
+                  right: 26,
+                }}
+                textStyle={{ fontSize: 14, fontFamily: 'Geometria-Bold' }}
+              >
+                <CloseIcon name='close' size={25} color={palette.secondaryColor} />
+              </Button>
             </View>
-            <View style={{ width: '100%', height: '10%', justifyContent: 'center' }}>
-              {errors.percent ? (
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    backgroundColor: palette.solidGrey,
-                    borderRadius: 25,
-                    paddingVertical: spacing[2],
-                    marginHorizontal: spacing[6],
-                    height: 45,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <>
-                    <Text
-                      tx='common.create'
-                      style={{
-                        color: palette.lighterGrey,
-                        marginRight: spacing[2],
-                        fontFamily: 'Geometria',
-                      }}
-                    />
-                    <MaterialIcons name='payments' size={20} color={palette.lighterGrey} />
-                  </>
+            <View style={{ width: '100%', height: '75%', flexDirection: 'column' }}>
+              <View style={{ width: '100%', height: '85%', justifyContent: 'center', marginVertical: '2%' }}>
+                <View style={{ marginBottom: 5, width: '70%', marginHorizontal: '15%' }}>
+                  <Controller
+                    control={control}
+                    name='percent'
+                    rules={{
+                      required: translate('errors.required'),
+                      validate: {
+                        isNumber: value => isNumber(value) || translate('errors.invalidPercent'),
+                        isValidPercent: value => isValidPercent(value) || translate('invoiceFormScreen.paymentRegulationForm.invalidPercent'),
+                      },
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <InputField
+                        labelTx={'invoiceFormScreen.paymentRegulationForm.percent'}
+                        error={!!errors.percent}
+                        value={value}
+                        onChange={onChange}
+                        errorMessage={errors.percent?.message}
+                        backgroundColor={palette.lighterGrey}
+                        rightRender={true}
+                        rightText={'%'}
+                      />
+                    )}
+                  />
                 </View>
-              ) : (
-                <Button
-                  onPress={handleSubmit(onSubmit)}
-                  style={{
-                    flexDirection: 'row',
-                    backgroundColor: palette.secondaryColor,
-                    borderRadius: 25,
-                    paddingVertical: spacing[2],
-                    marginHorizontal: spacing[6],
-                    height: 45,
-                  }}
-                >
-                  <>
-                    <Text
-                      tx='common.submit'
-                      style={{
-                        color: palette.white,
-                        marginRight: spacing[2],
-                        fontFamily: 'Geometria',
-                      }}
-                    />
-                    <MaterialIcons name='payments' size={20} color={palette.white} />
-                  </>
-                </Button>
-              )}
+                <View style={{ marginBottom: 5, width: '70%', marginHorizontal: '15%', height: 65 }}>
+                  <Controller
+                    control={control}
+                    name='maturityDate'
+                    rules={{
+                      required: translate('errors.required'),
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <DatePickerField
+                        labelTx='invoiceFormScreen.paymentRegulationForm.maturityDate'
+                        isButtonPreset={false}
+                        labelStyle={DATE_PICKER_LABEL_STYLE}
+                        containerStyle={{
+                          padding: spacing[4],
+                          backgroundColor: palette.lighterGrey,
+                          borderColor: '#E1E5EF',
+                          borderWidth: 1,
+                          borderRadius: 10,
+                        }}
+                        textStyle={DATE_PICKER_TEXT_STYLE}
+                        dateSeparator='/'
+                        value={value}
+                        onDateChange={onChange}
+                      />
+                    )}
+                  />
+                </View>
+                <View style={{ marginBottom: 5, width: '70%', marginHorizontal: '15%' }}>
+                  <Controller
+                    control={control}
+                    name='comment'
+                    defaultValue=''
+                    render={({ field: { onChange, value } }) => (
+                      <InputField
+                        labelTx={'invoiceFormScreen.paymentRegulationForm.comment'}
+                        error={!!errors.comment}
+                        value={value}
+                        onChange={onChange}
+                        errorMessage={errors.comment?.message}
+                        backgroundColor={palette.lighterGrey}
+                      />
+                    )}
+                  />
+                </View>
+              </View>
+              <View style={{ width: '100%', height: '10%', justifyContent: 'center' }}>
+                {errors.percent ? (
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      backgroundColor: palette.solidGrey,
+                      borderRadius: 25,
+                      paddingVertical: spacing[2],
+                      marginHorizontal: spacing[6],
+                      height: 45,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <>
+                      <Text
+                        tx='common.create'
+                        style={{
+                          color: palette.lighterGrey,
+                          marginRight: spacing[2],
+                          fontFamily: 'Geometria',
+                        }}
+                      />
+                      <MaterialIcons name='payments' size={20} color={palette.lighterGrey} />
+                    </>
+                  </View>
+                ) : (
+                  <Button
+                    onPress={handleSubmit(onSubmit)}
+                    style={{
+                      flexDirection: 'row',
+                      backgroundColor: palette.secondaryColor,
+                      borderRadius: 25,
+                      paddingVertical: spacing[2],
+                      marginHorizontal: spacing[6],
+                      height: 45,
+                    }}
+                  >
+                    <>
+                      <Text
+                        tx='common.submit'
+                        style={{
+                          color: palette.white,
+                          marginRight: spacing[2],
+                          fontFamily: 'Geometria',
+                        }}
+                      />
+                      <MaterialIcons name='payments' size={20} color={palette.white} />
+                    </>
+                  </Button>
+                )}
+              </View>
             </View>
           </View>
-        </View>
-      </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Modal>
   );
 };
