2a0cee8ae92c24bf1fc0e1fd6422edb1e5495637
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-27 13:12:23 +0300

    feat: implement partners screen in drawer

---
+++ app/components/bp-drawer/utils/utils.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type RouteNameProps = {
+  home: string | React.ReactElement;
+  profile: string | React.ReactElement;
+  transactionList: string | React.ReactElement;
+  paymentInitiation: string | React.ReactElement;
+  paymentList: string | React.ReactElement;
+  welcome: string | React.ReactElement;
+  oauth: string | React.ReactElement;
+  marketplace: string | React.ReactElement;
+  supportContact: string | React.ReactElement;
+  bank: string | React.ReactElement;
+  partners: string | React.ReactElement;
+};
