fb130fdec5bf0f3ce6cbed91dcad93b822a9198f
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-27 16:43:54 +0300

    feat: implement BRED insurance card

--- app/screens/partners/partners-screen.tsx
+++ app/screens/partners/partners-screen.tsx
@@ -1,16 +1,14 @@
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
-import { Linking, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { ScrollView, TextStyle, View, ViewStyle } from 'react-native';
 
-import { AutoImage, Header, Screen, Text } from '../../components';
-import { CENTER_CONTAINER_STYLE, TEXT_STYLE } from '../../components/bp-drawer/utils/styles';
-import { translate } from '../../i18n';
+import { Header, Screen } from '../../components';
 import { NavigatorParamList } from '../../navigators';
 import { spacing } from '../../theme';
 import { palette } from '../../theme/palette';
 import { ErrorBoundary } from '../error/error-boundary';
-import { bredUrl } from './utils/utils';
+import { PartnersCard } from './components/partners-card';
 
 const CONTAINER: ViewStyle = {
   backgroundColor: palette.white,
@@ -32,100 +30,23 @@ export const PartnersScreen: FC<DrawerScreenProps<NavigatorParamList, 'partners'
       <Header headerTx='partnersScreen.title' style={HEADER} titleStyle={HEADER_TITLE} leftIcon={'back'} onLeftPress={() => navigation.navigate('home')} />
       <Screen style={CONTAINER} preset='auto'>
         <View style={{ height: '100%', width: '100%', alignItems: 'center', marginTop: spacing[8] }}>
-          <View style={{ width: '90%', height: 300, backgroundColor: palette.blueDarker, flexDirection: 'column', borderRadius: 5 }}>
-            <View style={{ width: '100%', height: '30%', justifyContent: 'center', paddingLeft: spacing[6] }}>
-              <View
-                style={{
-                  width: 0,
-                  height: 0,
-                  borderLeftWidth: 35,
-                  borderBottomWidth: 35,
-                  borderLeftColor: 'transparent',
-                  borderBottomColor: palette.white,
-                  transform: [{ rotate: '180deg' }],
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                }}
-              />
-
-              <Text
-                style={{
-                  fontSize: 22,
-                  fontFamily: 'Geometria',
-                  color: palette.white,
-                  width: '100%',
-                }}
-                tx={'partnersScreen.bank.labelOne'}
-              />
-              <Text
-                style={{
-                  fontSize: 22,
-                  fontFamily: 'Geometria',
-                  color: palette.yellowDarker,
-                  width: '100%',
-                }}
-                tx={'partnersScreen.bank.labelTwo'}
-              />
-            </View>
-            <View style={{ width: '100%', height: '40%', flexDirection: 'column' }}>
-              <View style={{ height: '60%', justifyContent: 'center', alignItems: 'center' }}>
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontFamily: 'Geometria',
-                    color: palette.white,
-                    width: 250,
-                  }}
-                  tx={'partnersScreen.bank.textOne'}
-                />
-                <Text
-                  style={{
-                    fontSize: 17,
-                    fontFamily: 'Geometria',
-                    color: palette.white,
-                    width: 120,
-                  }}
-                  tx={'partnersScreen.bank.textTwo'}
-                />
-              </View>
-              <TouchableOpacity
-                style={{
-                  position: 'absolute',
-                  backgroundColor: palette.blue,
-                  width: '80%',
-                  height: 35,
-                  marginTop: 10,
-                  bottom: '3%',
-                  alignSelf: 'center',
-                  borderRadius: 5,
-                  justifyContent: 'center',
-                  flexDirection: 'row',
-                }}
-                onPress={() => Linking.openURL(bredUrl)}
-              >
-                <View style={CENTER_CONTAINER_STYLE}>
-                  <Text
-                    style={{
-                      ...TEXT_STYLE,
-                      color: palette.white,
-                      fontFamily: 'Geometria',
-                    }}
-                  >
-                    {translate('partnersScreen.bank.button')}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-            <View style={{ width: '100%', height: '30%', justifyContent: 'center', alignItems: 'flex-end' }}>
-              <AutoImage
-                source={require('./logo-bred.png')}
-                style={{ width: 180, height: 50, position: 'absolute', right: 10 }}
-                resizeMethod='auto'
-                resizeMode='stretch'
-              />
-            </View>
-          </View>
+          <ScrollView horizontal style={{ height: 400, width: '100%' }} contentContainerStyle={{ flexDirection: 'row' }} showsHorizontalScrollIndicator={false}>
+            <PartnersCard
+              firstLabel={'partnersScreen.bank.labelOne'}
+              secondLabel={'partnersScreen.bank.labelTwo'}
+              firstText={'partnersScreen.bank.textOne'}
+              secondText={'partnersScreen.bank.textTwo'}
+              buttonText={'partnersScreen.bank.button'}
+            />
+            <PartnersCard
+              firstLabel={'partnersScreen.insurance.labelOne'}
+              secondLabel={'partnersScreen.insurance.labelTwo'}
+              firstText={'partnersScreen.insurance.textOne'}
+              secondText={'partnersScreen.insurance.textTwo'}
+              buttonText={'partnersScreen.insurance.button'}
+              isInsurance={true}
+            />
+          </ScrollView>
         </View>
       </Screen>
     </ErrorBoundary>
--- app/screens/partners/utils/utils.ts
+++ app/screens/partners/utils/utils.ts
@@ -1,3 +1,18 @@
+import { TxKeyPath } from '../../../i18n';
+
 export const bredUrl =
   // eslint-disable-next-line max-len
   'https://www.bred.fr/professionnels-associations/ouvrir-un-compte-pro?utm_medium=referral&utm_source=bpartners.app&utm_campaign=RE_bpartners&utm_content=ban_compte-bancaire';
+
+export const insuranceUrl =
+  // eslint-disable-next-line max-len
+  'https://www.bred.fr/professionnels-associations/assurance/assurance-des-biens/assurance-multirisque-pro?utm_medium=referral&utm_source=bpartners.app&utm_campaign=RE_bpartners&utm_content=ban_assurance-pro';
+
+export type PartnersCardProps = {
+  firstLabel: TxKeyPath;
+  secondLabel: TxKeyPath;
+  firstText: TxKeyPath;
+  secondText: TxKeyPath;
+  buttonText: TxKeyPath;
+  isInsurance?: boolean;
+};
