1091eb4049da0abb5324f65e5a2b2104672ca6a7
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-09-06 10:03:39 +0300

    feat: add tab navigation in profile edition screen

--- app/screens/profile-edition/profile-edition-screen.tsx
+++ app/screens/profile-edition/profile-edition-screen.tsx
@@ -1,57 +1,33 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { StackScreenProps } from '@react-navigation/stack';
 import { observer } from 'mobx-react-lite';
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { TextStyle, ViewStyle } from 'react-native';
 
 import { Header, Screen, Text } from '../../components';
 import { translate } from '../../i18n';
-import { useStores } from '../../models';
-import { InvoiceStatus } from '../../models/entities/invoice/invoice';
 import { TabNavigatorParamList } from '../../navigators';
 import { color } from '../../theme';
 import { palette } from '../../theme/palette';
 import { ErrorBoundary } from '../error/error-boundary';
-import { invoicePageSize } from '../invoice-form/components/utils';
-import { DraftsScreen } from '../invoices/drafts-screen';
-import { InvoicesScreen } from '../invoices/invoices-screen';
-import { QuotationsScreen } from '../invoices/quotations-screen';
 import { HEADER, HEADER_TITLE } from '../payment-initiation/style';
+import { GlobalInfoForm } from './screen/global-info-form';
+import { RevenueTargetsForm } from './screen/revenue-targets-form';
 
 const NO_SHADOW: ViewStyle = { elevation: 0, shadowRadius: 0, shadowOpacity: 0, shadowOffset: { width: 0, height: 0 } };
 const TAB_BAR_STYLE: ViewStyle = { backgroundColor: palette.white, ...NO_SHADOW };
 
 type TabNameProps = {
-  drafts: string;
-  quotations: string;
-  invoices: string;
+  globalInfo: string;
+  revenueTargets: string;
 };
 
-export const ProfileEditionScreen: FC<StackScreenProps<TabNavigatorParamList, 'paymentList'>> = observer(function ProfileEditionScreen({ navigation, route }) {
-  const initialRoute = route.params?.initialRoute;
+export const ProfileEditionScreen: FC<StackScreenProps<TabNavigatorParamList, 'paymentList'>> = observer(function ProfileEditionScreen({ navigation }) {
   const Tab = createMaterialTopTabNavigator();
-  const { customerStore, productStore, draftStore, quotationStore, invoiceStore } = useStores();
-
-  useEffect(() => {
-    (async () => {
-      await productStore.getProducts({ page: 1, pageSize: invoicePageSize });
-      await customerStore.getCustomers({ page: 1, pageSize: invoicePageSize });
-    })();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      await draftStore.getDrafts({ status: InvoiceStatus.DRAFT, page: 1, pageSize: invoicePageSize });
-      await quotationStore.getQuotations({ status: InvoiceStatus.PROPOSAL, page: 1, pageSize: invoicePageSize });
-      await invoiceStore.getInvoices({ status: InvoiceStatus.CONFIRMED, page: 1, pageSize: invoicePageSize });
-      await invoiceStore.getPaidInvoices({ status: InvoiceStatus.PAID, page: 1, pageSize: invoicePageSize });
-    })();
-  }, []);
 
   const TabName: TabNameProps = {
-    drafts: translate('paymentListScreen.tabs.drafts'),
-    quotations: translate('paymentListScreen.tabs.quotations'),
-    invoices: translate('paymentListScreen.tabs.invoices'),
+    globalInfo: translate('paymentListScreen.tabs.drafts'),
+    revenueTargets: translate('paymentListScreen.tabs.quotations'),
   };
 
   return (
@@ -59,7 +35,7 @@ export const ProfileEditionScreen: FC<StackScreenProps<TabNavigatorParamList, 'p
       <Header headerTx='paymentListScreen.title' onLeftPress={() => navigation.navigate('home')} leftIcon='back' style={HEADER} titleStyle={HEADER_TITLE} />
       <Screen>
         <Tab.Navigator
-          initialRouteName={initialRoute}
+          initialRouteName={'globalInfo'}
           style={TAB_BAR_STYLE}
           screenOptions={({ route: tabRoute }) => ({
             tabBarIndicatorStyle: { backgroundColor: color.primary },
@@ -85,9 +61,9 @@ export const ProfileEditionScreen: FC<StackScreenProps<TabNavigatorParamList, 'p
           }}
         >
           <Tab.Screen
-            name={'drafts'}
-            component={DraftsScreen}
-            navigationKey='drafts'
+            name={'globalInfo'}
+            component={GlobalInfoForm}
+            navigationKey='globalInfo'
             listeners={{
               tabPress: () => {
                 // invoiceStore.getDrafts({ status: InvoiceStatus.DRAFT, page: 1, pageSize: 30 });
@@ -95,22 +71,12 @@ export const ProfileEditionScreen: FC<StackScreenProps<TabNavigatorParamList, 'p
             }}
           />
           <Tab.Screen
-            name={'quotations'}
-            component={QuotationsScreen}
-            navigationKey='quotations'
+            name={'revenueTargets'}
+            component={RevenueTargetsForm}
+            navigationKey='revenueTargets'
             listeners={{
               tabPress: () => {
                 // invoiceStore.getQuotations({ status: InvoiceStatus.PROPOSAL, page: 1, pageSize: 30 });
-              },
-            }}
-          />
-          <Tab.Screen
-            name={'invoices'}
-            component={InvoicesScreen}
-            navigationKey='invoices'
-            listeners={{
-              tabPress: () => {
-                // invoiceStore.getInvoices({ status: InvoiceStatus.CONFIRMED, page: 1, pageSize: 30 });
               },
             }}
           />
