a10eb61f6ecc11983c0914061eb94d4512832d08
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-09-14 11:15:26 +0300

    feat: implement first part of prospecting processus form

--- app/screens/prospect/components/process-modal.tsx
+++ app/screens/prospect/components/process-modal.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { TouchableOpacity, View } from 'react-native';
 import { Modal } from 'react-native-paper';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
-import { InputField, Text } from '../../../components';
+import { Button, InputField, Text } from '../../../components';
 import { translate } from '../../../i18n';
 import { color, spacing } from '../../../theme';
 import { palette } from '../../../theme/palette';
+import { SHADOW_STYLE } from '../../invoices/utils/styles';
 import { ProcessModalProps } from '../utils/utils';
 
 export const ProcessModal: React.FC<ProcessModalProps> = props => {
   const { showModal, setShowModal, prospect } = props;
+  const [currentPage, setCurrentPage] = useState<1 | 2>(1);
 
   const closeModal = () => {
     setShowModal(false);
@@ -49,7 +51,7 @@ export const ProcessModal: React.FC<ProcessModalProps> = props => {
           borderRadius: 20,
           marginHorizontal: '2%',
           width: '96%',
-          height: '85%',
+          height: 450,
         }}
       >
         <View
@@ -69,104 +71,175 @@ export const ProcessModal: React.FC<ProcessModalProps> = props => {
             <AntDesignIcon name='close' color={color.palette.lightGrey} size={20} />
           </TouchableOpacity>
         </View>
-        <View style={{ flex: 1, paddingHorizontal: spacing[4], paddingTop: spacing[2] }}>
-          <View style={{ marginBottom: 10, width: '100%' }}>
-            <Controller
-              control={control}
-              name='email'
-              rules={{
-                required: translate('errors.required'),
-              }}
-              defaultValue=''
-              render={({ field: { onChange, value } }) => (
-                <InputField
-                  labelTx={'profileEditionScreen.companyInfo.socialCapital'}
-                  error={!!errors.email}
-                  value={value}
-                  onChange={onChange}
-                  errorMessage={errors.email?.message}
-                  backgroundColor={palette.solidGrey}
-                />
-              )}
-            />
+        {currentPage === 1 ? (
+          <View style={{ flex: 1, paddingHorizontal: spacing[4], paddingTop: spacing[2] }}>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='email'
+                rules={{
+                  required: translate('errors.required'),
+                }}
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.email'}
+                    error={!!errors.email}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.email?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='phone'
+                rules={{
+                  required: translate('errors.required'),
+                }}
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.phone'}
+                    error={!!errors.phone}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.phone?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='address'
+                rules={{
+                  required: translate('errors.required'),
+                }}
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.address'}
+                    error={!!errors.address}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.address?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='name'
+                rules={{
+                  required: translate('errors.required'),
+                }}
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.name'}
+                    error={!!errors.name}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.name?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='comment'
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.comment'}
+                    error={!!errors.comment}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.comment?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
           </View>
-          <View style={{ marginBottom: 10, width: '100%' }}>
-            <Controller
-              control={control}
-              name='phone'
-              rules={{
-                required: translate('errors.required'),
-              }}
-              defaultValue=''
-              render={({ field: { onChange, value } }) => (
-                <InputField
-                  labelTx={'profileEditionScreen.companyInfo.socialCapital'}
-                  error={!!errors.phone}
-                  value={value}
-                  onChange={onChange}
-                  errorMessage={errors.phone?.message}
-                  backgroundColor={palette.solidGrey}
-                />
-              )}
-            />
+        ) : (
+          <View style={{ flex: 1, paddingHorizontal: spacing[4], paddingTop: spacing[2] }}>
+            <View style={{ marginBottom: 10, width: '100%' }}>
+              <Controller
+                control={control}
+                name='email'
+                rules={{
+                  required: translate('errors.required'),
+                }}
+                defaultValue=''
+                render={({ field: { onChange, value } }) => (
+                  <InputField
+                    labelTx={'prospectScreen.process.email'}
+                    error={!!errors.email}
+                    value={value}
+                    onChange={onChange}
+                    errorMessage={errors.email?.message}
+                    backgroundColor={palette.solidGrey}
+                  />
+                )}
+              />
+            </View>
           </View>
-          <View style={{ marginBottom: 10, width: '100%' }}>
-            <Controller
-              control={control}
-              name='address'
-              rules={{
-                required: translate('errors.required'),
-              }}
-              defaultValue=''
-              render={({ field: { onChange, value } }) => (
-                <InputField
-                  labelTx={'profileEditionScreen.companyInfo.socialCapital'}
-                  error={!!errors.address}
-                  value={value}
-                  onChange={onChange}
-                  errorMessage={errors.address?.message}
-                  backgroundColor={palette.solidGrey}
-                />
-              )}
-            />
-          </View>
-          <View style={{ marginBottom: 10, width: '100%' }}>
-            <Controller
-              control={control}
-              name='name'
-              rules={{
-                required: translate('errors.required'),
-              }}
-              defaultValue=''
-              render={({ field: { onChange, value } }) => (
-                <InputField
-                  labelTx={'profileEditionScreen.companyInfo.socialCapital'}
-                  error={!!errors.name}
-                  value={value}
-                  onChange={onChange}
-                  errorMessage={errors.name?.message}
-                  backgroundColor={palette.solidGrey}
-                />
-              )}
-            />
-          </View>
-          <View style={{ marginBottom: 10, width: '100%' }}>
-            <Controller
-              control={control}
-              name='comment'
-              defaultValue=''
-              render={({ field: { onChange, value } }) => (
-                <InputField
-                  labelTx={'profileEditionScreen.companyInfo.socialCapital'}
-                  error={!!errors.comment}
-                  value={value}
-                  onChange={onChange}
-                  errorMessage={errors.comment?.message}
-                  backgroundColor={palette.solidGrey}
-                />
-              )}
-            />
-          </View>
+        )}
+        <View
+          style={{
+            height: 60,
+            width: '100%',
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+            justifyContent: 'flex-end',
+            paddingRight: spacing[4],
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}
+        >
+          <Button
+            tx={currentPage === 1 ? 'common.cancel' : 'common.back'}
+            style={{
+              ...SHADOW_STYLE,
+              backgroundColor: palette.secondaryColor,
+              borderRadius: 10,
+              paddingVertical: spacing[3],
+              paddingHorizontal: spacing[2],
+              width: 100,
+              height: 40,
+              marginRight: spacing[2],
+            }}
+            onPress={() => {
+              currentPage === 1 ? closeModal() : setCurrentPage(1);
+            }}
+            textStyle={{ fontSize: 13, fontFamily: 'Geometria-Bold' }}
+          />
+          <Button
+            tx={currentPage === 1 ? 'common.next' : 'prospectScreen.process.reserve'}
+            style={{
+              ...SHADOW_STYLE,
+              backgroundColor: palette.secondaryColor,
+              borderRadius: 10,
+              paddingVertical: spacing[3],
+              paddingHorizontal: spacing[2],
+              width: 100,
+              height: 40,
+            }}
+            onPress={() => {
+              currentPage === 1 && setCurrentPage(2);
+            }}
+            textStyle={{ fontSize: 13, fontFamily: 'Geometria-Bold' }}
+          />
         </View>
       </View>
     </Modal>
