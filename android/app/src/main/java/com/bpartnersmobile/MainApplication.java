fc676c22523efe2912f81ce324884bbf028f3d23
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-11-07 15:00:47 +0300

    chore: remove react-native-notifications dependencies

--- android/app/src/main/java/com/bpartnersmobile/MainApplication.java
+++ android/app/src/main/java/com/bpartnersmobile/MainApplication.java
@@ -16,7 +16,6 @@ import com.facebook.soloader.SoLoader;
 import com.bpartnersmobile.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
