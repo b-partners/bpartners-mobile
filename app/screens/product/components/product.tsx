a338fbfdeb88c22cf1e4dbcc189f7ddb383c2518
Liana Finaritra <100028310+lianafinaritra@users.noreply.github.com>
2023-11-09 15:27:05 +0300

    Revert "feat: make product editable" (#318)
    This reverts commit 267fea38358832dc225df1701321093dcfe52e48.

--- app/screens/product/components/product.tsx
+++ app/screens/product/components/product.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { TouchableOpacity, View } from 'react-native';
 
 import { Text } from '../../../components';
@@ -6,28 +6,15 @@ import { Product as IProduct } from '../../../models/entities/product/product';
 import { spacing } from '../../../theme';
 import { palette } from '../../../theme/palette';
 import { printCurrencyToMajors } from '../../../utils/money';
-import { ProductModalType } from '../products-screen';
 
-type ProductProps = {
-  item: IProduct;
-  setModal: Dispatch<SetStateAction<ProductModalType>>;
-};
+type ProductProps = { item: IProduct };
 
 export const Product: React.FC<ProductProps> = props => {
-  const { item, setModal } = props;
+  const { item } = props;
 
   return (
     <View style={{ display: 'flex', flexDirection: 'row' }}>
-      <TouchableOpacity
-        style={{ paddingBottom: spacing[2], paddingTop: spacing[0], flex: 1 }}
-        onPress={() =>
-          setModal({
-            type: 'EDITION',
-            state: true,
-            product: item,
-          })
-        }
-      >
+      <TouchableOpacity style={{ paddingBottom: spacing[2], paddingTop: spacing[0], flex: 1 }}>
         <View style={{ display: 'flex', flexDirection: 'row', marginBottom: spacing[2], justifyContent: 'space-between' }}>
           <View style={{ flexDirection: 'row' }}>
             <Text
