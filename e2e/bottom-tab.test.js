b912fec43190222d26a8951440044ee9ed673ce9
Tahiry Randriamiarintsoa <tahirycontact@gmail.com>
2024-01-24 13:32:38 +0300

    fix: update test code to reach element

--- e2e/bottom-tab.test.js
+++ e2e/bottom-tab.test.js
@@ -1,3 +1,5 @@
+import { by, device, element, waitFor } from 'detox';
+
 describe('Bottom-tab', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -17,8 +19,10 @@ describe('Bottom-tab', () => {
   });
 
   it('show Modal', async () => {
-    await waitFor(element(by.id('serviceTab')));
-    await element(by.id('serviceTab')).tap();
+    await waitFor(element(by.id('serviceTab'))).toBeVisible();
+    try {
+      await element(by.id('serviceTab')).tap(); //all actions are available only for API31 based simulators
+    } catch (e) {}
     await waitFor(element(by.id('BottomTabModal'))).toBeVisible();
   });
 });
