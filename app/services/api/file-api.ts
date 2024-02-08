a0ae751cb2627f90a463bc13bbb2609e3d35afa9
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-02-08 12:19:52 +0300

    feat: update file api function

--- app/services/api/file-api.ts
+++ app/services/api/file-api.ts
@@ -26,9 +26,9 @@ export class FileApi {
     }
   }
 
-  async uploadFile(fileId: string, payload: File): Promise<UploadFileResult> {
+  async uploadFile(accountId: string, fileId: string, payload: File): Promise<UploadFileResult> {
     try {
-      const response: ApiResponse<any> = await this.api.apisauce.post(`files/${fileId}/raw`, payload);
+      const response: ApiResponse<any> = await this.api.apisauce.post(`accounts/${accountId}/files/${fileId}/raw`, payload);
 
       if (!response.ok) {
         const problem = getGeneralApiProblem(response);
