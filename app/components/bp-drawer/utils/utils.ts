da80dc3ab81dbb5f8c188bfbb510f3d670350490
Judicael <judi.randriamanjato@gmail.com>
2024-03-28 17:20:25 +0300

    wip: make dots removable

--- app/components/bp-drawer/utils/utils.ts
+++ app/components/bp-drawer/utils/utils.ts
@@ -1,19 +1,20 @@
 import React from 'react';
 
 export type RouteNameProps = {
-  home: string | React.ReactElement;
-  profile: string | React.ReactElement;
-  transactionList: string | React.ReactElement;
-  customer: string | React.ReactElement;
-  product: string | React.ReactElement;
-  paymentInitiation: string | React.ReactElement;
-  paymentList: string | React.ReactElement;
-  welcome: string | React.ReactElement;
-  oauth: string | React.ReactElement;
-  marketplace: string | React.ReactElement;
-  supportContact: string | React.ReactElement;
-  bank: string | React.ReactElement;
-  configuration: string | React.ReactElement;
-  partners: string | React.ReactElement;
-  calendar: string | React.ReactElement;
+    home: string | React.ReactElement;
+    profile: string | React.ReactElement;
+    transactionList: string | React.ReactElement;
+    customer: string | React.ReactElement;
+    product: string | React.ReactElement;
+    paymentInitiation: string | React.ReactElement;
+    paymentList: string | React.ReactElement;
+    welcome: string | React.ReactElement;
+    oauth: string | React.ReactElement;
+    marketplace: string | React.ReactElement;
+    supportContact: string | React.ReactElement;
+    bank: string | React.ReactElement;
+    configuration: string | React.ReactElement;
+    partners: string | React.ReactElement;
+    calendar: string | React.ReactElement;
+    annotatorEdition: string | React.ReactElement;
 };
