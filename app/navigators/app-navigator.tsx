012b6638888f648b9ca0d318a0ba98ae5c7c5503
Judicael <judi.randriamanjato@gmail.com>
2024-03-28 17:20:25 +0300

    wip: make dots removable

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -4,52 +4,51 @@
  * Generally speaking, it will contain an auth flow (registration, login, forgot password)
  * and a "main" flow which the user will use once logged in.
  */
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DarkTheme, DefaultTheme, NavigationContainer, NavigationState } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {DarkTheme, DefaultTheme, NavigationContainer, NavigationState} from '@react-navigation/native';
 import * as Linking from 'expo-linking';
-import { observer } from 'mobx-react-lite';
+import {observer} from 'mobx-react-lite';
 import React from 'react';
-import { Dimensions, useColorScheme } from 'react-native';
+import {Dimensions, useColorScheme} from 'react-native';
 
-import { BPDrawer, BpTabNavigation, Text } from '../components';
+import {BPDrawer, BpTabNavigation, Text} from '../components';
 // import { useError } from '../hook';
-import { translate } from '../i18n';
-import { useStores } from '../models';
+import {translate} from '../i18n';
+import {useStores} from '../models';
 import {
-  AnnotatorEditionScreen,
-  BankScreen,
-  ChangePasswordScreen,
-  CodeExchangeScreen,
-  ConfigurationScreen,
-  ErrorBoundary,
-  ForgotPasswordScreen,
-  HomeScreen,
-  InvoiceFormScreen,
-  InvoicePreviewScreen,
-  InvoicesScreen,
-  LegalFileScreen,
-  MarketPlaceScreen,
-  PartnersScreen,
-  PaymentInitiationScreen,
-  PaymentListScreen,
-  ProfileScreen,
-  ProspectConfigurationScreen,
-  ProspectScreen,
-  RegistrationScreen,
-  ResetPasswordScreen,
-  SupportContactScreen,
-  TransactionListScreen,
-  WelcomeScreen,
+    AnnotatorEditionScreen,
+    BankScreen,
+    ChangePasswordScreen,
+    CodeExchangeScreen,
+    ConfigurationScreen,
+    ErrorBoundary,
+    ForgotPasswordScreen,
+    HomeScreen,
+    InvoiceFormScreen,
+    InvoicePreviewScreen,
+    InvoicesScreen,
+    LegalFileScreen,
+    MarketPlaceScreen,
+    PartnersScreen,
+    PaymentInitiationScreen,
+    PaymentListScreen,
+    ProfileScreen,
+    ProspectConfigurationScreen,
+    ProspectScreen,
+    RegistrationScreen,
+    ResetPasswordScreen,
+    SupportContactScreen,
+    TransactionListScreen,
+    WelcomeScreen,
 } from '../screens';
-import { AnnotatorScreen } from '../screens/annotator/annotator-screen';
-import { CalendarScreen } from '../screens/calendar/calendar-screen';
-import { CustomersScreen } from '../screens/customer/customers-screen';
-import { ProductScreen } from '../screens/product/products-screen';
-import { ProfileEditionScreen } from '../screens/profile-edition/profile-edition-screen';
-import { navigationRef, useBackButtonHandler } from './navigation-utilities';
-import { NavigatorParamList, TabNavigatorParamList } from './utils/utils';
-import {AnnotatorScreen} from "../screens/annotator/annotator";
+import {AnnotatorScreen} from '../screens/annotator/annotator-screen';
+import {CalendarScreen} from '../screens/calendar/calendar-screen';
+import {CustomersScreen} from '../screens/customer/customers-screen';
+import {ProductScreen} from '../screens/product/products-screen';
+import {ProfileEditionScreen} from '../screens/profile-edition/profile-edition-screen';
+import {navigationRef, useBackButtonHandler} from './navigation-utilities';
+import {NavigatorParamList, TabNavigatorParamList} from './utils/utils';
 
 /**
  * This type allows TypeScript to know what routes are defined in this navigator
@@ -69,101 +68,111 @@ const Tab = createBottomTabNavigator<TabNavigatorParamList>();
 const windowWidth = Dimensions.get('window').width;
 
 const AppStack = observer(function () {
-  const HIDE_DRAWER_OPTIONS: any = {
-    swipeEnabled: false,
-    title: null,
-    drawerItemStyle: { display: 'none' },
-  };
+    const HIDE_DRAWER_OPTIONS: any = {
+        swipeEnabled: false,
+        title: null,
+        drawerItemStyle: {display: 'none'},
+    };
 
-  const { authStore, legalFilesStore } = useStores();
-  const { accessToken, currentAccount, currentUser } = authStore;
-  const hasAccount = currentAccount && !!currentAccount?.id;
-  const hasUser = currentUser && !!currentUser?.id;
-  const hasApprovedLegalFiles = legalFilesStore.unApprovedFiles.length <= 0;
-  const isAuthenticated = !!accessToken && hasAccount && hasUser;
-  const shouldApproveLegalFiles = (accessToken && hasUser && !hasApprovedLegalFiles) || (isAuthenticated && !hasApprovedLegalFiles);
-  const isAvalaible = isAuthenticated && hasApprovedLegalFiles;
+    const {authStore, legalFilesStore} = useStores();
+    const {accessToken, currentAccount, currentUser} = authStore;
+    const hasAccount = currentAccount && !!currentAccount?.id;
+    const hasUser = currentUser && !!currentUser?.id;
+    const hasApprovedLegalFiles = legalFilesStore.unApprovedFiles.length <= 0;
+    const isAuthenticated = !!accessToken && hasAccount && hasUser;
+    const shouldApproveLegalFiles = (accessToken && hasUser && !hasApprovedLegalFiles) || (isAuthenticated && !hasApprovedLegalFiles);
+    const isAvalaible = isAuthenticated && hasApprovedLegalFiles;
 
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: false,
-        drawerStyle: {
-          width: windowWidth,
-        },
-      }}
-      initialRouteName={accessToken ? 'home' : 'welcome'}
-      drawerContent={props => <BPDrawer {...props} />}
-    >
-      {shouldApproveLegalFiles ? (
-        <>
-          <Drawer.Screen name='legalFile' component={LegalFileScreen} options={HIDE_DRAWER_OPTIONS} />
-        </>
-      ) : isAvalaible ? (
-        <>
-          <Drawer.Screen name='home' component={AppTabStack} />
-          <Drawer.Screen name='profile' component={ProfileScreen} options={{ title: translate('profileScreen.title') }} />
-          <Drawer.Screen name='transactionList' component={TransactionListScreen} options={{ title: translate('transactionListScreen.title') }} />
-          <Drawer.Screen name='customer' component={CustomersScreen} />
-          <Drawer.Screen name='product' component={ProductScreen} />
-          <Drawer.Screen name='invoices' component={InvoicesScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='invoiceForm' component={InvoiceFormScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='invoicePreview' component={InvoicePreviewScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='bank' component={BankScreen} />
-          <Drawer.Screen name='configuration' component={ConfigurationScreen} />
-          <Drawer.Screen name='partners' component={PartnersScreen} />
-          <Drawer.Screen name='profileEdition' component={ProfileEditionScreen} />
-          <Drawer.Screen name='calendar' component={CalendarScreen} />
-          <Drawer.Screen name='annotatorEdition' component={AnnotatorEditionScreen} />
-        </>
-      ) : (
-        <>
-          <Drawer.Screen name='welcome' component={WelcomeScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='registration' component={RegistrationScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='changePassword' component={ChangePasswordScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='forgotPassword' component={ForgotPasswordScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='resetPassword' component={ResetPasswordScreen} options={HIDE_DRAWER_OPTIONS} />
-          <Drawer.Screen name='oauth' component={CodeExchangeScreen} options={HIDE_DRAWER_OPTIONS} />
-        </>
-      )}
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerShown: false,
+                drawerStyle: {
+                    width: windowWidth,
+                },
+            }}
+            initialRouteName={accessToken ? 'home' : 'welcome'}
+            drawerContent={props => <BPDrawer {...props} />}
+        >
+            {shouldApproveLegalFiles ? (
+                <>
+                    <Drawer.Screen name='legalFile' component={LegalFileScreen} options={HIDE_DRAWER_OPTIONS}/>
+                </>
+            ) : isAvalaible ? (
+                <>
+                    <Drawer.Screen name='home' component={AppTabStack}/>
+                    <Drawer.Screen name='profile' component={ProfileScreen}
+                                   options={{title: translate('profileScreen.title')}}/>
+                    <Drawer.Screen name='transactionList' component={TransactionListScreen}
+                                   options={{title: translate('transactionListScreen.title')}}/>
+                    <Drawer.Screen name='customer' component={CustomersScreen}/>
+                    <Drawer.Screen name='product' component={ProductScreen}/>
+                    <Drawer.Screen name='invoices' component={InvoicesScreen} options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='invoiceForm' component={InvoiceFormScreen} options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='invoicePreview' component={InvoicePreviewScreen}
+                                   options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='bank' component={BankScreen}/>
+                    <Drawer.Screen name='configuration' component={ConfigurationScreen}/>
+                    <Drawer.Screen name='partners' component={PartnersScreen}/>
+                    <Drawer.Screen name='profileEdition' component={ProfileEditionScreen}/>
+                    <Drawer.Screen name='calendar' component={CalendarScreen}/>
+                    <Drawer.Screen name='annotatorEdition' component={AnnotatorEditionScreen}/>
+                </>
+            ) : (
+                <>
+                    <Drawer.Screen name='welcome' component={WelcomeScreen} options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='registration' component={RegistrationScreen} options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='changePassword' component={ChangePasswordScreen}
+                                   options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='forgotPassword' component={ForgotPasswordScreen}
+                                   options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='resetPassword' component={ResetPasswordScreen} options={HIDE_DRAWER_OPTIONS}/>
+                    <Drawer.Screen name='oauth' component={CodeExchangeScreen} options={HIDE_DRAWER_OPTIONS}/>
+                </>
+            )}
+        </Drawer.Navigator>
+    );
 });
 
 const AppTabStack = observer(function () {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      initialRouteName={'bp_home'}
-      tabBar={props => <BpTabNavigation {...props} />}
-    >
-      <>
-        <Tab.Screen name='bp_home' component={HomeScreen} options={{ title: translate('homeScreen.title') }} />
-        <Tab.Screen name='marketplace' component={MarketPlaceScreen} options={{ title: translate('marketPlaceScreen.title') }} />
-        <Tab.Screen name='paymentInitiation' component={PaymentInitiationScreen} options={{ title: translate('paymentInitiationScreen.label') }} />
-        <Tab.Screen name='prospect' component={ProspectScreen} options={{ title: translate('homeScreen.title') }} />
-        <Tab.Screen name='prospectConfiguration' component={ProspectConfigurationScreen} options={{ title: translate('homeScreen.title') }} />
-        <Tab.Screen name='paymentList' component={PaymentListScreen} />
-        <Tab.Screen name='supportContact' component={SupportContactScreen} />
-        <Tab.Screen name='invoices' component={InvoicesScreen} />
-        <Tab.Screen name='invoiceForm' component={InvoiceFormScreen} />
-        <Tab.Screen name='annotator' component={AnnotatorScreen} options={{ title: translate('homeScreen.title') }} />
-      </>
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+            initialRouteName={'bp_home'}
+            tabBar={props => <BpTabNavigation {...props} />}
+        >
+            <>
+                <Tab.Screen name='bp_home' component={HomeScreen} options={{title: translate('homeScreen.title')}}/>
+                <Tab.Screen name='marketplace' component={MarketPlaceScreen}
+                            options={{title: translate('marketPlaceScreen.title')}}/>
+                <Tab.Screen name='paymentInitiation' component={PaymentInitiationScreen}
+                            options={{title: translate('paymentInitiationScreen.label')}}/>
+                <Tab.Screen name='prospect' component={ProspectScreen}
+                            options={{title: translate('homeScreen.title')}}/>
+                <Tab.Screen name='prospectConfiguration' component={ProspectConfigurationScreen}
+                            options={{title: translate('homeScreen.title')}}/>
+                <Tab.Screen name='paymentList' component={PaymentListScreen}/>
+                <Tab.Screen name='supportContact' component={SupportContactScreen}/>
+                <Tab.Screen name='invoices' component={InvoicesScreen}/>
+                <Tab.Screen name='invoiceForm' component={InvoiceFormScreen}/>
+                <Tab.Screen name='annotator' component={AnnotatorScreen}
+                            options={{title: translate('homeScreen.title')}}/>
+            </>
+        </Tab.Navigator>
+    );
 });
 
 type NavigationProps = Partial<React.ComponentProps<typeof NavigationContainer>>;
 
 export function AppNavigator(props: NavigationProps) {
-  const colorScheme = useColorScheme();
-  useBackButtonHandler(canExit);
-  const { bankInfo, currentUser } = useStores();
-  // const { setError } = useError();
+    const colorScheme = useColorScheme();
+    useBackButtonHandler(canExit);
+    const {bankInfo, currentUser} = useStores();
+    // const { setError } = useError();
 
-  /*const handleError = async (asyncFunc: () => any) => {
+    /*const handleError = async (asyncFunc: () => any) => {
       try {
         await asyncFunc();
       } catch (e) {
@@ -171,13 +180,13 @@ export function AppNavigator(props: NavigationProps) {
       }
     };*/
 
-  const onStateChange = async (state: NavigationState) => {
-    const route = state.routeNames[state.index];
-    switch (route) {
-      case 'bridge':
-        bankInfo.fetchBankInfo(currentUser.id);
-        break;
-      /*case 'transactionList':
+    const onStateChange = async (state: NavigationState) => {
+        const route = state.routeNames[state.index];
+        switch (route) {
+            case 'bridge':
+                bankInfo.fetchBankInfo(currentUser.id);
+                break;
+            /*case 'transactionList':
               await handleError(async () => await Promise.all([transactionStore.getTransactions(), transactionStore.getTransactionCategories()]));
               break;
             /*case 'paymentList':
@@ -195,32 +204,32 @@ export function AppNavigator(props: NavigationProps) {
                   case 'invoiceForm':
                     await handleError(async () => await Promise.all([invoiceStore.getProducts(), invoiceStore.getCustomers()]));
                     break;*/
-    }
-  };
+        }
+    };
 
-  return (
-    <ErrorBoundary catchErrors={'always'}>
-      <NavigationContainer
-        linking={{
-          prefixes: ['bpartners://', Linking.createURL('/')],
-          config: {
-            screens: {
-              initialRouteName: 'welcome',
-              oauth: 'auth',
-              paymentList: 'paymentList',
-            },
-          },
-        }}
-        fallback={<Text text={'Loading...'} />}
-        ref={navigationRef}
-        theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        {...props}
-        onStateChange={onStateChange}
-      >
-        <AppStack />
-      </NavigationContainer>
-    </ErrorBoundary>
-  );
+    return (
+        <ErrorBoundary catchErrors={'always'}>
+            <NavigationContainer
+                linking={{
+                    prefixes: ['bpartners://', Linking.createURL('/')],
+                    config: {
+                        screens: {
+                            initialRouteName: 'welcome',
+                            oauth: 'auth',
+                            paymentList: 'paymentList',
+                        },
+                    },
+                }}
+                fallback={<Text text={'Loading...'}/>}
+                ref={navigationRef}
+                theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+                {...props}
+                onStateChange={onStateChange}
+            >
+                <AppStack/>
+            </NavigationContainer>
+        </ErrorBoundary>
+    );
 }
 
 AppNavigator.displayName = 'AppNavigator';
