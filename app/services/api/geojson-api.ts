ab11c4acbf69809a213ac5596a7804600158fd3f
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-04-16 14:31:22 +0300

    refactor: geojson store process

--- app/services/api/geojson-api.ts
+++ app/services/api/geojson-api.ts
@@ -3,16 +3,11 @@ import { ApiResponse } from 'apisauce';
 import { Points } from '../../models/entities/points/points';
 import { getGeneralApiProblem } from './api-problem';
 import { GeojsonResult } from './api.types';
-import { GeojsonApiConfig } from './geojson-api-config';
+import { geojsonBase } from './geojson-base-api';
 
 export class GeojsonApi {
-  private api: GeojsonApiConfig;
-
-  constructor(api: GeojsonApiConfig) {
-    this.api = api;
-  }
   async convertPoints(points: Points): Promise<GeojsonResult> {
-    const response: ApiResponse<GeojsonResult> = await this.api.apisauce.post(``, points);
+    const response: ApiResponse<GeojsonResult> = await geojsonBase.post(``, points);
 
     if (!response.ok) {
       const problem = getGeneralApiProblem(response);
