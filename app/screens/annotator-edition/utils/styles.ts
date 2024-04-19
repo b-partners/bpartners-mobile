9e57d01c68bf9d5e53f4a9bb6905256bebf70a0a
Judicael <judi.randriamanjato@gmail.com>
2024-04-19 13:26:13 +0300

    refactor: separate utility logics from the code base

---
+++ app/screens/annotator-edition/utils/styles.ts
@@ -0,0 +1,41 @@
+import {StyleSheet} from "react-native";
+import { spacing } from "../../../theme";
+import {palette} from "../../../theme/palette";
+
+export const styles = StyleSheet.create({
+    buttonContainer: {
+        width: '90%',
+        height: 45,
+        marginHorizontal: '5%',
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: palette.secondaryColor,
+        borderColor: palette.secondaryColor,
+        width: 350,
+        height: 40,
+        borderRadius: 5,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        borderWidth: 1,
+        marginVertical: spacing[1],
+    },
+    disabledButton: {
+        width: 350,
+        height: 40,
+        borderRadius: 5,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        borderWidth: 1,
+        marginVertical: spacing[1],
+        backgroundColor: palette.lighterGrey,
+        borderColor: palette.lighterGrey,
+    },
+    buttonText: {
+        fontSize: 16,
+        color: palette.white,
+    },
+    separator: {
+        borderColor: palette.lighterGrey
+    },
+});
