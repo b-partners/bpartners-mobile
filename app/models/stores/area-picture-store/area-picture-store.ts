08967c91674e8d94c26cb328d1b9a738d52c617e
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-04-26 14:58:05 +0300

    feat: implement get annotations for area picture in store

--- app/models/stores/area-picture-store/area-picture-store.ts
+++ app/models/stores/area-picture-store/area-picture-store.ts
@@ -1,7 +1,7 @@
 import { Instance, SnapshotIn, SnapshotOut, flow, types } from 'mobx-state-tree';
 
 import { AreaPictureApi } from '../../../services/api/area-picture-api';
-import { AnnotationModel } from '../../entities/annotation/annotation';
+import { Annotation, AnnotationModel } from '../../entities/annotation/annotation';
 import { AreaPicture, AreaPictureModel } from '../../entities/area-picture/area-picture';
 import { withCredentials } from '../../extensions/with-credentials';
 import { withEnvironment } from '../../extensions/with-environment';
@@ -11,7 +11,7 @@ export const AreaPictureStoreModel = types
   .model('Bank')
   .props({
     areaPicture: types.maybeNull(AreaPictureModel),
-    annotations: types.optional(types.array(AnnotationModel), []),
+    annotation: types.maybeNull(AnnotationModel),
   })
   .extend(withRootStore)
   .extend(withEnvironment)
@@ -31,6 +31,19 @@ export const AreaPictureStoreModel = types
     },
   }))
   .actions(self => ({
+    getAreaPictureAnnotationsSuccess: (annotations: Annotation[]) => {
+      if (annotations.length > 0) {
+        self.annotation = annotations[1];
+      }
+    },
+  }))
+  .actions(self => ({
+    getAreaPictureAnnotationsFail: error => {
+      __DEV__ && console.tron.log(error);
+      self.catchOrThrow(error);
+    },
+  }))
+  .actions(self => ({
     getAreaPicture: flow(function* (id: string) {
       const areaPictureApi = new AreaPictureApi(self.environment.api);
       try {
@@ -38,6 +51,17 @@ export const AreaPictureStoreModel = types
         self.getAreaPictureSuccess(getAreaPictureResult.areaPicture);
       } catch (e) {
         self.getAreaPictureFail(e);
+      }
+    }),
+  }))
+  .actions(self => ({
+    getAreaPictureAnnotations: flow(function* (id: string) {
+      const areaPictureApi = new AreaPictureApi(self.environment.api);
+      try {
+        const getAreaPictureAnnotationsResult = yield areaPictureApi.getAreaPictureAnnotations(self.currentAccount.id, id);
+        self.getAreaPictureAnnotationsSuccess(getAreaPictureAnnotationsResult.annotations);
+      } catch (e) {
+        self.getAreaPictureAnnotationsFail(e);
       }
     }),
   }));
