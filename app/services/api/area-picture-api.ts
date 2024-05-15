6c133541272ac6c92ddeef42e566821c49950ba8
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-05-15 11:27:48 +0300

    feat: implement get area picture with address in api

--- app/services/api/area-picture-api.ts
+++ app/services/api/area-picture-api.ts
@@ -2,9 +2,10 @@ import { ApiResponse } from 'apisauce';
 import uuid from 'react-native-uuid';
 
 import { Annotation } from '../../models/entities/annotation/annotation';
+import { ZoomLevel } from '../../models/entities/area-picture/area-picture';
 import { Api } from './api';
 import { getGeneralApiProblem } from './api-problem';
-import { GetAreaPictureAnnotationsResult, GetAreaPictureResult, GetAreaPicturesResult } from './api.types';
+import { GetAddressAreaPictureResult, GetAreaPictureAnnotationsResult, GetAreaPictureResult, GetAreaPicturesResult } from './api.types';
 
 export class AreaPictureApi {
   private api: Api;
@@ -37,6 +38,29 @@ export class AreaPictureApi {
 
   async getAreaPictureAnnotations(accountId: string, id: string): Promise<GetAreaPictureAnnotationsResult> {
     const response: ApiResponse<GetAreaPictureAnnotationsResult> = await this.api.apisauce.get(`accounts/${accountId}/areaPictures/${id}/annotations`);
+
+    if (!response.ok) {
+      const problem = getGeneralApiProblem(response);
+      if (problem) throw new Error(problem.kind);
+    }
+
+    return response.data;
+  }
+
+  async getAddressAreaPicture(accountId: string, prospectId: string, address: string): Promise<GetAddressAreaPictureResult> {
+    const areaPictureId = uuid.v4();
+    const fileId = uuid.v4();
+    const payload = {
+      address: address,
+      zoomLevel: ZoomLevel.HOUSES_0,
+      layer: null,
+      fileId: fileId,
+      filename: null,
+      prospectId: prospectId,
+      createdAt: null,
+      updatedAt: null,
+    };
+    const response: ApiResponse<GetAddressAreaPictureResult> = await this.api.apisauce.put(`accounts/${accountId}/areaPictures/${areaPictureId}/raw`, payload);
 
     if (!response.ok) {
       const problem = getGeneralApiProblem(response);
