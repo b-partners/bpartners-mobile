342a52f552cfd0d0ca3c9cc16235fbc9d484901a
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-06-12 14:56:08 +0300

    refactor: forgot password  screen styles

--- app/screens/forgot-password/utils/styles.ts
+++ app/screens/forgot-password/utils/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet, TextStyle } from 'react-native';
 
-import { spacing } from '../../../theme';
+import { color, spacing } from '../../../theme';
 import { palette } from '../../../theme/palette';
 
 export const normalText: TextStyle = {
@@ -124,5 +124,81 @@ export const forgotPasswordStyles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     marginTop: spacing[2],
+  },
+});
+
+export const resetPasswordStyles = StyleSheet.create({
+  screen: {
+    paddingTop: spacing[6],
+    paddingHorizontal: spacing[6],
+    marginTop: spacing[8],
+    height: 350,
+    backgroundColor: palette.solidYellow,
+    marginHorizontal: spacing[4],
+    borderRadius: 20,
+  },
+  container: {
+    marginTop: '5%',
+    padding: 15,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  field: {
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: color.primary,
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    color: palette.secondaryColor,
+  },
+  error: {
+    color: 'red',
+    marginTop: 5,
+  },
+  danger: {
+    color: 'red',
+  },
+  signup: {
+    textAlign: 'center',
+    color: palette.lightGrey,
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  disabled: {
+    borderRadius: 50,
+    paddingVertical: spacing[3],
+    backgroundColor: palette.solidGrey,
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: spacing[4],
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonLabel: {
+    color: palette.secondaryColor,
+    fontFamily: 'Geometria-Bold',
+    marginRight: spacing[2],
+  },
+  button: {
+    borderRadius: 50,
+    paddingVertical: spacing[3],
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: spacing[4],
   },
 });
