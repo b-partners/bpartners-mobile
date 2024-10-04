7f42549a213c563c84d70a06a9b7beb9aa6509ee
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-10-04 13:11:53 +0300

    feat: implement zoom level menu

--- app/screens/annotator-edition/components/annotation-header-button.tsx
+++ app/screens/annotator-edition/components/annotation-header-button.tsx
@@ -6,6 +6,8 @@ import RBSheet from 'react-native-raw-bottom-sheet';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+import { palette } from '../../../theme/palette';
+
 const zoomInProps: IconSource = {
   source: MaterialIcons.getImageSourceSync('zoom-in'),
   direction: 'auto',
@@ -35,22 +37,22 @@ export const AnnotationHeaderButtons = () => {
           margin: 'auto',
         }}
       >
-        <Button icon={zoomInProps} mode='contained' onPress={handleOpen}>
+        <Button buttonColor={palette.lighterPurple} textColor={palette.white} icon={zoomInProps} mode='contained' onPress={handleOpen}>
           Niveau de zoom
         </Button>
-        <Button icon={pictureProps} mode='contained' onPress={handleOpen}>
+        <Button buttonColor={palette.lighterPurple} textColor={palette.white} icon={pictureProps} mode='contained' onPress={handleOpen}>
           Source d'image
         </Button>
-        <Button icon={zoomOutMap} mode='contained' onPress={handleOpen}>
+        <Button buttonColor={palette.lighterPurple} textColor={palette.white} icon={zoomOutMap} mode='contained' onPress={handleOpen}>
           Recenter l'image
         </Button>
       </ScrollView>
       <RBSheet ref={refRBSheet} draggable={true} dragOnContent={true} height={400}>
         <View style={{ padding: 10 }}>
           <RadioButton.Group onValueChange={() => {}} value='19'>
-            <RadioButton.Item label='Quartier' value='19' />
-            <RadioButton.Item label='Parcelle cadastrale' value='20' />
-            <RadioButton.Item label='Toiture' value='23' />
+            <RadioButton.Item color={palette.mulberry} label='Quartier' value='19' />
+            <RadioButton.Item color={palette.mulberry} label='Parcelle cadastrale' value='20' />
+            <RadioButton.Item color={palette.mulberry} label='Toiture' value='23' />
           </RadioButton.Group>
         </View>
       </RBSheet>
