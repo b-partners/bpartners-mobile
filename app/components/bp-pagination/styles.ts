16175e5e073c361697a3d3f633bfe56b2c4fafcc
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-10-22 09:41:51 +0300

    feat: update pagination component

--- app/components/bp-pagination/styles.ts
+++ app/components/bp-pagination/styles.ts
@@ -5,15 +5,17 @@ import { palette } from '../../theme/palette';
 export const PAGINATION_STYLE = {
   container: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     flexDirection: 'row',
     width: '100%',
     marginVertical: 10,
     paddingVertical: 5,
+    paddingHorizontal: 10,
   } as StyleProp<ViewStyle>,
   iconColor: (disabled: boolean) => (disabled ? palette.lightGrey : palette.lighterPurple),
   pageText: {
     color: palette.lighterPurple,
+    fontSize: 11,
   } as StyleProp<TextStyle>,
 };
