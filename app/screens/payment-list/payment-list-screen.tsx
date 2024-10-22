83c5e63b41a77bbd9371fb39724eff24d8eded51
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-10-22 15:00:27 +0300

    fix: invoice ts error

--- app/screens/payment-list/payment-list-screen.tsx
+++ app/screens/payment-list/payment-list-screen.tsx
@@ -7,11 +7,9 @@ import { TextStyle, ViewStyle } from 'react-native';
 import { Header, Screen, Text } from '../../components';
 import { translate } from '../../i18n';
 import { useStores } from '../../models';
-import { InvoiceStatus } from '../../models/entities/invoice/invoice';
 import { TabNavigatorParamList } from '../../navigators/utils/utils';
 import { color } from '../../theme';
 import { palette } from '../../theme/palette';
-import { ErrorBoundary } from '../error/error-boundary';
 import { invoicePageSize } from '../invoice-form/utils/utils';
 import { DraftsScreen } from '../invoices/drafts-screen';
 import { InvoicesScreen } from '../invoices/invoices-screen';
@@ -30,21 +28,12 @@ type TabNameProps = {
 export const PaymentListScreen: FC<StackScreenProps<TabNavigatorParamList, 'paymentList'>> = observer(function PaymentListScreen({ navigation, route }) {
   const initialRoute = route.params?.initialRoute;
   const Tab = createMaterialTopTabNavigator();
-  const { customerStore, productStore, draftStore, quotationStore, invoiceStore } = useStores();
+  const { customerStore, productStore } = useStores();
 
   useEffect(() => {
     (async () => {
       await productStore.getProducts({ page: 1, pageSize: invoicePageSize });
       await customerStore.getCustomers({ page: 1, pageSize: invoicePageSize });
-    })();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      await draftStore.getDrafts({ status: InvoiceStatus.DRAFT, page: 1, pageSize: invoicePageSize });
-      await quotationStore.getQuotations({ status: InvoiceStatus.PROPOSAL, page: 1, pageSize: invoicePageSize });
-      await invoiceStore.getInvoices({ status: InvoiceStatus.CONFIRMED, page: 1, pageSize: invoicePageSize });
-      await invoiceStore.getPaidInvoices({ status: InvoiceStatus.PAID, page: 1, pageSize: invoicePageSize });
     })();
   }, []);
 
@@ -55,7 +44,7 @@ export const PaymentListScreen: FC<StackScreenProps<TabNavigatorParamList, 'paym
   };
 
   return (
-    <ErrorBoundary catchErrors='always'>
+    <>
       <Header headerTx='paymentListScreen.title' onLeftPress={() => navigation.navigate('bp_home')} leftIcon='back' style={HEADER} titleStyle={HEADER_TITLE} />
       <Screen>
         <Tab.Navigator
@@ -105,6 +94,6 @@ export const PaymentListScreen: FC<StackScreenProps<TabNavigatorParamList, 'paym
           />
         </Tab.Navigator>
       </Screen>
-    </ErrorBoundary>
+    </>
   );
 });
