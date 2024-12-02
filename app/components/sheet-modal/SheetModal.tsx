d6e9c156f9153628fd39d41a217c7486adeea49e
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-12-02 14:28:09 +0300

    chore: update sheet modal handler

--- app/components/sheet-modal/SheetModal.tsx
+++ app/components/sheet-modal/SheetModal.tsx
@@ -1,6 +1,6 @@
 import { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
 import React, { useCallback, useEffect, useRef } from 'react';
-import { ViewStyle } from 'react-native';
+import { BackHandler, ViewStyle } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useSheetModal } from '../../hook';
@@ -11,13 +11,28 @@ export const SheetModal = () => {
   const { isOpen, close, containerStyle, content } = useSheetModal();
 
   useEffect(() => {
-    isOpen && bottomSheetModalRef.current?.present();
+    const onBackPress = () => {
+      if (isOpen) {
+        close();
+        return true;
+      }
+      return false;
+    };
+
+    const subscription = BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+    return () => subscription.remove();
+  }, [isOpen]);
+
+  useEffect(() => {
+    isOpen ? bottomSheetModalRef.current?.present() : bottomSheetModalRef.current?.dismiss();
   }, [isOpen]);
 
   const height: ViewStyle = isOpen ? sheetModalStyles.fullHeight : { height: 0, display: 'none' };
   const handleSheetChanges = useCallback((index: number) => {
     if (index !== 0) close();
   }, []);
+
   return (
     <GestureHandlerRootView style={{ ...sheetModalStyles.container, ...height }}>
       <BottomSheetModalProvider>
--- app/components/sheet-modal/index.ts
+++ app/components/sheet-modal/index.ts
@@ -1,1 +1,2 @@
+export * from './BpSheetInput';
 export * from './SheetModal';
