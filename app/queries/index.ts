5a42dcacc59daf7049170143934880ce2b2c0054
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-12-02 14:29:32 +0300

    feat: add prospect custom mutation

--- app/queries/index.ts
+++ app/queries/index.ts
@@ -1,3 +1,4 @@
+export * from './use-crupdate-prospect';
 export * from './use-query-invoice';
 export * from './use-query-list';
 export * from './use-query-prospect';
---
+++ app/queries/use-crupdate-prospect.ts
@@ -0,0 +1,55 @@
+import { ProspectStatus, UpdateProspect } from '@bpartners/typescript-client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { v4 } from 'uuid';
+import { z as Zod } from 'zod';
+
+import { prospectProvider } from '../provider';
+
+const schema = Zod.object({
+  email: Zod.string(),
+  phone: Zod.string(),
+  address: Zod.string({ message: 'errors.required' }).min(1, 'errors.required'),
+  name: Zod.string({ message: 'errors.required' }).min(1, 'errors.required'),
+  firstName: Zod.string(),
+  comment: Zod.string(),
+  id: Zod.custom(() => true),
+  status: Zod.custom(() => true),
+});
+
+const resolver = zodResolver(schema);
+
+const prospectDefaultValues: UpdateProspect = {
+  email: '',
+  phone: '',
+  address: '',
+  name: '',
+  firstName: '',
+  status: ProspectStatus.TO_CONTACT,
+};
+
+const useProspectForm = (_defaultValues?: UpdateProspect) =>
+  useForm<UpdateProspect>({
+    mode: 'onBlur',
+    resolver: resolver,
+    defaultValues: _defaultValues || prospectDefaultValues,
+  });
+
+interface UseCrupdateProspectOptions {
+  defaultValues?: UpdateProspect;
+}
+
+export const useCrupdateProspect = (options?: UseCrupdateProspectOptions) => {
+  const { defaultValues } = options || {};
+  const form = useProspectForm(defaultValues);
+  const { data, mutate, isPending, ...others } = useMutation({ mutationFn: prospectProvider.crupdateProspect, mutationKey: ['prospect', 'crupdate'] });
+  const crupdate = form.handleSubmit((prospect: UpdateProspect) => {
+    mutate({
+      ...prospect,
+      id: prospect.id || v4(),
+    });
+  });
+
+  return { form, prospect: data, isLoading: isPending, crupdate, query: others };
+};
