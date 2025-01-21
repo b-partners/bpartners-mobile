a64c50c82842eca38c8441e5d062be091450a2c5
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-01-21 16:28:49 +0300

    feat: update bp select

--- app/components/bp-input/index.ts
+++ app/components/bp-input/index.ts
@@ -1,5 +1,6 @@
 export * from './bp-input';
 export * from './bp-input-select';
+export * from './bp-input-select-base';
 export * from './bp-input-select-simple-text-renderer';
 export * from './bp-password-input';
 export * from './style';
